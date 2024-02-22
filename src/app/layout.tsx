68a06dbdd3cefd847665864a3c1222c149436311
Olena Posternak <alena.kavyrshyna@gmail.com>
2024-02-23 00:39:01 +0200

    Add fonts
    Update basic font family and font-sizes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,44 @@ import React from "react";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import localFont from "next/font/local";
 
-const inter = Inter({ subsets: ["latin"] });
+const caviar = localFont({
+    src: [
+        {
+            path: "./fonts/CaviarDreams/CaviarDreams.ttf",
+            weight: "400",
+            style: "normal",
+        },
+        {
+            path: "./fonts/CaviarDreams/CaviarDreams_Italic.ttf",
+            weight: "400",
+            style: "italic",
+        },
+        {
+            path: "./fonts/CaviarDreams/CaviarDreams_Bold.ttf",
+            weight: "700",
+            style: "normal",
+        },
+        {
+            path: "./fonts/CaviarDreams/CaviarDreams_BoldItalic.ttf",
+            weight: "700",
+            style: "italic",
+        },
+    ],
+    variable: "--font-caviar",
+});
+
+const geist = localFont({
+    src: [
+        {
+            path: "./fonts/geist/Geist-Regular.woff2",
+            weight: "400",
+            style: "normal",
+        },
+    ],
+    variable: "--font-geist",
+});
 
 export const metadata: Metadata = {
     title: "Create Next App",
@@ -17,7 +53,9 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
-            <body className={inter.className}>{children}</body>
+            <body className={`${caviar.variable} ${geist.variable}`}>
+                {children}
+            </body>
         </html>
     );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,14 +7,64 @@ const config: Config = {
         "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     ],
     theme: {
+        fontSize: {
+            xs: " 0.75rem",
+            sm: "0.875rem",
+            base: "1rem",
+            baseb: [
+                "1.0rem",
+                {
+                    fontWeight: "700",
+                },
+            ],
+            lg: [
+                "1.125rem",
+                {
+                    fontWeight: "700",
+                },
+            ],
+            xl: "1.25rem",
+            xlb: [
+                "1.25rem",
+                {
+                    fontWeight: "700",
+                },
+            ],
+            "2xl": [
+                "1.5rem",
+                {
+                    fontWeight: "700",
+                },
+            ],
+            "4xl": [
+                "2.5rem",
+                {
+                    lineHeight: "3.25rem",
+                    fontWeight: "700",
+                },
+            ],
+            "5xl": [
+                "3.0rem",
+                {
+                    lineHeight: "3.0rem",
+
+                    fontWeight: "700",
+                },
+            ],
+        },
         extend: {
             backgroundImage: {
                 "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
                 "gradient-conic":
                     "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
             },
+            fontFamily: {
+                geist: ["var(--font-geist)"],
+                caviar: ["var(--font-caviar)"],
+            },
         },
     },
+
     plugins: [],
 };
 export default config;
