5523f5e7865f99b87f0ddf260a930f76f48d2d4d
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2024-02-22 17:32:28 +0200

    Update i18n.ts

--- src/i18n.ts
+++ src/i18n.ts
@@ -4,9 +4,9 @@ import { getRequestConfig } from "next-intl/server";
 // Can be imported from a shared config
 const locales = ["uk", "en", "pl"];
 
-export default getRequestConfig(async ({ locale }) => {
+export default getRequestConfig(async ({ locale }: { locale: string }) => {
     // Validate that the incoming `locale` parameter is valid
-    if (!locales.includes(locale as string)) notFound();
+    if (!locales.includes(locale)) notFound();
 
     return {
         messages: (await import(`../messages/${locale}.json`)).default,
