aee2a5c7b0650c9f6c0937ba7cf19610ec3f66c4
Olena Posternak <alena.kavyrshyna@gmail.com>
2024-02-24 00:44:36 +0200

    Update margin

--- src/app/[locale]/components/shared/Section.tsx
+++ src/app/[locale]/components/shared/Section.tsx
@@ -8,7 +8,7 @@ interface SectionProps {
 export const Section = ({ children, className }: SectionProps) => {
     return (
         <section
-            className={`max-w-[67.5rem] pb-[3.75rem] px-[3.75rem] mb-[3.75rem] ${className}`}
+            className={`max-w-[67.5rem] pb-[3.75rem] px-[3.75rem] mb-[6.25rem] ${className}`}
         >
             {children}
         </section>
