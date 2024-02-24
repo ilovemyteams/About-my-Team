40ba7d8b5f75130d9dc294e9bdd5fb198496c510
Olena Posternak <alena.kavyrshyna@gmail.com>
2024-02-24 22:54:08 +0200

    Update page.tsx

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,9 +1,7 @@
 import HomeTest from "@/src/components/Home";
-import { useTranslations } from "next-intl";
 import { ServicesSection } from "../../components/services/ServicesSection";
 
 export default function Home() {
-    const t = useTranslations("Home");
     return (
         <main className="flex min-h-screen flex-col items-center justify-between">
             <ServicesSection />
