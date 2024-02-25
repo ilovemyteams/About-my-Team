2c5fa09c1fc3493d808c733583b9dea937101709
IraMira333 <irinat333@gmail.com>
2024-02-25 21:55:21 +0200

    Added .env & dotenv for Playwright (baseURL)

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,10 +1,6 @@
 import { defineConfig, devices } from "@playwright/test";
 
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-//require("dotenv").config();
+require("dotenv").config();
 
 /**
  * See https://playwright.dev/docs/test-configuration.
@@ -24,7 +20,7 @@ export default defineConfig({
     /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
     use: {
         /* Base URL to use in actions like `await page.goto('/')`. */
-        baseURL: "http://localhost:3000",
+        baseURL: process.env.NEXT_PUBLIC_PLAYWRIGHT,
 
         /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
         trace: "on-first-retry",
