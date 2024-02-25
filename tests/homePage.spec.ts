854ac5679fe0ae055feff8764c52c099e416d811
IraMira333 <irinat333@gmail.com>
2024-02-25 22:25:49 +0200

    Changed baseURL

--- tests/homePage.spec.ts
+++ tests/homePage.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from "@playwright/test";
 
 test("homePageNudeTest", async ({ page }) => {
-    await page.goto("/", { waitUntil: "load" });
+    await page.goto("/uk", { waitUntil: "load" });
     await page.getByText("SMALL PAGE TITLE").click();
     await page.getByText("Назва сайту").click();
     await page.getByRole("heading", { name: "TEST JEST" }).click();
