ba5d64cbdc07cc49a8d58f7e77b70e11c4205940
Olena Posternak <alena.kavyrshyna@gmail.com>
2024-02-26 11:28:36 +0200

    Update getTranslation naming

--- src/components/services/Title.tsx
+++ src/components/services/Title.tsx
@@ -2,18 +2,18 @@ import { useTranslations } from "next-intl";
 import { SmallPageTitle } from "../shared/SmallPageTitle";
 
 export const Title = () => {
-    const t = useTranslations("Services");
+    const getTranslation = useTranslations("Services");
 
     return (
         <div>
             <h2 className="font-caviar text-5xl flex flex-col">
                 <div className="flex">
                     <SmallPageTitle className="min-w-[75px] ml-1 mr-3 h-[21px] flex items-center">
-                        {t("pageTitle")}
+                        {getTranslation("pageTitle")}
                     </SmallPageTitle>
-                    <span>{t("titleFirstRow")}</span>
+                    <span>{getTranslation("titleFirstRow")}</span>
                 </div>
-                <span>{t("titleSecondRow")}</span>
+                <span>{getTranslation("titleSecondRow")}</span>
             </h2>
         </div>
     );
