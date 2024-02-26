f3e1b9105ecf0fc48b804b84519295c00db8c645
Olena Posternak <alena.kavyrshyna@gmail.com>
2024-02-26 16:40:38 +0200

    Add One service info title

---
+++ src/components/services/OneServiceInfo.tsx
@@ -0,0 +1,9 @@
+export const OneServiceInfo = () => {
+    const t = 1;
+    return (
+        <div>
+            <div className="w-6 h-6 bg-purple-400"></div>
+            <h3>Бізнес-аналіз</h3>
+        </div>
+    );
+};
