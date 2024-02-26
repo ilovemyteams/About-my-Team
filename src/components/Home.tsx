b9d2d51d67088017faeadfa8defa9deb03aebb3f
IraMira333 <irinat333@gmail.com>
2024-02-26 21:50:33 +0200

    Update Home.tsx

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,11 +1,8 @@
 const HomeTest = () => {
-    con sisters = 5;
-
     return (
         <div>
             <h1>TEST JEST</h1>
         </div>
-        <h1>Second Test</h1>
     );
 };
 export default HomeTest;
