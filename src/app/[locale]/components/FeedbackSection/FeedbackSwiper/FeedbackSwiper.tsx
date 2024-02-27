f2a92a1e5512f3672718f9e0b3486f9c0d46e5a1
IraMira333 <irinat333@gmail.com>
2024-02-27 17:13:16 +0200

    Create FeedbackSwiper.tsx

---
+++ src/app/[locale]/components/FeedbackSection/FeedbackSwiper/FeedbackSwiper.tsx
@@ -0,0 +1,10 @@
+// import { Swiper, SwiperSlide } from "swiper/react";
+// import { Navigation, Pagination } from "swiper/modules";
+
+import "swiper/css";
+
+const FeedbackSwiper = () => {
+    return <div>Swiper</div>;
+};
+
+export default FeedbackSwiper;
