c0995416a96104a660ad663376051c6afe6a9c43
Nataliia Pashchenko <94239256+NataPas2021@users.noreply.github.com>
2024-02-28 20:12:33 +0100

    fixing font

--- src/components/sectionOurTeam/ListItem.tsx
+++ src/components/sectionOurTeam/ListItem.tsx
@@ -5,7 +5,7 @@ interface ListItemProps {
 export const ListItem = ({ children }: ListItemProps) => {
     return (
         <li className="w-64 py-0.5 px-0 mb-3 text-base leading-6 tracking-normal text-left">
-            <p className="text-base">{children}</p>
+            <p className="text-xl font-normal font-caviar">{children}</p>
         </li>
     );
 };
