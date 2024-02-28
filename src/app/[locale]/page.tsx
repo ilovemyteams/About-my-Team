e8be88fe5f818ccc98d07038b5e423a53d00c817
IraMira333 <irinat333@gmail.com>
2024-02-28 22:48:34 +0200

    Added FeedbackSection to HomePage

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,11 +1,13 @@
 import HomeTest from "@/src/components/Home";
 import { ServicesSection } from "../../components/services/ServicesSection";
 import { MissionSection } from "@/src/components/mission/MissionSection";
+import FeedbackSection from "@/src/components/FeedbackSection/FeedbackSection";
 
 export default function Home() {
     return (
         <div className="pc:ml-[80px]">
             <MissionSection />
+            <FeedbackSection />
             <ServicesSection />
             <HomeTest />
         </div>
