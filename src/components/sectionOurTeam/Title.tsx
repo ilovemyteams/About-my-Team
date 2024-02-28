826e6875861923434780a878d6bb23d607846cba
Nataliia Pashchenko <94239256+NataPas2021@users.noreply.github.com>
2024-02-28 11:20:57 +0100

    adding title wrapper

--- src/components/sectionOurTeam/Title.tsx
+++ src/components/sectionOurTeam/Title.tsx
@@ -1,16 +1,19 @@
 import { useTranslations } from "next-intl";
 import { SmallPageTitle } from "../shared/SmallPageTitle";
+import { TitleWrapper } from "../shared/TitleWrapper";
 
 export const Title = () => {
     const getTranslation = useTranslations("OurTeam");
     return (
-        <h2 className="font-caviar text-5xl flex flex-col mb-16">
-            <div className="flex">
-                <SmallPageTitle className="min-w-[75px] ml-1 mr-3 h-[21px] flex items-center">
-                    {getTranslation("pageTitle")}
-                </SmallPageTitle>
-                <span>{getTranslation("pageBigTitle")}</span>
-            </div>
-        </h2>
+        <TitleWrapper>
+            <h2 className="font-caviar text-5xl flex flex-col mb-16">
+                <div className="flex">
+                    <SmallPageTitle className="min-w-[75px] ml-1 mr-3 h-[21px] flex items-center">
+                        {getTranslation("pageTitle")}
+                    </SmallPageTitle>
+                    <p>{getTranslation("pageBigTitle")}</p>
+                </div>
+            </h2>
+        </TitleWrapper>
     );
 };
