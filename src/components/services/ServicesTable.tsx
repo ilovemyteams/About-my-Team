cc7bb05133364bb32743d640229952661fd30794
Olena Posternak <alena.kavyrshyna@gmail.com>
2024-02-28 11:25:27 +0200

    update list

--- src/components/services/ServicesTable.tsx
+++ src/components/services/ServicesTable.tsx
@@ -8,11 +8,11 @@ export const ServicesTable = () => {
     return (
         <ul className="tab:grid tab:grid-cols-2 pc:grid-cols-4 pc:grid-rows-2 pc:gap-x-0 pc:gap-y-0">
             <ServicesList />
-            <div className="col-span-2 row-span-1 py-[40px] pc:px-4 pc:py-[65px] flex flex-col items-center">
+            <li className="col-span-2 row-span-1 py-[40px] pc:px-4 pc:py-[65px] flex flex-col items-center">
                 <p className="mb-6">{getTranslation("contactUs")}</p>
                 {/* TODO: Update button text and styles when design is ready */}
                 <Button>Button</Button>
-            </div>
+            </li>
         </ul>
     );
 };
