c37458b4c344e45244ca2a5c34aa9bb7a8008937
IraMira333 <irinat333@gmail.com>
2024-02-29 22:36:18 +0200

    Update homePage.spec.ts

--- tests/homePage.spec.ts
+++ tests/homePage.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from "@playwright/test";
 
 test("test", async ({ page }) => {
-    await page.goto("/");
+    await page.goto("http://localhost:3000/");
     await expect(page.getByText("НАША МІСІЯ")).toBeVisible();
 
     await expect(page.getByText("КОНСУЛЬТАЦІЇ", { exact: true })).toBeVisible();
