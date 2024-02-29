881510baa6731d6bbda68810243f1be77921b062
IraMira333 <irinat333@gmail.com>
2024-02-29 22:19:41 +0200

    Update jest.config.ts

--- jest.config.ts
+++ jest.config.ts
@@ -6,13 +6,12 @@ const createJestConfig = nextJest({
     dir: "./",
 });
 
-// Add any custom config to be passed to Jest
 const config: Config = {
     coverageProvider: "v8",
     testEnvironment: "jsdom",
     moduleNameMapper: { "^@//(.*)$": "<rootDir>/src/$1" },
     setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
-    modulePathIgnorePatterns: ["<roodDir>/tests/"],
+    modulePathIgnorePatterns: ["<roodDir>/tests"],
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
