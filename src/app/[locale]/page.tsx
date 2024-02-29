6dcb203ca5c16566795551e20c6029bba2922b7a
IraMira333 <irinat333@gmail.com>
2024-02-29 21:42:07 +0200

    Update page.tsx

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -12,6 +12,7 @@ export default function Home() {
             <MissionSection />
             <JoinTheTeamSection />
             <FeedbackSection />
+            <JoinTheTeamSection />
             <ServicesSection />
             <StagesSection />
             <OurTeamSection />
