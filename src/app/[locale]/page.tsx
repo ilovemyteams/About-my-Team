ff181a41685f8e297e894eabc4991d109bdcd491
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2024-02-29 21:17:39 +0200

    added write us section for 320 and 1280 breakpoints

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -4,12 +4,14 @@ import { MissionSection } from "@/src/components/missionSection/MissionSection";
 import { OurTeamSection } from "@/src/components/ourTeamSection/OurTeamSection";
 import { ServicesSection } from "@/src/components/servicesSection/ServicesSection";
 import { StagesSection } from "@/src/components/stagesSection/StagesSection";
+import { WriteUsSection } from "@/src/components/writeUsSection/WriteUsSection";
 
 export default function Home() {
     return (
         <div className="pc:ml-[80px]">
             <MissionSection />
             <JoinTheTeamSection />
+            <WriteUsSection />
             <ServicesSection />
             <StagesSection />
             <OurTeamSection />
