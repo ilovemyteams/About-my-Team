de411c0e177743fcb5e3b3addb0b951b8cd488c9
Olena Posternak <alena.kavyrshyna@gmail.com>
2024-02-29 12:39:22 +0200

    del duplicated section

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,9 +1,9 @@
 import HomeTest from "@/src/components/Home";
-
-import { ServicesSection } from "../../components/sectionServices/ServicesSection";
 import { MissionSection } from "@/src/components/mission/MissionSection";
 import { OurTeamSection } from "@/src/components/sectionOurTeam/OurTeamSection";
+import { ServicesSection } from "@/src/components/servicesSection/ServicesSection";
 import { StagesSection } from "@/src/components/stagesSection/StagesSection";
+
 export default function Home() {
     return (
         <div className="pc:ml-[80px]">
