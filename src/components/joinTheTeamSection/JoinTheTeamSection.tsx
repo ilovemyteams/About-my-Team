68b7681e39bfd20111ff4e1d50b33efebb61eb7c
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2024-03-02 18:53:47 +0200

    updated TitleCTAWrapper

--- src/components/joinTheTeamSection/JoinTheTeamSection.tsx
+++ src/components/joinTheTeamSection/JoinTheTeamSection.tsx
@@ -8,7 +8,7 @@ export const JoinTheTeamSection = () => {
     return (
         <SectionCTA className="flex flex-col gap-y-[40px] pc:gap-0 pc:flex-row items-center justify-between">
             <TitleCTAWrapper className="pc:w-[70%]">
-                <p>{getTranslation("title")}</p>
+                {getTranslation("title")}
             </TitleCTAWrapper>
             <Button>Button</Button>
         </SectionCTA>
