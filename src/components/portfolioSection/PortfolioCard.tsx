dac3aa0cb4c1cc891f0010d853f350f608b4d238
IraMira333 <irinat333@gmail.com>
2024-03-04 01:22:01 +0200

    Styles PortfolioCard

--- src/components/portfolioSection/PortfolioCard.tsx
+++ src/components/portfolioSection/PortfolioCard.tsx
@@ -1,38 +1,44 @@
 import Image from "next/image";
-import { SmallPageTitle } from "../shared/SmallPageTitle";
 import { TopBox } from "./TopBox";
+import { BottomBox } from "./BottomBox";
 
 export const PortfolioCard = () => {
     return (
-        <div className="relative -mx-4">
-            <Image
-                src="https://res.cloudinary.com/dxvtacrde/image/upload/v1700146260/samples/balloons.jpg"
-                alt="Picture"
-                width={320}
-                height={384}
-            />
-            <div className="flex">
-                <div>
-                    <TopBox>Design</TopBox>
-                    <TopBox>Logo Design</TopBox>
-                    <TopBox>Web Development</TopBox>
+        <>
+            <div className="relative -mx-4 w-80 h-96">
+                <Image
+                    src="https://res.cloudinary.com/dxvtacrde/image/upload/v1700146260/samples/balloons.jpg"
+                    alt="Picture"
+                    fill
+                    style={{
+                        objectFit: "cover",
+                    }}
+                />
+
+                <div className="flex absolute mt-6 mx-4">
+                    <div>
+                        <TopBox className="mr-2 mb-2">Design</TopBox>
+                        <TopBox className="mr-2">Logo Design</TopBox>
+                        <TopBox>Web Development</TopBox>
+                    </div>
+                    <TopBox className="h-8">01</TopBox>
                 </div>
-                <TopBox>01</TopBox>
+
+                <div className="absolute bottom-6 mx-4">
+                    <BottomBox className="font-caviar font-bold text-xl mb-1">
+                        I love my team
+                    </BottomBox>
+
+                    <BottomBox className="text-sm font-normal text-purple-50 mb-3">
+                        ЛЕНДІНГ
+                    </BottomBox>
+                    <BottomBox>
+                        На нашому сайті ви знайдете інформацію про вже
+                        реалізовані проекти та команди, які стояли за їхнім
+                        створенням,
+                    </BottomBox>
+                </div>
             </div>
-            <div>
-                <h4>I love my team</h4>
-            </div>
-            <div>
-                <SmallPageTitle className="ml-1 mr-3 flex items-center">
-                    лендінг
-                </SmallPageTitle>
-            </div>
-            <div>
-                <p>
-                    На нашому сайті ви знайдете інформацію про вже реалізовані
-                    проекти та команди, які стояли за їхнім створенням,{" "}
-                </p>
-            </div>
-        </div>
+        </>
     );
 };
--- src/components/portfolioSection/TopBox.tsx
+++ src/components/portfolioSection/TopBox.tsx
@@ -1,12 +1,12 @@
-interface TopBoxProps {
+export interface BoxProps {
     children: string;
     className?: string;
 }
 
-export const TopBox = ({ children, className }: TopBoxProps) => {
+export const TopBox = ({ children, className }: BoxProps) => {
     return (
         <div
-            className={`inline-block bg-white px-3 py-1 text-lg text-grey border rounded border-grey ${className} `}
+            className={`inline-block bg-white px-3 py-1 text-lg text-grey font-caviar border rounded border-grey ${className} `}
         >
             {children}
         </div>
