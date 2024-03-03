c145841862ab0d83747aa0577df58b7c7ea0a6d5
Olena Posternak <alena.kavyrshyna@gmail.com>
2024-03-03 19:11:17 +0200

    update accessible of input

--- src/components/header/ThemeSwitcher.tsx
+++ src/components/header/ThemeSwitcher.tsx
@@ -23,13 +23,15 @@ export function ThemeSwitcher() {
         <div
             className={`relative bg-transparent w-[66px] h-[28px] border rounded-[32px] ${resolvedTheme === "dark" ? " border-grey" : " border-purple-200"}`}
         >
-            <label className="cursor-pointer ">
+            <label htmlFor="themeToggle" className="cursor-pointer ">
                 <input
                     type="checkbox"
+                    id="themeToggle"
                     value=""
                     className="sr-only"
                     defaultChecked={resolvedTheme === "dark"}
                     onChange={handleChange}
+                    aria-label="Toggle theme mode"
                 />
                 <div className="relative w-[66px] h-[28px]">
                     <div
