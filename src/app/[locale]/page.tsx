f64205d22d887f6d4965014b24d56742cf463094
IraMira333 <irinat333@gmail.com>
2024-03-03 23:24:15 +0200

    Added PortfolioSection

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -2,6 +2,7 @@ import HomeTest from "@/src/components/Home";
 import { JoinTheTeamSection } from "@/src/components/joinTheTeamSection/JoinTheTeamSection";
 import { MissionSection } from "@/src/components/missionSection/MissionSection";
 import { OurTeamSection } from "@/src/components/ourTeamSection/OurTeamSection";
+import { PortfolioSection } from "@/src/components/portfolioSection/PortfolioSection";
 import { ServicesSection } from "@/src/components/servicesSection/ServicesSection";
 import { StagesSection } from "@/src/components/stagesSection/StagesSection";
 
@@ -9,6 +10,7 @@ export default function Home() {
     return (
         <div className="pc:ml-[80px]">
             <MissionSection />
+            <PortfolioSection />
             <JoinTheTeamSection />
             <ServicesSection />
             <StagesSection />
