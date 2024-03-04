22e433c684ced14a41a9a1a9ab656f410d61a94a
Olena Posternak <alena.kavyrshyna@gmail.com>
2024-03-04 23:01:15 +0200

    update Theme switcher

--- src/components/header/ThemeSwitcher.tsx
+++ src/components/header/ThemeSwitcher.tsx
@@ -1,18 +1,23 @@
 "use client";
 
+import { useState, useEffect } from "react";
 import { useTheme } from "next-themes";
 
 export function ThemeSwitcher() {
     const { setTheme, resolvedTheme } = useTheme();
+    const [mounted, setMounted] = useState<boolean>(false);
+
+    useEffect(() => {
+        setMounted(true);
+    }, []);
 
     const handleChange = () => {
         setTheme(resolvedTheme === "dark" ? "light" : "dark");
     };
-    const userTheme = localStorage.getItem("theme");
 
     return (
         <div
-            className={`relative bg-transparent w-[66px] h-[28px] border rounded-[32px] ${resolvedTheme === "dark" || userTheme === "dark" ? " border-purple-stroke" : " border-purple-200"}`}
+            className={`relative bg-transparent w-[66px] h-[28px] border rounded-[32px] ${resolvedTheme === "dark" ? " border-purple-stroke" : " border-purple-200"}`}
         >
             <label htmlFor="themeToggle" className="cursor-pointer ">
                 <input
@@ -27,7 +32,7 @@ export function ThemeSwitcher() {
                 <div className="relative w-[66px] h-[28px]">
                     <div
                         className={`absolute w-[22px] h-[22px] rounded-full translate-y-[2px] 
-                        ${resolvedTheme === "dark" || userTheme === "dark" ? "translate-x-[40px] bg-purple-stroke" : "translate-x-[3px]  bg-purple-200"} 
+                        ${resolvedTheme === "dark" || !mounted ? "translate-x-[40px] bg-purple-stroke" : "translate-x-[3px]  bg-purple-200"} 
                         transition-transform duration-300 ease-in-out`}
                     ></div>
                 </div>
