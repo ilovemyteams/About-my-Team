8e03af8a387bb84034f3be3dd3bfac0b009b822d
IraMira333 <irinat333@gmail.com>
2024-03-04 23:34:04 +0200

    Update PortfolioSection.tsx

--- src/components/portfolioSection/PortfolioSection.tsx
+++ src/components/portfolioSection/PortfolioSection.tsx
@@ -1,14 +1,20 @@
 import { Button } from "../shared/Button";
 import { Section } from "../shared/Section";
-import { PortfolioCard } from "./PortfolioCard";
+import { PortfolioCarousel } from "./PortfolioCarousel";
+import PortfolioCarouselVertical from "./PortfolioCarouselVertical";
 import { PortfolioTitle } from "./PortfolioTitle";
+import { EmblaOptionsType } from "embla-carousel";
 
 export const PortfolioSection = () => {
+    const OPTIONS: EmblaOptionsType = { axis: "y", loop: true };
+    const SLIDE_COUNT = 5;
+    const SLIDES = Array.from(Array(SLIDE_COUNT).keys());
     return (
         <Section>
             <PortfolioTitle />
             <div className="relative flex flex-col justify-start items-center pc:items-end gap-y-[40px] pc:gap-y-[84px] ">
-                <PortfolioCard />
+                <PortfolioCarousel />
+                <PortfolioCarouselVertical slides={SLIDES} options={OPTIONS} />
                 <Button className="">Button</Button>
             </div>
         </Section>
