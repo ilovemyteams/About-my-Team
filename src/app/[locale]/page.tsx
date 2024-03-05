6eede51e8f74d83a9b0f6011c33a099e7acaff7d
IraMira333 <irinat333@gmail.com>
2024-03-05 23:01:56 +0200

    Added Section to page

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -4,6 +4,7 @@ import { HireUsSection } from "@/src/components/hireUsSection/HireUsSection";
 import { JoinTheTeamSection } from "@/src/components/joinTheTeamSection/JoinTheTeamSection";
 import { MissionSection } from "@/src/components/missionSection/MissionSection";
 import { OurTeamSection } from "@/src/components/ourTeamSection/OurTeamSection";
+import { PortfolioSection } from "@/src/components/portfolioSection/PortfolioSection";
 import { ServicesSection } from "@/src/components/servicesSection/ServicesSection";
 import { StagesSection } from "@/src/components/stagesSection/StagesSection";
 import { WriteUsSection } from "@/src/components/writeUsSection/WriteUsSection";
@@ -13,6 +14,7 @@ export default function Home() {
         <div className="pc:ml-[80px] h-[100vh] overflow-y-auto">
             <HeroSection />
             <MissionSection />
+            <PortfolioSection />
             <JoinTheTeamSection />
             <WriteUsSection />
             <ServicesSection />
