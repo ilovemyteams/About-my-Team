5dbec66120ef8ea34ccc7b19bedf7cbb99ac3940
IraMira333 <irinat333@gmail.com>
2024-03-05 14:11:46 +0200

    Update PortfolioCard.tsx

--- src/components/portfolioSection/PortfolioCard.tsx
+++ src/components/portfolioSection/PortfolioCard.tsx
@@ -2,19 +2,17 @@ import Image from "next/image";
 import { TopBox } from "./TopBox";
 import { BottomBox } from "./BottomBox";
 import { PortfolioDataItem } from "./portfolioData";
-import { useTranslations } from "next-intl";
 
 interface PortfolioItemProps {
     item: PortfolioDataItem;
 }
 
 export const PortfolioCard = ({ item }: PortfolioItemProps) => {
-    const getTranslation = useTranslations();
-    const { image, siteView, description, name } = item;
+    const { image, siteView, name, implementations, id } = item;
     const siteTypeUp = siteView.toUpperCase();
     return (
         <>
-            <div className="relative -mx-4 w-80 h-96">
+            <div className="relative w-80 h-96">
                 <Image
                     src={image}
                     alt={name}
@@ -26,22 +24,23 @@ export const PortfolioCard = ({ item }: PortfolioItemProps) => {
 
                 <div className="flex absolute mt-6 mx-4">
                     <div>
-                        <TopBox className="mr-2 mb-2">Design</TopBox>
-                        <TopBox className="mr-2">Logo Design</TopBox>
-                        <TopBox>Web Development</TopBox>
+                        {implementations.map((implementation, index) => (
+                            <TopBox key={index} className="mr-2 mb-2">
+                                {implementation}
+                            </TopBox>
+                        ))}
                     </div>
-                    <TopBox className="h-8">01</TopBox>
+                    <TopBox className="h-8">{id}</TopBox>
                 </div>
 
                 <div className="absolute bottom-6 mx-4">
                     <BottomBox className="font-caviar font-bold text-xl mb-1">
-                        {getTranslation(`${name}`)}
+                        {name}
                     </BottomBox>
 
                     <BottomBox className="text-sm font-normal text-purple-50 mb-3">
                         {siteTypeUp}
                     </BottomBox>
-                    <BottomBox>{getTranslation(`${description}`)}</BottomBox>
                 </div>
             </div>
         </>
