7380afa7108d989c25a7f8daa336ca55a34dafe0
IraMira333 <irinat333@gmail.com>
2024-03-05 22:30:00 +0200

    Added Titles

---
+++ src/components/feedbackSection/Title/FeedbackTitle.tsx
@@ -0,0 +1,21 @@
+import { useTranslations } from "next-intl";
+import { DeskTitle } from "./DeskTitle";
+import { MobTitle } from "./MobTitle";
+import { TitleWrapper } from "../../shared/TitleWrapper";
+import { SmallPageTitle } from "../../shared/SmallPageTitle";
+
+export const FeedbackTitle = () => {
+    const getTranslation = useTranslations("Feedback");
+
+    return (
+        <div>
+            <TitleWrapper className="flex flex-col pc:flex-row items-start">
+                <SmallPageTitle className="ml-1 mr-3 flex items-center">
+                    {getTranslation("pageTitle")}
+                </SmallPageTitle>
+                <DeskTitle />
+                <MobTitle />
+            </TitleWrapper>
+        </div>
+    );
+};
