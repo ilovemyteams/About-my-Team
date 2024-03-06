e02240b09a624e3031065f7a090281ab5d5e0373
Olena Posternak <alena.kavyrshyna@gmail.com>
2024-03-06 09:48:28 +0200

    add by me a coffee link

--- src/components/shared/BuyMeCoffee.tsx
+++ src/components/shared/BuyMeCoffee.tsx
@@ -1,9 +1,11 @@
 import Link from "next/link";
 import Image from "next/image";
 
+const BUY_ME_COFFEE = "https://www.buymeacoffee.com/susanna.salata";
+
 export const BuyMeCoffee = () => {
     return (
-        <Link href="#" target="_blank" className="pointer">
+        <Link href={BUY_ME_COFFEE} target="_blank" className="pointer">
             <Image
                 src="/images/CoffeeCup.svg"
                 width={31}
