eaaa99ce5dc5efdda31ed8f604b34be14a23abdb
IraMira333 <irinat333@gmail.com>
2024-03-06 13:28:10 +0200

    Update FeedbackSection.tsx

--- src/components/feedbackSection/FeedbackSection.tsx
+++ src/components/feedbackSection/FeedbackSection.tsx
@@ -1,11 +1,18 @@
 import { Button } from "../shared/Button";
 import { Section } from "../shared/Section";
+import { FeedbackCardFirstPart, FeedbackCardSecondPart } from "./FeedbackCard";
 import { FeedbackTitle } from "./Title/FeedbackTitle";
+import { FeedbackData } from "./feedbackData";
+
+const feedbackItems = FeedbackData();
+const feedbackItem = feedbackItems[1];
 
 const FeedbackSection = () => {
     return (
         <Section>
             <FeedbackTitle />
+            <FeedbackCardFirstPart item={feedbackItem} />
+            <FeedbackCardSecondPart item={feedbackItem} />
 
             <Button className="">Button</Button>
         </Section>
