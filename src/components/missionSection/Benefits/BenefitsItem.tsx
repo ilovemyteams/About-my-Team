f3fcb4e430a54be9b7428a0c4ff6477bca4490e4
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2024-03-06 18:36:07 +0200

    updated styles for misison section according to the design 06.03

--- src/components/missionSection/Benefits/BenefitsItem.tsx
+++ src/components/missionSection/Benefits/BenefitsItem.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Image from "next/image";
 
 interface BenefitsItemProps {
     children: string;
@@ -6,8 +7,14 @@ interface BenefitsItemProps {
 
 export const BenefitsItem = ({ children }: BenefitsItemProps) => {
     return (
-        <li className="flex gap-3">
-            <div className="min-w-4 h-4 pc:min-w-6 pc:h-6 bg-purple-100"></div>
+        <li className="flex gap-3 items-start">
+            <Image
+                src="/images/check.svg"
+                width="0"
+                height="0"
+                alt="Check icon"
+                className="w-6 h-auto"
+            />
             <p className="text-base pc:text-xl">{children}</p>
         </li>
     );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -134,8 +134,9 @@ const config: Config = {
         },
         backgroundImage: {
             bgImage: 'url("/images/bgImage.png")',
-            heart: 'url("/images/Heart.svg")',
-            triangles: 'url("/images/Triangles.svg")',
+            heart: 'url("/images/heart.svg")',
+            triangles: 'url("/images/triangles.svg")',
+            check: 'url("/images/check.svg")',
             portfolioGradient:
                 "linear-gradient(to right, #1E003D 0% , #3C0566 50%, #6607B1 100%)",
         },
