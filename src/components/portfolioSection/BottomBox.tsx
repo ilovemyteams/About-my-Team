bd299a27b016324264e9d9021fdbfb908d542f32
IraMira333 <irinat333@gmail.com>
2024-03-07 12:02:43 +0200

    Deleted components

--- src/components/portfolioSection/BottomBox.tsx
+++
@@ -1,9 +0,0 @@
-import { BoxProps } from "./TopBox";
-
-export const BottomBox = ({ children, className }: BoxProps) => {
-    return (
-        <div className={`block bg-white px-0 py-0.5 w-72 ${className} `}>
-            {children}
-        </div>
-    );
-};
