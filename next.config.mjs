9b8aa16534166d2decbb4dbe3588450fc7bd224d
IraMira333 <irinat333@gmail.com>
2024-03-07 21:37:11 +0200

    Updated to remotePatterns next.config

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,13 @@ const withNextIntl = createNextIntlPlugin();
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ["res.cloudinary.com", "loremflickr.com"],
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "res.cloudinary.com",
+                port: "",
+            },
+        ],
     },
 };
 
