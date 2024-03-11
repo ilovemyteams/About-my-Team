bcf8de3fd0332b4a153ac1b45b0fd9aa2f0fdb2f
Olena Posternak <alena.kavyrshyna@gmail.com>
2024-03-11 14:32:48 +0200

    update z-index and hover styles

--- src/components/backgroundImages/BackgroundImages.tsx
+++ src/components/backgroundImages/BackgroundImages.tsx
@@ -3,41 +3,41 @@ import Image from "next/image";
 export const BackgroundImages = () => {
     return (
         <>
-            <div className="h-[100vh] absolute w-screen blur-[100px] overflow-hidden z-[-1]">
+            <div className="h-[100vh] absolute w-screen blur-[100px] overflow-hidden z-[-10]">
                 <Image
                     src="/images/bgImages/leftBottom.svg"
                     width="0"
                     height="0"
                     alt="background image circle"
-                    className="absolute left-[32px] bottom-0 w-[180px] h-[180px] z-[-1]"
+                    className="absolute left-[32px] bottom-0 w-[180px] h-[180px] z-[-10]"
                 />
                 <Image
                     src="/images/bgImages/rightBottom.svg"
                     width="0"
                     height="0"
                     alt="background image circle"
-                    className="absolute right-[240px] bottom-0 w-[540px] h-[270px] z-[-1]"
+                    className="absolute right-[240px] bottom-0 w-[540px] h-[270px] z-[-10]"
                 />
                 <Image
                     src="/images/bgImages/rightTop.svg"
                     width="0"
                     height="0"
                     alt="background image circle"
-                    className="absolute right-0 top-0 w-[314px] h-[253px] z-[-1]"
+                    className="absolute right-0 top-0 w-[314px] h-[253px] z-[-10]"
                 />
             </div>
 
-            <div className="absolute right-0 top-0 flex items-end h-[786px] w-[569px] overflow-hidden z-[-1] ">
+            <div className="absolute right-0 top-0 flex items-end h-[786px] w-[569px] overflow-hidden z-[-10] ">
                 <Image
                     src="/images/bgImages/curve.svg"
                     width="0"
                     height="0"
                     alt="background image curve"
-                    className="object-none object-left	top-[-502px] w-[1276px] h-[1288px] z-[-1]"
+                    className="object-none object-left	top-[-502px] w-[1276px] h-[1288px] z-[-10]"
                 />
             </div>
 
-            <div className="absolute left-[100px] bottom-0 h-[230px] w-[357px] overflow-hidden z-[-1]">
+            <div className="absolute left-[100px] bottom-0 h-[230px] w-[357px] overflow-hidden z-[-10]">
                 <Image
                     src="/images/bgImages/heart.svg"
                     width="0"
--- src/components/shared/BuyMeCoffeeLink.tsx
+++ src/components/shared/BuyMeCoffeeLink.tsx
@@ -14,7 +14,8 @@ export const BuyMeCoffeeLink = ({ className }: BuyMeCoffeeLinkProps) => {
             rel="noopener noreferrer"
             className={`pointer relative w-[56px] h-[56px] flex justify-center items-center
             after:content-[''] after:absolute after:w-[0px] after:h-[0px] after:rounded-full  after:bg-purple-stroke03 
-            after:scale-0 after:origin-center after:transition-scale after:ease-out after:duration-[600ms] 
+            after:scale-0 after:origin-center 
+            after:ease-out after:duration-[600ms] 
             hover:after:transition-scale hover:after:ease-out hover:after:duration-[600ms] 
             hover:after:scale-100 hover:after:blur-[2px] hover:after:w-[56px] hover:after:h-[56px]
             focus-visible:after:transition-scale focus-visible:after:ease-out focus-visible:after:duration-[600ms]
