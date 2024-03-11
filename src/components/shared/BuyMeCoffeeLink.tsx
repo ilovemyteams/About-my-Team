c0a92813bb1380f5afbe215ca6b4a5c9612f05fe
Olena Posternak <alena.kavyrshyna@gmail.com>
2024-03-11 14:11:02 +0200

    update transition

--- src/components/shared/BuyMeCoffeeLink.tsx
+++ src/components/shared/BuyMeCoffeeLink.tsx
@@ -13,10 +13,11 @@ export const BuyMeCoffeeLink = ({ className }: BuyMeCoffeeLinkProps) => {
             target="_blank"
             rel="noopener noreferrer"
             className={`pointer relative w-[56px] h-[56px] flex justify-center items-center
-            after:content-[''] after:absolute after:w-[0px] after:h-[0px] after:rounded-full after:top-0 after:left-0 after:bg-purple-stroke03 
-            after:scale-0 after:origin-center after:transition-transform after:ease-out after:duration-[600ms] 
-            hover:after:transition-transform hover:after:ease-out hover:after:duration-[600ms] 
+            after:content-[''] after:absolute after:w-[0px] after:h-[0px] after:rounded-full  after:bg-purple-stroke03 
+            after:scale-0 after:origin-center after:transition-scale after:ease-out after:duration-[600ms] 
+            hover:after:transition-scale hover:after:ease-out hover:after:duration-[600ms] 
             hover:after:scale-100 hover:after:blur-[2px] hover:after:w-[56px] hover:after:h-[56px]
+            focus-visible:after:transition-scale focus-visible:after:ease-out focus-visible:after:duration-[600ms]
             focus-visible:after:scale-100  focus-visible:after:blur-[2px] focus-visible:after:w-[56px] focus-visible:after:h-[56px] 
             focus-visible:outline-none focus-within:outline-none
             ${className}`}
