0800a4ec746033d6dc88b3ec24cd26aa0536c1ce
Olena Posternak <alena.kavyrshyna@gmail.com>
2024-03-11 14:00:30 +0200

    update z index

--- src/components/backgroundImages/BackgroundImages.tsx
+++ src/components/backgroundImages/BackgroundImages.tsx
@@ -9,35 +9,35 @@ export const BackgroundImages = () => {
                     width="0"
                     height="0"
                     alt="background image circle"
-                    className="absolute left-[32px] bottom-0 w-[180px] h-[180px] z-0"
+                    className="absolute left-[32px] bottom-0 w-[180px] h-[180px] z-[-1]"
                 />
                 <Image
                     src="/images/bgImages/rightBottom.svg"
                     width="0"
                     height="0"
                     alt="background image circle"
-                    className="absolute right-[240px] bottom-0 w-[540px] h-[270px] z-0"
+                    className="absolute right-[240px] bottom-0 w-[540px] h-[270px] z-[-1]"
                 />
                 <Image
                     src="/images/bgImages/rightTop.svg"
                     width="0"
                     height="0"
                     alt="background image circle"
-                    className="absolute right-0 top-0 w-[314px] h-[253px] z-0"
+                    className="absolute right-0 top-0 w-[314px] h-[253px] z-[-1]"
                 />
             </div>
 
-            <div className="absolute right-0 top-0 flex items-end h-[786px] w-[569px] overflow-hidden z-0 ">
+            <div className="absolute right-0 top-0 flex items-end h-[786px] w-[569px] overflow-hidden z-[-1] ">
                 <Image
                     src="/images/bgImages/curve.svg"
                     width="0"
                     height="0"
                     alt="background image curve"
-                    className="object-none object-left	top-[-502px] w-[1276px] h-[1288px] z-0"
+                    className="object-none object-left	top-[-502px] w-[1276px] h-[1288px] z-[-1]"
                 />
             </div>
 
-            <div className="absolute left-[100px] bottom-0 h-[230px] w-[357px] overflow-hidden z-0">
+            <div className="absolute left-[100px] bottom-0 h-[230px] w-[357px] overflow-hidden z-[-1]">
                 <Image
                     src="/images/bgImages/heart.svg"
                     width="0"
