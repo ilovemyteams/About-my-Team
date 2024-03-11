a579c9959c07f58f2f80f34350b2445d6a4b9fcf
Olena Posternak <alena.kavyrshyna@gmail.com>
2024-03-11 12:06:11 +0200

    update Hero text

--- src/components/heroSection/HeroInfo.tsx
+++ src/components/heroSection/HeroInfo.tsx
@@ -2,20 +2,16 @@ import { useTranslations } from "next-intl";
 import { Button } from "../shared/Button";
 
 export const HeroInfo = () => {
-    const getTranslation = useTranslations("Buttons");
+    const getTranslation = useTranslations();
 
     return (
         <div className="w-[50%] pt-6">
-            {/* TODO: Change when text is avalible */}
-            <p className="max-w-[450px]">
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sit
-                cum, ullam ratione nihil minus aperiam dicta. Maxime quod odio
-                repudiandae hic sunt ab, exercitationem totam, expedita animi
-                similique, a perspiciatis.
+            <p className="max-w-[450px] pc:h-[100px] pc:mb-[96px]">
+                {getTranslation("Hero.heroInfoText")}
             </p>
             <div className="flex flex-col gap-6 mt-[96px]">
-                <Button color="grey">{getTranslation("joinUs")}</Button>
-                <Button>{getTranslation("order")}</Button>
+                <Button>{getTranslation("Buttons.order")}</Button>
+                <Button color="grey">{getTranslation("Buttons.joinUs")}</Button>
             </div>
         </div>
     );
