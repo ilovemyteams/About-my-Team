a5dfd95ab46163b6d4f7b76bbcdf1d9c8489c70c
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2024-03-12 21:48:43 +0200

    changed styles and text for mission section

--- src/components/missionSection/MissionSection.tsx
+++ src/components/missionSection/MissionSection.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import { Button } from "../shared/Button";
 import { Section } from "../shared/Section";
 import { BenefitsList } from "./Benefits/BenefitsList";
@@ -10,9 +11,18 @@ export const MissionSection = () => {
     return (
         <Section id="mission">
             <MissionTitle />
-            <div className="relative flex flex-col justify-start items-center pc:items-end gap-y-[40px] pc:gap-y-[84px] ">
-                <BenefitsList />
-                <Button>{getTranslation("order")}</Button>
+            <div className="flex justify-around">
+                <Image
+                    src="/images/bgImages/heart.svg"
+                    width="0"
+                    height="0"
+                    alt="background image heart"
+                    className="w-[450px] h-[402px]"
+                />
+                <div className="relative flex flex-col justify-start items-center pc:items-end gap-y-[40px] pc:gap-y-[48px] pc:w-1/2">
+                    <BenefitsList />
+                    <Button>{getTranslation("order")}</Button>
+                </div>
             </div>
             <StagesList />
         </Section>
