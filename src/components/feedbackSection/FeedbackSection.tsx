cd28f66673b615822f59fc179ff30c5b027ee545
IraMira333 <irinat333@gmail.com>
2024-03-13 23:20:43 +0200

    Update FeedbackSection.tsx

--- src/components/feedbackSection/FeedbackSection.tsx
+++ src/components/feedbackSection/FeedbackSection.tsx
@@ -1,10 +1,22 @@
+import { useLocale } from "next-intl";
 import { Section } from "../shared/Section";
+import { FeedbackCard } from "./FeedbackCard";
 import { FeedbackTitle } from "./Title/FeedbackTitle";
+import { FeedbackDataItemType, feedbackData } from "./feedbackData";
+
+type Locale = "uk" | "en" | "pl";
+
+const feedback: FeedbackDataItemType = feedbackData[1];
 
 export const FeedbackSection = () => {
+    const locale = useLocale();
     return (
         <Section id="feedback">
             <FeedbackTitle />
+            <FeedbackCard
+                data={feedback.data}
+                localizationData={feedback[locale as Locale]}
+            />
         </Section>
     );
 };
