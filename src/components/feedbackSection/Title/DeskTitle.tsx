a52c29d76cb64529db7ba4f7fc888ffaa9a413f9
IraMira333 <irinat333@gmail.com>
2024-03-13 16:39:44 +0200

    Create DeskTitle.tsx

---
+++ src/components/feedbackSection/Title/DeskTitle.tsx
@@ -0,0 +1,22 @@
+import { useTranslations } from "next-intl";
+
+export const DeskTitle = () => {
+    const getTranslation = useTranslations("Feedback");
+    return (
+        <div className="hidden pc:block">
+            <p>
+                {getTranslation("titleFirstWord")}
+                <span className="pc:text-purple-100">
+                    {getTranslation("titleSecondWord")}
+                </span>
+                ,
+            </p>
+            <p>
+                {getTranslation("titleThirdWord")}
+                <span className="pc:text-purple-100">
+                    {getTranslation("titleFourthWord")}
+                </span>
+            </p>
+        </div>
+    );
+};
