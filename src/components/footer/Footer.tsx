6e8937ae807331e22ee3762289d074aaedf12301
Nataliia Pashchenko <94239256+NataPas2021@users.noreply.github.com>
2024-03-14 11:51:49 +0100

    min-w

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -7,7 +7,7 @@ import { TitleFooter } from "./TitleFooter/TitleFooter";
 
 export const Footer = () => {
     return (
-        <footer className="w-[1200px] ml-[80px] px-[60px] pc:mb-[40px] ">
+        <footer className="min-w-[1200px] ml-[80px] px-[60px] pc:mb-[40px] ">
             <div className="border-y-[1px] border-solid border-purple-stroke">
                 <TitleFooter />
                 <div className="flex items-baseline pb-0 mb-[28px] justify-between">
