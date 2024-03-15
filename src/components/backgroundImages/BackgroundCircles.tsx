4ee0aa0290941f95781cdc8d2e500adf7a011b37
Olena Posternak <101277404+OlenaPosternak@users.noreply.github.com>
2024-03-15 23:16:11 +0200

    update bg images
    add new test  images

--- src/components/backgroundImages/BackgroundCircles.tsx
+++ src/components/backgroundImages/BackgroundCircles.tsx
@@ -10,21 +10,21 @@ export const BackgroundCircles = ({ className }: BackgroundCirclesProps) => {
             className={`hidden pc:block h-[100vh] absolute w-screen blur-[100px] overflow-hidden z-[-10] ${className}`}
         >
             <Image
-                src="/images/bgImages/leftBottom.svg"
+                src="/images/bgImages/leftBottom1.svg"
                 width="0"
                 height="0"
                 alt="background image circle"
                 className={`absolute left-[32px] bottom-0 w-[180px] h-[180px] z-[-10]`}
             />
             <Image
-                src="/images/bgImages/rightBottom.svg"
+                src="/images/bgImages/rightBottom1.svg"
                 width="0"
                 height="0"
                 alt="background image circle"
                 className={`absolute right-[240px] bottom-0 w-[540px] h-[270px] z-[-10]`}
             />
             <Image
-                src="/images/bgImages/rightTop.svg"
+                src="/images/bgImages/rightTop1.svg"
                 width="0"
                 height="0"
                 alt="background image circle"
