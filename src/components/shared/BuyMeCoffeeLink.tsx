9b73498e7e5a6738d3ddd01760cb0dbe689c0271
Nataliia Pashchenko <94239256+NataPas2021@users.noreply.github.com>
2024-03-15 11:56:48 +0100

    update BuyMeCoffeLink w h

--- src/components/shared/BuyMeCoffeeLink.tsx
+++ src/components/shared/BuyMeCoffeeLink.tsx
@@ -12,11 +12,11 @@ export const BuyMeCoffeeLink = ({ className }: BuyMeCoffeeLinkProps) => {
             href={BUY_ME_COFFEE}
             target="_blank"
             rel="noopener noreferrer"
-            className={`pointer relative w-[48px] h-[48px] flex justify-center items-center
+            className={`pointer relative w-[56px] h-[56px] flex justify-center items-center
             after:content-[''] after:absolute after:w-[0px] after:h-[0px] after:rounded-full after:bg-purple-stroke03 
             after:scale-0 after:origin-center after:blur-[2px] after:transition-scale after:ease-out after:duration-[600ms]
-            hover:after:scale-100 hover:after:blur-[2px] hover:after:w-[56px] hover:after:h-[56px]
-            focus-visible:after:scale-100 focus-visible:after:blur-[2px] focus-visible:after:w-[56px] focus-visible:after:h-[56px] 
+            hover:after:scale-100 hover:after:blur-[2px] hover:after:w-full hover:after:full
+            focus-visible:after:scale-100 focus-visible:after:blur-[2px] focus-visible:after:w-full focus-visible:after:full 
             focus-visible:outline-none focus-within:outline-none
             ${className}`}
         >
