0fa3a8d11582f61ae1e5ddb1585e3834703e133e
IraMira333 <irinat333@gmail.com>
2024-03-16 21:41:25 +0200

    Update CarouselButtons.tsx

--- src/components/shared/SliderComponents/CarouselButtons.tsx
+++ src/components/shared/SliderComponents/CarouselButtons.tsx
@@ -5,7 +5,7 @@ import React, {
     useState,
 } from "react";
 import { EmblaCarouselType } from "embla-carousel";
-import { ArrowIcon } from "../../portfolioSection/Slider/ArrowIcon";
+import { ArrowIcon } from "./ArrowIcon";
 
 type UsePrevNextButtonsType = {
     prevBtnDisabled: boolean;
