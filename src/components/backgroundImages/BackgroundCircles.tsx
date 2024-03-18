c51d2312f7f8738ef6dfb1fac2e3186c606e068a
Olena Posternak <101277404+OlenaPosternak@users.noreply.github.com>
2024-03-18 12:57:28 +0200

    update styles

--- src/components/backgroundImages/BackgroundCircles.tsx
+++ src/components/backgroundImages/BackgroundCircles.tsx
@@ -1,14 +1,22 @@
 import Image from "next/image";
 
-export const BackgroundCircles = () => {
+export const BackgroundCircles = ({
+    className,
+    classNameLeftCircle,
+}: {
+    className?: string;
+    classNameLeftCircle?: string;
+}) => {
     return (
-        <div className="hidden pc:block h-[100vh] absolute w-screen overflow-hidden z-[-10]">
+        <div
+            className={`h-[100vh] absolute overflow-hidden z-[-10] ${className}`}
+        >
             <Image
                 src="/images/bgImages/leftBottom1.svg"
                 width="0"
                 height="0"
                 alt="background image circle"
-                className="absolute left-0 bottom-0 w-[363px] h-[333px] z-[-10]"
+                className={`absolute left-0 bottom-0 w-[363px] h-[333px] z-[-10] ${classNameLeftCircle}`}
             />
             <Image
                 src="/images/bgImages/rightBottom1.svg"
