aa8dd6f0902bb3fbe888864b84dbbed1eb3c63cf
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2024-03-18 13:50:21 +0200

    updated mission section styles for mobile

--- src/components/missionSection/Benefits/BenefitsList.tsx
+++ src/components/missionSection/Benefits/BenefitsList.tsx
@@ -10,7 +10,7 @@ export const BenefitsList = () => {
     ];
 
     return (
-        <ul className="flex flex-col justify-center gap-4 w-full pc:gap-[61px]">
+        <ul className="flex flex-col justify-center gap-4 tab:gap-[10px] pc:gap-[61px] w-full">
             {benefitsList.map((benefit, idx) => (
                 <BenefitsItem key={idx}>{benefit}</BenefitsItem>
             ))}
