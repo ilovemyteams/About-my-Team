6c74c8081544ced1f9d207267f632c17a21fe21e
Olena Posternak <101277404+OlenaPosternak@users.noreply.github.com>
2024-03-18 13:23:06 +0200

    update pictures

--- src/components/backgroundImages/BackgroundCircles.tsx
+++ src/components/backgroundImages/BackgroundCircles.tsx
@@ -7,21 +7,21 @@ interface BackgroundCirclesProps {
 export const BackgroundCircles = ({ className }: BackgroundCirclesProps) => {
     return (
         <div
-            className={`hidden pc:block h-[100vh] absolute w-screen blur-[100px] overflow-hidden z-[-10] ${className}`}
+            className={`hidden pc:block h-[100vh] absolute w-screen  overflow-hidden z-[-10] ${className}`}
         >
             <Image
                 src="/images/bgImages/leftBottom.svg"
                 width="0"
                 height="0"
                 alt="background image circle"
-                className={`absolute left-[32px] bottom-0 w-[180px] h-[180px] z-[-10]`}
+                className={`absolute left-0 bottom-0 w-[363px] h-[333px] z-[-10]`}
             />
             <Image
                 src="/images/bgImages/rightBottom.svg"
                 width="0"
                 height="0"
                 alt="background image circle"
-                className={`absolute right-[240px] bottom-0 w-[540px] h-[270px] z-[-10]`}
+                className={`absolute right-0 bottom-0 w-[970px] h-[427px] z-[-10]`}
             />
             <Image
                 src="/images/bgImages/rightTop.svg"
