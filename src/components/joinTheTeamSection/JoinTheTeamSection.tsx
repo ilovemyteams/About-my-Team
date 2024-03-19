39268a5b6b6685229c4ee874888d6282e7c3188b
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2024-03-19 18:19:35 +0200

    updated styles for join us and write us sections

--- src/components/joinTheTeamSection/JoinTheTeamSection.tsx
+++ src/components/joinTheTeamSection/JoinTheTeamSection.tsx
@@ -7,8 +7,8 @@ export const JoinTheTeamSection = () => {
     const getTranslation = useTranslations();
 
     return (
-        <SectionCTA className="flex flex-col gap-y-[40px] pc:gap-0 pc:flex-row items-center justify-between">
-            <TitleCTAWrapper className="pc:w-[70%]">
+        <SectionCTA className="flex flex-col gap-y-[40px] tab:gap-0 tab:flex-row items-center justify-between">
+            <TitleCTAWrapper className="tab:w-[50%] pc:w-[70%]">
                 {getTranslation("JoinTheTeam.title")}
             </TitleCTAWrapper>
             <Button color="grey">{getTranslation("Buttons.joinUs")}</Button>
