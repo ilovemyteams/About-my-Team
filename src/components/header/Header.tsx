5c23ae1f929a0a9ed9bb283c13a686f6fb5fe32f
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2024-03-19 16:25:44 +0200

    updated styles for max-w 540

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,11 +1,11 @@
 import { DesktopHeader } from "./DesktopHeader/DesktopHeader";
-import { MobileHeader } from "./MobileTabletHeader/MobileTabletHeader";
+import { MobileTabletHeader } from "./MobileTabletHeader/MobileTabletHeader";
 
 export const Header = () => {
     return (
         <header className="fixed top-0 left-0 z-20">
             <DesktopHeader />
-            <MobileHeader />
+            <MobileTabletHeader />
         </header>
     );
 };
