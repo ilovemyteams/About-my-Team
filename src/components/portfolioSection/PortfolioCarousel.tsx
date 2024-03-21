1bccc54bf5df9402cf9bd535dd137b93f317a09c
IraMira333 <irinat333@gmail.com>
2024-03-21 23:09:01 +0200

    Updated mt for buttons for mobile

--- src/components/portfolioSection/PortfolioCarousel.tsx
+++ src/components/portfolioSection/PortfolioCarousel.tsx
@@ -52,7 +52,7 @@ export const PortfolioCarousel: React.FC<PortfolioCarouselProps> = props => {
                         </div>
                     ))}
                 </div>
-                <div className="embla__controls  tab:absolute tab:-top-[80px] pc:-top-[112px] right-0 mt-6 tab:mt-0">
+                <div className="embla__controls  tab:absolute tab:-top-[80px] pc:-top-[112px] right-0 mt-[16px] tab:mt-0">
                     <div className="embla__buttons flex justify-between gap-6">
                         <PrevButton
                             onClick={onPrevButtonClick}
