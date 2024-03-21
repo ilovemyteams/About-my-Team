e60940500267d78bc36502d6a9bd0f5e138663ce
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2024-03-21 22:19:56 +0200

    updated styles scroll to top button for tablet and pc

--- src/components/scrollToTopButton/ScrollToTopButton.tsx
+++ src/components/scrollToTopButton/ScrollToTopButton.tsx
@@ -9,9 +9,9 @@ export const ScrollToTopButton = () => {
         const toggleVisible = () => {
             const scrolled = document.documentElement.scrollTop;
 
-            if (scrolled > 300) {
+            if (scrolled > 800) {
                 setIsVisible(true);
-            } else if (scrolled <= 300) {
+            } else if (scrolled <= 800) {
                 setIsVisible(false);
             }
         };
@@ -27,7 +27,7 @@ export const ScrollToTopButton = () => {
     return (
         <>
             <button
-                className={`${isVisible ? "block" : "hidden"} text-grey fixed bottom-[264px] right-[60px] w-[64px] h-[64px] p-3 rounded-full color-grey after:content-[''] after:absolute after:top-0 after:left-0 after:z-[-10] after:w-[64px] after:h-[64px] after:rounded-full after:bg-purple-100 bg-opacity-80 after:blur-[2px]`}
+                className={`${isVisible ? "block" : "hidden"} text-grey fixed bottom-[235px] right-[28px] pc:bottom-[264px] pc:right-[60px] w-[64px] h-[64px] p-3 rounded-full color-grey after:content-[''] after:absolute after:top-0 after:left-0 after:z-[-10] after:w-[64px] after:h-[64px] after:rounded-full after:bg-purple-100 bg-opacity-80 after:blur-[2px]`}
                 onClick={scrollToTop}
             >
                 <IconUp />
