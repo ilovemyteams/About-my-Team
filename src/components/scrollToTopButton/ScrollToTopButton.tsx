683f4ccf51729b57bd2aeb5fc3d25f8835d022f0
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2024-03-22 17:31:22 +0200

    update scroll to top button opacity

--- src/components/scrollToTopButton/ScrollToTopButton.tsx
+++ src/components/scrollToTopButton/ScrollToTopButton.tsx
@@ -27,7 +27,7 @@ export const ScrollToTopButton = () => {
     return (
         <>
             <button
-                className={`${isVisible ? "block" : "hidden"} text-grey fixed bottom-[164px] right-[8px] tab:right-[28px] pc:right-[60px] w-[64px] h-[64px] p-3 rounded-full color-grey after:content-[''] after:absolute after:top-0 after:left-0 after:z-[-10] after:w-[64px] after:h-[64px] after:rounded-full after:bg-purple-100 bg-opacity-80 after:blur-[2px]`}
+                className={`${isVisible ? "block" : "hidden"} text-grey fixed bottom-[164px] right-[8px] tab:right-[28px] pc:right-[60px] w-[64px] h-[64px] p-3 rounded-full color-grey after:content-[''] after:absolute after:top-0 after:left-0 after:z-[-10] after:w-[64px] after:h-[64px] after:rounded-full after:bg-purple-100 after:bg-opacity-40 after:blur-[2px]`}
                 onClick={scrollToTop}
             >
                 <IconUp />
