85ff73d98ce75b50a5e49183f17dadf96ea0a03e
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2024-03-22 12:01:38 +0200

    updated scroll to top button position on mobile

--- src/components/scrollToTopButton/ScrollToTopButton.tsx
+++ src/components/scrollToTopButton/ScrollToTopButton.tsx
@@ -27,7 +27,7 @@ export const ScrollToTopButton = () => {
     return (
         <>
             <button
-                className={`${isVisible ? "block" : "hidden"} text-grey fixed bottom-[235px] right-[28px] pc:bottom-[264px] pc:right-[60px] w-[64px] h-[64px] p-3 rounded-full color-grey after:content-[''] after:absolute after:top-0 after:left-0 after:z-[-10] after:w-[64px] after:h-[64px] after:rounded-full after:bg-purple-100 bg-opacity-80 after:blur-[2px]`}
+                className={`${isVisible ? "block" : "hidden"} text-grey fixed bottom-[164px] right-[8px] tab:right-[28px] pc:right-[60px] w-[64px] h-[64px] p-3 rounded-full color-grey after:content-[''] after:absolute after:top-0 after:left-0 after:z-[-10] after:w-[64px] after:h-[64px] after:rounded-full after:bg-purple-100 bg-opacity-80 after:blur-[2px]`}
                 onClick={scrollToTop}
             >
                 <IconUp />
