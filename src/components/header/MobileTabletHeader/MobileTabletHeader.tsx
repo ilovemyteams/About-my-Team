23b525453e1216eb17f89972390abc7e2befb858
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2024-03-24 21:35:00 +0200

    updated header menu height

--- src/components/header/MobileTabletHeader/MobileTabletHeader.tsx
+++ src/components/header/MobileTabletHeader/MobileTabletHeader.tsx
@@ -19,7 +19,7 @@ export const MobileTabletHeader = () => {
         <div className="pc:hidden absolute top-0 left-0 w-[100vw]">
             <div className="absolute z-20 h-[80px] overflow-hidden bg-grey dark:bg-purple-400">
                 <BackgroundCirclesMobile />
-                <div className=" w-[100vw] h-[80px] px-[24px] py-[20px] border-solid border-b-[1px] border-purple-stroke ">
+                <div className=" w-[100vw] h-[80px] px-[24px] py-[15px] border-solid border-b-[1px] border-purple-stroke ">
                     <div className="flex justify-between items-center max-w-[540px] tab:max-w-full mx-auto">
                         <LogoLink
                             setIsHeaderMenuOpened={setIsHeaderMenuOpened}
--- src/components/header/MobileTabletHeader/MobileTabletHeaderMenu.tsx
+++ src/components/header/MobileTabletHeader/MobileTabletHeaderMenu.tsx
@@ -24,7 +24,7 @@ export const MobileTabletHeaderMenu = ({
     bg-grey dark:bg-purple-400 ${isHeaderMenuOpened ? openedStyles : closedStyles}`}
         >
             <BackgroundCirclesMobile className="top-[-80px]" />
-            <div className="flex flex-col justify-between items-center max-w-[540px] mx-auto tab:max-w-full h-80 p-4 tab:px-6 tab:py-[80px] mb-2 tab:mb-0 overflow-y-auto">
+            <div className="flex flex-col justify-between items-center max-w-[540px] mx-auto tab:max-w-full h-70 p-4 tab:px-6 tab:py-[80px] mb-2 tab:mb-0 overflow-y-auto">
                 <div className="flex justify-between tab:justify-end tab:gap-[111px] z-20 w-full">
                     <LocaleSwitcher />
                     <ThemeSwitcher />
