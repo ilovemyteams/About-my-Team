d12e062ca53e02cd7bb7da065754bf658a9d9af1
Nataliia Pashchenko <94239256+NataPas2021@users.noreply.github.com>
2024-03-26 17:17:52 +0100

    json for one card(test)

--- src/components/ourTeamSection/membersData.ts
+++ src/components/ourTeamSection/membersData.ts
@@ -1,11 +1,8 @@
-//import { ToolsProp } from "@/types/ToolsProp";
-
 export type MemberDataItemTranslation = {
     name: string;
     about: string;
     services: string;
     team: string;
-    category: string;
 };
 
 export type SocialLinks = {
@@ -19,10 +16,11 @@ export type DataType = {
     id: string;
     imageURL?: string;
     position: string;
+    categoryName: string;
     pricePerHour: string;
     projectsExperience: string[];
     socialLinks: SocialLinks;
-    tools: string[]; //ToolsProp;
+    tools: string[];
 };
 
 export type MemberDataItemType = {
@@ -38,6 +36,7 @@ export const membersData: MemberDataItemType[] = [
             id: "1",
             imageURL: "",
             position: "product manager",
+            categoryName: "manager",
             projectsExperience: ["www.website.ua"],
             pricePerHour: "100",
             socialLinks: {
@@ -51,21 +50,18 @@ export const membersData: MemberDataItemType[] = [
             services:
                 "Створення зручних для користувачів та ефективних для бізнесу web застосунків різного рівня складності. Фокусуюсь на поєднанні максимальної функціональності та естетичності для досягнення бездоганного результату.",
             team: "I Love My Team",
-            category: "продукт менеджери",
         },
         en: {
             name: "Susanna Salata",
             about: "Result oriented",
             services: "There will be text in English",
             team: "I Love My Team",
-            category: "product manager",
         },
         pl: {
             name: "Susanna Salata",
             about: "There will be text in Polish",
             services: "There will be text in Polish",
             team: "I Love My Team",
-            category: "product manager",
         },
     },
 ];
---
+++ src/components/ourTeamSection/selectData.ts
@@ -0,0 +1,24 @@
+type СategoryNamesProp = {
+    name: string;
+    ua: string;
+    pl: string;
+    en: string;
+};
+
+export const categoryNames: СategoryNamesProp[] = [
+    {
+        name: "developers",
+        ua: "розробники",
+        pl: "developers",
+        en: "developers",
+    },
+    { name: "designers", ua: "дизайнери", pl: "designers", en: "designers" },
+    { name: "qa", ua: "тестувальники", pl: "qa", en: "qa" },
+    {
+        name: "business-analytics",
+        ua: "бізнес-аналітики",
+        pl: "designers",
+        en: "designers",
+    },
+    { name: "managers", ua: "менеджери", pl: "designers", en: "designers" },
+];
