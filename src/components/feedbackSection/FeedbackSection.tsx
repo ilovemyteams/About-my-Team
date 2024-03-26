3810ebf19ca80fbb6c30c59b8a4fb94589cdb744
IraMira333 <irinat333@gmail.com>
2024-03-26 23:58:47 +0200

    Styling for ShareButton

--- src/components/feedbackSection/FeedbackSection.tsx
+++ src/components/feedbackSection/FeedbackSection.tsx
@@ -3,13 +3,15 @@ import { Section } from "../shared/Section";
 import { FeedbackTitle } from "./Title/FeedbackTitle";
 import { feedbackData } from "./feedbackData";
 import { FeedbackSlider } from "./FeedbackSlider";
+import { SharePopover } from "../shared/SharePopover";
 
 export const FeedbackSection = () => {
     const OPTIONS: EmblaOptionsType = { loop: true, align: "start" };
     return (
-        <Section id="feedback">
+        <Section id="feedback" className="relative">
             <FeedbackTitle />
             <FeedbackSlider feedbacks={feedbackData} options={OPTIONS} />
+            <SharePopover />
         </Section>
     );
 };
--- src/components/portfolioSection/PortfolioSection.tsx
+++ src/components/portfolioSection/PortfolioSection.tsx
@@ -3,15 +3,17 @@ import { PortfolioCarousel } from "./PortfolioCarousel";
 import { PortfolioTitle } from "./Title/PortfolioTitle";
 import { EmblaOptionsType } from "embla-carousel";
 import { portfolioData } from "./portfolioData";
+import { SharePopover } from "../shared/SharePopover";
 
 export const PortfolioSection = () => {
     const OPTIONS: EmblaOptionsType = { loop: true, align: "start" };
     return (
-        <Section id="portfolio">
+        <Section id="portfolio" className="relative">
             <PortfolioTitle />
             <div className="relative flex flex-col justify-start items-center pc:items-end gap-y-[40px] pc:gap-y-[84px] ">
                 <PortfolioCarousel projects={portfolioData} options={OPTIONS} />
             </div>
+            <SharePopover />
         </Section>
     );
 };
