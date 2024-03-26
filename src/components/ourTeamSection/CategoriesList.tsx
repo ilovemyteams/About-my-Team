92faffa32e9c93292c11d8bfb7f25ee17260dcbe
Nataliia Pashchenko <94239256+NataPas2021@users.noreply.github.com>
2024-03-26 17:51:22 +0100

    commented code for vercel

--- src/components/ourTeamSection/CategoriesList.tsx
+++ src/components/ourTeamSection/CategoriesList.tsx
@@ -1,26 +1,26 @@
-import { useTranslations } from "next-intl";
-import { ListItem } from "./ListItem";
+//import { useTranslations } from "next-intl";
+//import { ListItem } from "./ListItem";
 
 export const CategoriesList = () => {
-    const getTranslation = useTranslations("OurTeam");
-    const categoriesList = [
-        getTranslation("listItemDes"),
-        getTranslation("listItemDev"),
-        getTranslation("listItemQA"),
-        getTranslation("listItemBA"),
-        getTranslation("listItemPM"),
-    ];
+    // const getTranslation = useTranslations("OurTeam");
+    // const categoriesList = [
+    //     getTranslation("listItemDes"),
+    //     getTranslation("listItemDev"),
+    //     getTranslation("listItemQA"),
+    //     getTranslation("listItemBA"),
+    //     getTranslation("listItemPM"),
+    // ];
 
     return (
         <div className="pt-[12px]">
-            <span className="font-geist font-normal text-sm text-purple-50 mb-8 pc:mt-[12px]">
+            {/* <span className="font-geist font-normal text-sm text-purple-50 mb-8 pc:mt-[12px]">
                 {getTranslation("listTitle").toUpperCase()}
             </span>
             <ul className="[&>*:first-child]:mt-[12px] [&>*:last-child]:mb-[12px]">
                 {categoriesList.map((category, idx) => (
                     <ListItem key={idx}>{category}</ListItem>
                 ))}
-            </ul>
+            </ul> */}
         </div>
     );
 };
--- src/components/ourTeamSection/TeamsList.tsx
+++ src/components/ourTeamSection/TeamsList.tsx
@@ -1,19 +1,19 @@
-import { useTranslations } from "next-intl";
-import { ListItem } from "./ListItem";
+//import { useTranslations } from "next-intl";
+//import { ListItem } from "./ListItem";
 
 export const TeamsList = () => {
-    const getTranslation = useTranslations("OurTeam");
-    const teamList = [getTranslation("team1"), getTranslation("team2")];
+    //const getTranslation = useTranslations("OurTeam");
+    //const teamList = [getTranslation("team1"), getTranslation("team2")];
     return (
         <>
-            <span className="font-geist font-normal text-sm text-purple-50">
+            {/* <span className="font-geist text-sm text-purple-50">
                 {getTranslation("teamsList").toUpperCase()}
             </span>
             <ul className="[&>*:first-child]:mt-[12px] [&>*:last-child]:mb-[12px] after:absolute after:border-b-[1px] after:border-purple-stroke after:left-[32px] tab:after:left-0 after:w-[169px]">
                 {teamList.map((team, idx) => (
                     <ListItem key={idx}>{team}</ListItem>
                 ))}
-            </ul>
+            </ul> */}
         </>
     );
 };
