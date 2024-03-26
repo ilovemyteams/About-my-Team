bf0d4a507a7817ecad20156ee3275b7d04dd34c8
Nataliia Pashchenko <94239256+NataPas2021@users.noreply.github.com>
2024-03-26 22:34:29 +0100

    adding using portfolioData

--- src/components/ourTeamSection/TeamsList.tsx
+++ src/components/ourTeamSection/TeamsList.tsx
@@ -1,6 +1,6 @@
 import { useTranslations } from "next-intl";
 import { ListItem } from "./ListItem";
-import { teamNames } from "./data/teamNames";
+import { portfolioData } from "../portfolioSection/portfolioData";
 
 export const TeamsList = () => {
     const getTranslation = useTranslations("OurTeam");
@@ -11,9 +11,10 @@ export const TeamsList = () => {
                 {getTranslation("teamsList").toUpperCase()}
             </span>
             <ul className="[&>*:first-child]:mt-[12px] [&>*:last-child]:mb-[12px] after:absolute after:border-b-[1px] after:border-purple-stroke after:left-[32px] tab:after:left-0 after:w-[169px]">
-                {teamNames.map(({ name }, idx) => (
+                {portfolioData.map((item, idx) => (
                     <ListItem key={idx}>
-                        {name.charAt(0).toUpperCase() + name.slice(1)}
+                        {item.uk.name.charAt(0).toUpperCase() +
+                            item.uk.name.slice(1)}
                     </ListItem>
                 ))}
             </ul>
--- src/components/ourTeamSection/data/teamNames.ts
+++
@@ -1,8 +0,0 @@
-export type TeamNamesProp = {
-    name: string;
-};
-
-export const teamNames: TeamNamesProp[] = [
-    { name: "i love my Team" },
-    { name: "ферма" },
-];
