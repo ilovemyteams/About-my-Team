e7edf8ab650994595309f5786f0eca2db216546f
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2024-03-26 10:31:26 +0200

    added disabled and active

--- src/components/shared/Button.tsx
+++ src/components/shared/Button.tsx
@@ -12,7 +12,8 @@ export const Button = ({ children, color = "red", className }: ButtonProps) => {
             className={`group ${color === "red" ? redColor : greyColor} relative w-[288px] min-h-[48px] h-[48px] tab:w-[270px] tab:h-[56px] font-caviar text-xlb tab:text-2xl border-solid border-y-[1px]
             pc:hover:after:transition-opacity pc:hover:after:duration-200 pc:hover:after:ease-out pc:hover:before:transition-opacity pc:hover:before:duration-200 pc:hover:before:ease-out after:content-[''] after:absolute after:bottom-[2px] 
              after:left-0 after:h-[0.8px] after:w-full after:opacity-0 before:content-[''] before:absolute before:bottom-[5px] before:left-0 before:h-[0.8px] before:w-full
-             before:opacity-0 pc:hover:after:opacity-100 pc:hover:before:opacity-100 pc:hover:before:delay-[400ms] pc:focus:before:opacity-100 pc:focus:after:opacity-0 focus:outline-none focus-within:outline-none ${className}`}
+             before:opacity-0 pc:hover:after:opacity-100 pc:hover:before:opacity-100 pc:hover:before:delay-[400ms] pc:focus:before:opacity-100 pc:focus:after:opacity-0 focus:outline-none focus-within:outline-none 
+             disabled:border-purple-stroke active:before:opacity-100 ${className}`}
         >
             <span className="inline-block transform pc:group-hover:translate-y-[-12%] pc:group-focus:translate-y-[0%] transition duration-[600ms] ease-out">
                 {children.toLowerCase()}
