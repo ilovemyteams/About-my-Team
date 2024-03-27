ad5b0328bd61aa1a82876460ae64394b9c1c3fd1
IraMira333 <irinat333@gmail.com>
2024-03-27 21:50:33 +0200

    Moved import

--- src/app/[locale]/Providers.tsx
+++ src/app/[locale]/Providers.tsx
@@ -1,6 +1,6 @@
+import React from "react";
 import { ProvidersNextUI } from "@/src/providers/ProvidersNextUI";
 import { ThemeProvider } from "@/src/providers/ThemeProvider";
-import React from "react";
 
 export const Providers = ({ children }: { children: React.ReactNode }) => {
     return (
