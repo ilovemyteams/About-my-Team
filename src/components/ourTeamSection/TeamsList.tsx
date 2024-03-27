fac3e3438dfe264097d93746bd62bce38e04db9d
Nataliia Pashchenko <94239256+NataPas2021@users.noreply.github.com>
2024-03-27 21:54:16 +0100

    fix

--- src/components/ourTeamSection/TeamsList.tsx
+++ src/components/ourTeamSection/TeamsList.tsx
@@ -15,7 +15,7 @@ export const TeamsList = () => {
             <ul className="[&>*:first-child]:mt-[12px] [&>*:last-child]:mb-[12px] after:absolute after:border-b-[1px] after:border-purple-stroke after:left-[32px] tab:after:left-0 after:w-[169px]">
                 {portfolioData.map((project, idx) => (
                     <ListItem key={idx}>
-                        {project[locale as LocaleType].name}
+                        {project[locale as LocaleType]?.name}
                     </ListItem>
                 ))}
             </ul>
