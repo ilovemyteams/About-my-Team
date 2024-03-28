048565feb41043a0e7ee3acca182572ec4b042c1
Olena Posternak <101277404+OlenaPosternak@users.noreply.github.com>
2024-03-28 22:06:37 +0200

    Update DesktopHeaderMenu.tsx

--- src/components/header/DesktopHeader/DesktopHeaderMenu.tsx
+++ src/components/header/DesktopHeader/DesktopHeaderMenu.tsx
@@ -22,7 +22,7 @@ export const DesktopHeaderMenu = ({
             className={`${isHeaderMenuOpened ? openedStyles : closedStyles} absolute top-0 left-[80px] deskxl:left-[120px] z-20 h-[100vh] overflow-hidden transition-width duration-[600ms] ease-out
         bg-grey dark:bg-purple-400`}
         >
-            <BackgroundCircles className="left-[-80px] deskxl:left-[120px]" />
+            <BackgroundCircles className="left-[-80px] deskxl:left-[-120px]" />
             <div className="w-80 deskxl:w-120">
                 <div className="flex flex-col justify-between w-80 deskxl:w-120 desk:max-w-[1456px] px-[60px] py-[32px] h-[100vh] mx-auto">
                     <div className="flex justify-end gap-[204px] w-full relative z-20">
