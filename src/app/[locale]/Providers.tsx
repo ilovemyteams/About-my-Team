aa35556f2bc33994a9ff313fe225b1b5b895498a
IraMira333 <irinat333@gmail.com>
2024-03-28 12:11:13 +0200

    Changed name holder of Providers

--- src/app/[locale]/Providers.tsx
+++ src/app/[locale]/Providers.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { ProvidersNextUI } from "@/src/providers/ProvidersNextUI";
-import { ThemeProvider } from "@/src/providers/ThemeProvider";
+import { ProvidersNextUI } from "@/src/components/appProviders/ProvidersNextUI";
+import { ThemeProvider } from "@/src/components/appProviders/ThemeProvider";
 
 export const Providers = ({ children }: { children: React.ReactNode }) => {
     return (
