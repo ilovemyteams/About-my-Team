8321ad7f0739c88e905028e867c56abc0227249d
IraMira333 <irinat333@gmail.com>
2024-03-28 14:36:35 +0200

    Changed Providers.tsx added all code of providers

--- src/app/[locale]/Providers.tsx
+++ src/app/[locale]/Providers.tsx
@@ -1,11 +1,20 @@
-import React from "react";
-import { ProvidersNextUI } from "@/src/components/appProviders/ProvidersNextUI";
-import { ThemeProvider } from "@/src/components/appProviders/ThemeProvider";
+"use client";
+import * as React from "react";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { type ThemeProviderProps } from "next-themes/dist/types";
+import { NextUIProvider } from "@nextui-org/react";
 
-export const Providers = ({ children }: { children: React.ReactNode }) => {
+export const Providers = ({ children, ...props }: ThemeProviderProps) => {
     return (
-        <ProvidersNextUI>
-            <ThemeProvider>{children}</ThemeProvider>
-        </ProvidersNextUI>
+        <NextUIProvider>
+            <NextThemesProvider
+                attribute="class"
+                defaultTheme="system"
+                enableSystem
+                {...props}
+            >
+                {children}
+            </NextThemesProvider>
+        </NextUIProvider>
     );
 };
--- src/components/appProviders/ProvidersNextUI.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-import React from "react";
-import { NextUIProvider } from "@nextui-org/react";
-
-export function ProvidersNextUI({ children }: { children: React.ReactNode }) {
-    return <NextUIProvider>{children}</NextUIProvider>;
-}
