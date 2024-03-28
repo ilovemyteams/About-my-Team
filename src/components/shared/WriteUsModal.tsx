78aca28b16d53e2b5d043b5b6bf4a276451755c3
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2024-03-28 21:26:37 +0200

    added close button

--- src/components/shared/WriteUsModal.tsx
+++ src/components/shared/WriteUsModal.tsx
@@ -2,6 +2,7 @@
 
 import { Modal, ModalContent, useDisclosure } from "@nextui-org/react";
 import { useTranslations } from "next-intl";
+import Image from "next/image";
 import { Button } from "./Button";
 import { CustomerForm } from "./CustomerForm";
 
@@ -17,11 +18,26 @@ export const WriteUsModal = () => {
                 onOpenChange={onOpenChange}
                 placement="center"
                 shouldBlockScroll={false}
+                hideCloseButton={true}
                 className="min-w-[320px] tab:min-w-[660px] pc:min-w-[750px] h-[792px] tab:h-[789px] pc:h-[813px]
                 px-[16px] tab:px-[24px] pc:px-[60px] py-[64px] pc:py-[72px] overflow-y-auto tab:overflow-y-visible
                 bg-purple-400"
             >
-                <ModalContent className="h-full m-0 ">
+                <ModalContent className="h-full m-0">
+                    <button
+                        type="button"
+                        onClick={onClose}
+                        className="absolute top-2 right-4 pc:top-3 pc:right-3 h-12 w-12 p-3 bg-transparent icon-hover-rounded-purple
+                         icon-hover-rounded-purple:hover icon-hover-rounded-purple:focus-visible icon-hover-rounded-purple:focus-within
+                         icon-hover-rounded-purple:active"
+                    >
+                        <Image
+                            src="/images/closeX.svg"
+                            alt="Close button"
+                            width={24}
+                            height={24}
+                        />
+                    </button>
                     <CustomerForm onClose={onClose} />
                 </ModalContent>
             </Modal>
