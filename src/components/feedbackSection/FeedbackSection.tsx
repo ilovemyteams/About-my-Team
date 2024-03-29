86ab3f25cb5079dccd4489c06c1d2ab9bc698c17
IraMira333 <irinat333@gmail.com>
2024-03-29 22:47:52 +0200

    Added SliderForTablet

--- src/components/feedbackSection/FeedbackSection.tsx
+++ src/components/feedbackSection/FeedbackSection.tsx
@@ -4,13 +4,22 @@ import { FeedbackTitle } from "./Title/FeedbackTitle";
 import { feedbackData } from "./feedbackData";
 import { FeedbackSlider } from "./FeedbackSlider";
 import { SharePopover } from "../shared/SharePopover";
+import { FeedbackSliderFromTab } from "./FeedbackSliderFromTab";
 
 export const FeedbackSection = () => {
     const OPTIONS: EmblaOptionsType = { loop: true, align: "start" };
     return (
         <Section id="feedback" className="relative">
             <FeedbackTitle />
-            <FeedbackSlider feedbacks={feedbackData} options={OPTIONS} />
+            <div className="tab:hidden">
+                <FeedbackSlider feedbacks={feedbackData} options={OPTIONS} />
+            </div>
+            <div className="hidden tab:block">
+                <FeedbackSliderFromTab
+                    feedbacks={feedbackData}
+                    options={OPTIONS}
+                />
+            </div>
             <SharePopover
                 className="absolute -top-3 right-4 tab:right-6 tab:top-[414px] tab:items-end pc:right-[60px] pc:top-[526px]"
                 trigerShowShareText={false}
