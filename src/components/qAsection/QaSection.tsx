0a60c338fca17c2b7d302c7f475ff255b21348a5
Nataliia Pashchenko <94239256+NataPas2021@users.noreply.github.com>
2024-04-03 16:12:22 +0200

    fix

--- src/components/qAsection/QaSection.tsx
+++ src/components/qAsection/QaSection.tsx
@@ -6,10 +6,10 @@ import { SharePopover } from "../shared/SharePopover";
 export const QaSection = () => {
     return (
         <Section className="relative">
-            <div className="flex">
+            <div className="flex tab:justify-between pc:items-end">
                 <TitleQA />
                 <SharePopover
-                    className="absolute -top-3 right-4 tab:top-0 tab:right-4 pc:right-[60px] pc:top-[25px] deskxl:top-[76px]"
+                    className="absolute -top-3 right-4 tab:top-0 tab:right-4 pc:right-[60px] deskxl:top-[25px] "
                     trigerShowShareText={true}
                 />
             </div>
--- src/components/qAsection/TitleQa/TitleQA.tsx
+++ src/components/qAsection/TitleQa/TitleQA.tsx
@@ -6,7 +6,7 @@ import { MobileBigTitleQa } from "./MobileBigTitleQa";
 export const TitleQA = () => {
     const getTranslation = useTranslations("Q&A");
     return (
-        <TitleWrapper className="flex flex-col pc:justify-start pc:items-center deskxl:mt-[35px]">
+        <TitleWrapper className="flex flex-col pc:justify-start pc:items-center">
             <div className="flex flex-col tab:flex-row justify-between items-start tab:items-center pc:flex-row">
                 <SmallPageTitle className="px-[4px] mt-[2px] ml-0 mx-auto mr-[10px] mb-[10px] tab:mr-3 flex  ">
                     {getTranslation("pageTitle")}
