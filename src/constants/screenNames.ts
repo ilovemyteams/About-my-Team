80926b9a81ec06f3edd47f69ce94791c72ddc50c
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2024-04-04 12:54:50 +0300

    added use screen size hook

---
+++ src/constants/screenNames.ts
@@ -0,0 +1,7 @@
+export const SCREEN_NAMES = {
+    mobileName: "mobile",
+    tabletName: "tablet",
+    pcName: "pc",
+    desktopName: "desktop",
+    desktopXlName: "desktopXl",
+};
---
+++ src/constants/screenSizes.ts
@@ -0,0 +1,6 @@
+export const SCREEN_SIZES = {
+    tablet: 768,
+    pc: 1280,
+    desktop: 1536,
+    desktopXl: 1920,
+};
