db55e205ac844c1474b479b34f1cf177733f58bf
Olena Posternak <101277404+OlenaPosternak@users.noreply.github.com>
2024-04-09 10:27:42 +0300

    Update basic styles
    Update basic styles

--- src/components/ourTeamSection/SocialLinkSmallMemberCard.tsx
+++ src/components/ourTeamSection/SocialLinkSmallMemberCard.tsx
@@ -16,7 +16,7 @@ export const SocialLinkSmallMemberCard = ({
             href={url}
             target="_blank"
             rel="noopener noreferrer"
-            className="relative cursor-pointer flex justify-center items-center w-[48px] h-[48px] p-3 pc:p-2
+            className="relative cursor-pointer flex justify-center items-center w-[48px] h-[48px] p-3 pc:p-3
             icon-hover-rounded-purple icon-hover-rounded-purple:hover icon-hover-rounded-purple:focus-visible icon-hover-rounded-purple:focus-within
             icon-hover-rounded-purple:active"
         >
