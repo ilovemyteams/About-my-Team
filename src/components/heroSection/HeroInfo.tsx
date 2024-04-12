ab6e07390030e960b1da7e3f49951c0327b9ae3c
IraMira333 <irinat333@gmail.com>
2024-04-12 14:28:52 +0200

    Added h1 to HeroInfo

--- src/components/heroSection/HeroInfo.tsx
+++ src/components/heroSection/HeroInfo.tsx
@@ -7,12 +7,12 @@ export const HeroInfo = () => {
 
     return (
         <div className="flex flex-col items-center pt-4 tab:pt-0 pc:block pc:w-[50%] pc:pt-6">
-            <p
+            <h1
                 className="text-xl pc:mb-[154px] pc:static
                            tab:absolute tab:left-0 tab:top-0 deskxl:text-2xl deskxl:mb-[173px]"
             >
                 {getTranslation("Hero.heroInfoText")}
-            </p>
+            </h1>
             <div className="flex flex-col gap-4 tab:mt-[86px] pc:gap-6 mt-8 pc:mt-[96px]">
                 <WriteUs />
                 <JoinUsButton />
