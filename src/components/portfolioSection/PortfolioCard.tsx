b804c4ba4f5f9141f18a85535757052e90ec2b99
IraMira333 <irinat333@gmail.com>
2024-04-12 14:56:41 +0200

    Updated styles of PortfolioCard

--- src/components/portfolioSection/PortfolioCard.tsx
+++ src/components/portfolioSection/PortfolioCard.tsx
@@ -26,41 +26,53 @@ export const PortfolioCard = ({
     const siteTypeUp = siteView?.toUpperCase();
     const idFormater = formatNumberWithLeadingZero(projectNumber);
     return (
-        <>
-            <div className="relative min-w-[100%] h-[330px] tab:min-w-[50%] pc:h-[400px]">
-                <Image
-                    src={image}
-                    alt={name}
-                    fill
-                    sizes="min-w-[100%] h-[330px] tab:min-w-[50%] pc:h-[400px]"
-                    className=" object-cover"
-                />
-                <div className="absolute bottom-0 block min-w-full h-[134px] tab:h-[130px] pc:h-[142px] bg-portfolioSectionGradient">
-                    <div className="absolute top-5 left-2 tab:top-[18px] tab:left-4 pc:left-5 pc:top-5">
-                        <h3 className="font-caviar font-bold text-lg tab:text-xl mb-[6px]">
-                            {name}
-                        </h3>
+        <div className="relative min-w-[100%] h-[330px] tab:min-w-[50%] pc:h-[400px]">
+            <Image
+                src={image}
+                alt={name}
+                width={288}
+                height={196}
+                className=" object-cover w-[100%] h-[196px] tab:h-[200px] pc:h-[258px]"
+            />
+            <div className="absolute -bottom-0 block min-w-full h-[134px] tab:h-[130px] pc:h-[142px] bg-portfolioSectionGradient">
+                <div className="absolute top-5 left-2 tab:top-[18px] tab:left-4 pc:left-5 pc:top-5">
+                    <h3 className="font-caviar font-bold text-lg tab:text-xl mb-[6px]">
+                        {name}
+                    </h3>
 
-                        <p className="text-xs tab:text-sm font-normal text-red mb-4 tab:mb-3 pc:mb-[18px]">
-                            {siteTypeUp}
-                        </p>
+                    <p className="text-xs tab:text-sm font-normal text-red mb-4 tab:mb-3 pc:mb-[18px]">
+                        {siteTypeUp}
+                    </p>
 
-                        <div className="flex gap-2">
-                            {implementations.map((implementation, index) => (
-                                <div
-                                    key={index}
-                                    className="text-xs pc:text-base px-1 py-[4.5px] tab:px-2 tab:py-[8.5px] pc:px-3 pc:py-[6px] border border-grey"
-                                >
-                                    {implementation}
-                                </div>
-                            ))}
-                        </div>
+                    <div className="flex gap-2 pc:hidden">
+                        {implementations.map((implementation, index) => {
+                            if (index < 4) {
+                                return (
+                                    <div
+                                        key={index}
+                                        className="text-xs pc:text-base px-1 py-[4.5px] tab:px-2 tab:py-[8.5px] pc:px-3 pc:py-[6px] border border-grey"
+                                    >
+                                        {implementation}
+                                    </div>
+                                );
+                            }
+                        })}
                     </div>
-                    <div className="absolute font-caviar font-bold text-base flex w-[30px] h-7 top-5 right-2 tab:top-4 tab:right-4 pc:right-5 pc:top-5 items-center justify-center">
-                        {idFormater}
+                    <div className="hidden pc:flex gap-2">
+                        {implementations.map((implementation, index) => (
+                            <div
+                                key={index}
+                                className="text-xs pc:text-base px-1 py-[4.5px] tab:px-2 tab:py-[8.5px] pc:px-3 pc:py-[6px] border border-grey"
+                            >
+                                {implementation}
+                            </div>
+                        ))}
                     </div>
                 </div>
+                <div className="absolute font-caviar font-bold text-base flex w-[30px] h-7 top-5 right-2 tab:top-4 tab:right-4 pc:right-5 pc:top-5 items-center justify-center">
+                    {idFormater}
+                </div>
             </div>
-        </>
+        </div>
     );
 };
