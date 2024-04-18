1b28896f7873108a1243ca0b46f8a6738f8b912c
IraMira333 <irinat333@gmail.com>
2024-04-19 02:04:55 +0300

    Update page.tsx

--- src/app/[locale]/member/[slug]/page.tsx
+++ src/app/[locale]/member/[slug]/page.tsx
@@ -1,3 +1,4 @@
+import { Modal } from "@/src/components/Modal/Modal";
 import { MemberCardModalBody } from "@/src/components/ourTeamSection/MemberCardModalBody";
 import { membersData } from "@/src/components/shared/mockedData/membersData";
 import Image from "next/image";
@@ -5,15 +6,17 @@ import Image from "next/image";
 const MemberPage = ({ params: { slug } }: { params: { slug: number } }) => {
     console.log(slug);
     return (
-        <div className="w-full h-[679px] overflow-hidden relative mt-[80px] pt-4">
-            <Image
-                src="/images/bgModalMember/imageModalMemberCardMob.png"
-                alt="BG for modal mobile"
-                fill
-                className="object-cover absolute inset-0 w-[320px] h-[679px] z-4"
-            />
-            <MemberCardModalBody data={membersData[slug]} />
-        </div>
+        <Modal>
+            <div className="w-[320px] h-[679px] overflow-hidden fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-[100]">
+                <Image
+                    src="/images/bgModalMember/imageModalMemberCardMob.png"
+                    alt="BG for modal mobile"
+                    fill
+                    className="object-cover absolute inset-0 w-[320px] h-[679px] "
+                />
+                <MemberCardModalBody data={membersData[slug]} />
+            </div>
+        </Modal>
     );
 };
 
