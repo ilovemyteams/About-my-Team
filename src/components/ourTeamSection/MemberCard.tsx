8e2dfa8b5d4f9769c0f013742b03412dacd7451f
IraMira333 <irinat333@gmail.com>
2024-04-22 22:28:12 +0300

    Update MemberCard.tsx

--- src/components/ourTeamSection/MemberCard.tsx
+++ src/components/ourTeamSection/MemberCard.tsx
@@ -38,7 +38,7 @@ export const MemberCard = ({ data }: MemberCardProps) => {
                 )}
             </div>
 
-            <div className="min-h-[106px] tab:min-h-[59px] flex flex-col justify-between">
+            <div className="min-h-[106px] tab:min-h-[40px] flex flex-col justify-between">
                 <h4 className="font-caviar text-baseb text-white pr-2">
                     {data[locale as LocaleType]?.name}
                 </h4>
