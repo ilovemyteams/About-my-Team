bdfb0fa85e8f8c7b44a33b194314aef4e08928c1
IraMira333 <irinat333@gmail.com>
2024-04-23 00:01:27 +0300

    Changed styles for mob & tablet

--- src/components/ourTeamSection/MemberCard.tsx
+++ src/components/ourTeamSection/MemberCard.tsx
@@ -17,12 +17,12 @@ export const MemberCard = ({ data }: MemberCardProps) => {
 
     return (
         <div
-            className="pt-2 pl-[6px] flex flex-col justify-between  
+            className="pt-2 pl-[6px] flex flex-col 
             tab:odd:border-r-[1px] tab:border-purple-stroke 
             tab:[&:nth-child(1)]:border-b-[1px] tab:[&:nth-child(2)]:border-b-[1px]
-            tab:pl-2 tab:aspect-[250/270]"
+            tab:pl-2 relative"
         >
-            <div className="relative min-w-[128px] aspect-[128/150] mr-[6px] overflow-hidden tab:min-w-[234px] tab:aspect-[234/186] tab:mr-2 tab:mb-2 ">
+            <div className="relative min-w-[128px] aspect-[128/150] mr-[6px] overflow-hidden tab:min-w-[234px] tab:aspect-[234/186] tab:mr-2 mb-2 ">
                 {imageURL ? (
                     <Link href={`/member/${id}`}>
                         <Image
@@ -38,21 +38,20 @@ export const MemberCard = ({ data }: MemberCardProps) => {
                 )}
             </div>
 
-            <div className="min-h-[106px] tab:min-h-[40px] flex flex-col justify-between">
-                <h4 className="font-caviar text-baseb text-white pr-2">
+            <div className="flex flex-col justify-between min-h-[144px] tab:min-h-[84px] ">
+                <h4 className="font-caviar text-baseb text-white pr-2 min-h-[60px] tab:min-h-[40px] mb-[14px] tab:mb-1">
                     {data[locale as LocaleType]?.name}
                 </h4>
-            </div>
-            <div className="flex font-geist text-xs justify-between items-center relative">
-                <p className="text-purple-50 mr-12 min-h-12 flex items-center tab:w-full">
+                <p className="text-purple-50 font-geist text-xs mr-12 mb-[10px] tab:w-[76%] tab:mr-0">
                     {position.toUpperCase()}
                 </p>
-                <SocialLinkSmallMemberCard
-                    url={socialLinks.linkedin}
-                    image={"linkedin.svg"}
-                    alt={"Linkedin"}
-                />
             </div>
+
+            <SocialLinkSmallMemberCard
+                url={socialLinks.linkedin}
+                image={"linkedin.svg"}
+                alt={"Linkedin"}
+            />
         </div>
     );
 };
