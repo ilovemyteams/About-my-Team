9323c5d8a2628d5c22854892623eea30cef1b16f
IraMira333 <irinat333@gmail.com>
2024-04-23 00:25:36 +0300

    Update MemberCard.tsx

--- src/components/ourTeamSection/MemberCard.tsx
+++ src/components/ourTeamSection/MemberCard.tsx
@@ -22,7 +22,7 @@ export const MemberCard = ({ data }: MemberCardProps) => {
             tab:[&:nth-child(1)]:border-b-[1px] tab:[&:nth-child(2)]:border-b-[1px]
             tab:pl-2 relative"
         >
-            <div className="relative min-w-[128px] aspect-[128/150] mr-[6px] overflow-hidden tab:min-w-[234px] tab:aspect-[234/186] tab:mr-2 mb-2 ">
+            <div className="relative min-w-[128px] aspect-[128/150] mr-[6px] overflow-hidden tab:min-w-[234px] tab:aspect-[234/186] tab:mr-2 mb-2  ">
                 {imageURL ? (
                     <Link href={`/member/${id}`}>
                         <Image
@@ -38,11 +38,11 @@ export const MemberCard = ({ data }: MemberCardProps) => {
                 )}
             </div>
 
-            <div className="flex flex-col justify-between min-h-[129px] tab:min-h-[84px] ">
+            <div className="flex flex-col justify-between min-h-[129px] tab:min-h-[82px] ">
                 <h4 className="font-caviar text-baseb text-white pr-2 min-h-[60px] tab:min-h-[40px] mb-[14px] tab:mb-1">
                     {data[locale as LocaleType]?.name}
                 </h4>
-                <p className="text-purple-50 font-geist text-xs mr-10 mb-[10px] tab:w-[76%] tab:mr-0">
+                <p className="text-purple-50 font-geist text-xs mr-10 mb-[10px] tab:mb-2 tab:w-[76%] tab:mr-0">
                     {position.toUpperCase()}
                 </p>
             </div>
