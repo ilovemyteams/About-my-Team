963325e32d5b5b979139c3687cd6c392c0dbe095
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2024-04-23 13:13:17 +0300

    Update MenuTeamSection.tsx

--- src/components/ourTeamSection/MenuTeamSection.tsx
+++ src/components/ourTeamSection/MenuTeamSection.tsx
@@ -75,14 +75,14 @@ export const MenuTeamSection = ({
                 />
             </div>
             <div
-                className={`${isOpen ? "max-h-[1280px]" : "max-h-0"} tab:max-h-full tab:h-full bg-purple-400 absolute top-12 z-10 min-w-[288px] max-w-[540px] w-full 
+                className={`${isOpen ? "max-h-[1280px]" : "max-h-0"} tab:max-h-full tab:h-full absolute top-12 z-10 min-w-[288px] max-w-[540px] w-full 
                     transition-[max-height] duration-[300ms] overflow-hidden 
                     tab:static tab:border-[1px] tab:border-purple-stroke tab:w-[27vw] tab:min-w-[208px]
                     `}
             >
                 <div className="py-3 px-4 tab:px-[1.05vw] tab:min-h-[540px]">
-                    <p className="text-sm text-purple-50 pb-3">
-                        {getTranslation("teamsList")}
+                    <p className="text-sm dark:text-purple-50 text-purple-130 pb-3">
+                        {getTranslation("teamsList").toUpperCase()}
                     </p>
                     <ul
                         className="relative flex flex-col gap-[12px] pb-3  text-baseb font-caviar
@@ -99,13 +99,13 @@ export const MenuTeamSection = ({
                                         optionType: "team",
                                     })
                                 }
-                                className={`${selectedOption.optionValue === project.data.id ? "text-red" : "text-grey"} cursor-pointer `}
+                                className={`${selectedOption.optionValue === project.data.id ? "text-red" : ""} cursor-pointer `}
                             >
                                 {project[locale as LocaleType]?.name}
                             </li>
                         ))}
                     </ul>
-                    <p className="text-sm text-purple-50 py-3 ">
+                    <p className="text-sm dark:text-purple-50 text-purple-130 py-3 ">
                         {getTranslation("listTitle").toUpperCase()}
                     </p>
                     <ul className="flex flex-col gap-[12px] text-baseb font-caviar">
@@ -115,7 +115,7 @@ export const MenuTeamSection = ({
                                 onClick={() =>
                                     handleOptionSelectCategory(category)
                                 }
-                                className={`${selectedOption.optionValue === category.categoryName ? "text-red" : "text-grey"} cursor-pointer `}
+                                className={`${selectedOption.optionValue === category.categoryName ? "text-red" : ""} cursor-pointer `}
                             >
                                 {category[locale as LocaleType]}
                             </li>
