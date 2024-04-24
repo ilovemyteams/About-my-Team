fc940de92cd6f0b0e9a696e4a9a7f814db6301b9
IraMira333 <irinat333@gmail.com>
2024-04-24 17:50:42 +0300

    Update CarouselButtons.tsx

--- src/components/shared/SliderComponents/CarouselButtons.tsx
+++ src/components/shared/SliderComponents/CarouselButtons.tsx
@@ -65,7 +65,7 @@ export const PrevButton: React.FC<ButtonType> = props => {
         <button
             aria-label="show previous slide button"
             className={`embla__button--prev flex justify-center items-center w-12 h-12 relative
-            tab:border-t tab:border-b group
+            tab:border-t tab:border-b dark: border-redLight dark:text-red text-redLight group
             icon-hover-rounded-purple icon-hover-rounded-purple:hover icon-hover-rounded-purple:focus-visible icon-hover-rounded-purple:focus-within
             icon-hover-rounded-purple:active ${disabled ? "border-purple-stroke" : "border-red"}`}
             type="button"
