1a80509a88501fca9515b186532767e8a8d0b9ac
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2024-04-25 22:30:25 +0300

    added light theme bg image for desktop

--- src/components/backgroundImages/BackgroundCircles.tsx
+++ src/components/backgroundImages/BackgroundCircles.tsx
@@ -14,21 +14,28 @@ export const BackgroundCircles = ({ className }: BackgroundCirclesProps) => {
                 width="0"
                 height="0"
                 alt="background image circle"
-                className={`absolute left-0 bottom-0 w-[363px] h-[333px] z-[-10]`}
+                className={`hidden dark:block absolute left-0 bottom-0 w-[363px] h-[333px] z-[-10]`}
             />
             <Image
                 src="/images/bgImages/rightBottom.svg"
                 width="0"
                 height="0"
                 alt="background image circle"
-                className={`absolute right-0 bottom-0 w-[970px] h-[427px] z-[-10]`}
+                className={`hidden dark:block absolute right-0 bottom-0 w-[970px] h-[427px] z-[-10]`}
             />
             <Image
                 src="/images/bgImages/rightTop.svg"
                 width="0"
                 height="0"
                 alt="background image circle"
-                className={`absolute right-0 top-0 w-[520px] h-[440px] z-[-10] `}
+                className={`hidden dark:block absolute right-0 top-0 w-[520px] h-[440px] z-[-10] `}
+            />
+            <Image
+                src="/images/bgImages/rightTopLight.svg"
+                width="0"
+                height="0"
+                alt="background image circle"
+                className={`block dark:hidden absolute right-0 top-0 w-[542px] h-[592px] z-[-10] `}
             />
         </div>
     );
