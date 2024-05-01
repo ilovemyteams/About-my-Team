ceb92ebf335533f1eb6bbfae7d480ebb8d0e3439
IraMira333 <irinat333@gmail.com>
2024-05-01 23:22:15 +0300

    Update CarouselButtons.tsx

--- src/components/shared/SliderComponents/CarouselButtons.tsx
+++ src/components/shared/SliderComponents/CarouselButtons.tsx
@@ -65,14 +65,11 @@ export const PrevButton: React.FC<ButtonType> = props => {
         <button
             disabled={disabled}
             aria-label="show previous slide button"
-            className={`embla__button--prev flex justify-center items-center w-12 h-12 relative
+            className="embla__button--prev flex justify-center items-center w-12 h-12 relative
             tab:border-t tab:border-b  group
              icon-hover-rounded-purple:hover icon-hover-rounded-purple:focus-visible icon-hover-rounded-purple:focus-within
-            ${
-                disabled
-                    ? "cursor-default dark:border-purple-stroke dark:text-purple-stroke border-disabledLight text-disabledLight"
-                    : "icon-hover-rounded-purple dark:border-red border-redLight dark:text-red text-redLight group"
-            }`}
+            disabled:dark:border-purple-stroke disabled:dark:text-purple-stroke disabled:border-disabledLight disabled:text-disabledLight
+            icon-hover-rounded-purple dark:border-red border-redLight dark:text-red text-redLight group"
             type="button"
             {...restProps}
         >
@@ -88,15 +85,12 @@ export const NextButton: React.FC<ButtonType> = props => {
         <button
             disabled={disabled}
             aria-label="show next slide button"
-            className={`embla__button--prev flex justify-center items-center w-12 h-12 rotate-180
+            className="embla__button--prev flex justify-center items-center w-12 h-12 rotate-180
             tab:border-t tab:border-b  group
            icon-hover-rounded-purple:hover icon-hover-rounded-purple:focus-visible icon-hover-rounded-purple:focus-within
             icon-hover-rounded-purple:active 
-            ${
-                disabled
-                    ? "cursor-default dark:border-purple-stroke dark:text-purple-stroke border-disabledLight text-disabledLight"
-                    : "icon-hover-rounded-purple dark:border-red border-redLight dark:text-red text-redLight group"
-            }`}
+            disabled:dark:border-purple-stroke disabled:dark:text-purple-stroke disabled:border-disabledLight disabled:text-disabledLight
+            icon-hover-rounded-purple dark:border-red border-redLight dark:text-red text-redLight group"
             type="button"
             {...restProps}
         >
