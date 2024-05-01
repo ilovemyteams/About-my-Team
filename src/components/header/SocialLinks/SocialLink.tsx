2d5e38d242764d7008e306d2a0838447e893792a
IraMira333 <irinat333@gmail.com>
2024-05-02 01:01:04 +0300

    I rewrote the code without .map

--- src/components/header/SocialLinks/SocialLink.tsx
+++ src/components/header/SocialLinks/SocialLink.tsx
@@ -3,17 +3,18 @@ import { ReactNode } from "react";
 interface SocialLinkProps {
     url: string;
     image: ReactNode;
+    classname?: string;
 }
 
-export const SocialLink = ({ url, image }: SocialLinkProps) => {
+export const SocialLink = ({ url, image, classname }: SocialLinkProps) => {
     return (
         <a
             href={url}
             target="_blank"
             rel="noopener noreferrer"
-            className="relative cursor-pointer flex justify-center items-center w-[48px] h-[48px] p-3 pc:p-2 dark:text-purple-50 text-purple-130
+            className={`relative cursor-pointer flex justify-center items-center w-[48px] h-[48px] p-3 pc:p-2 dark:text-purple-50 text-purple-130
             icon-hover-rounded-purple icon-hover-rounded-purple:hover icon-hover-rounded-purple:focus-visible icon-hover-rounded-purple:focus-within
-            icon-hover-rounded-purple:active"
+            icon-hover-rounded-purple:active ${classname}`}
         >
             {image}
         </a>
