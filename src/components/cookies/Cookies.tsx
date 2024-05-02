d6b73814b1907f46032ee3b3a3a4f59edb6a2163
Olena Posternak <101277404+OlenaPosternak@users.noreply.github.com>
2024-05-02 13:13:56 +0300

    Update Cookies.tsx
    update styles

--- src/components/cookies/Cookies.tsx
+++ src/components/cookies/Cookies.tsx
@@ -28,10 +28,10 @@ export const CookiesComponent = () => {
     return (
         <div
             ref={cookiesRef}
-            className={`${isAccepted ? "h-0 " : "h-[292px] tab:h-[171px] pc:h-[212px] "} w-full transition-[height] duration-[2000ms] dark:bg-CTAGradient bg-CTAGradientLight  overflow-hidden linear  fixed z-[11]  bottom-0 left-0 flex  pc:pl-[140px]`}
+            className={`${isAccepted ? "h-0 " : "h-[292px] tab:h-[212px] pc:h-[171px] "} w-full transition-[height] duration-[2000ms] dark:bg-CTAGradient bg-CTAGradientLight  overflow-hidden linear  fixed z-[11]  bottom-0 left-0 flex  pc:pl-[80px] deskxl:pl-[120px]`}
         >
-            <div className="px-4 py-10 pc:px-6 pc:py-10 flex flex-col gap-6 items-center tab:items-start justify-center max-w-[540px] tab:tab:max-w-full mx-auto">
-                <p>
+            <div className="px-4 py-10 tab:px-6 pc:px-[100px]  pc:py-10 flex flex-col pc:flex-row gap-6 pc:gap-9 items-center tab:items-start pc:items-center justify-center max-w-[540px] tab:tab:max-w-full mx-auto">
+                <div className="pc:w-[60%]">
                     {getTranslation("text")}{" "}
                     <a
                         href="/"
@@ -41,10 +41,12 @@ export const CookiesComponent = () => {
                     >
                         {getTranslation("policy")}
                     </a>
-                </p>
-                <Button onClick={handleClick}>
-                    {getTranslation("button")}
-                </Button>
+                </div>
+                <div className="w-[288px]">
+                    <Button onClick={handleClick}>
+                        {getTranslation("button")}
+                    </Button>
+                </div>
             </div>
         </div>
     );
