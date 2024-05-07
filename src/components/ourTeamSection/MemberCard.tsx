ea74df931d7f94e77761a00a97f0b76d285f4579
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2024-05-07 11:22:44 +0300

    updated member card styles

--- src/components/ourTeamSection/MemberCard.tsx
+++ src/components/ourTeamSection/MemberCard.tsx
@@ -18,11 +18,11 @@ export const MemberCard = ({ data }: MemberCardProps) => {
 
     return (
         <div
-            className="flex flex-col justify-between pt-2 pc:pt-3 pl-[6px] tab:pl-2 pc:pl-3 tab:border-purple-stroke 
+            className="flex flex-col justify-between pt-2 pc:pt-3 pl-[6px] tab:pl-2 pc:pl-3 tab:border-purple-strokeLight  dark:tab:border-purple-stroke 
             tab:aspect-[250/270] pc:aspect-[270/292] 
             tab:odd:border-r tab:[&:nth-child(1)]:border-b tab:[&:nth-child(2)]:border-b 
             pc:[&:nth-child(3)]:border-b pc:border-r pc:[&:nth-child(3)]:border-r-transparent pc:[&:nth-child(6)]:border-r-transparent
-            deskxl:border-r deskxl:[&:nth-child(4)]:border-r-transparent deskxl:[&:nth-child(8)]:border-r-transparent deskxl:[&:nth-child(3)]:border-r-purple-stroke deskxl:[&:nth-child(6)]:border-r-purple-stroke deskxl:[&:nth-child(4)]:border-b"
+            deskxl:border-r deskxl:[&:nth-child(4)]:border-r-transparent deskxl:[&:nth-child(8)]:border-r-transparent deskxl:[&:nth-child(3)]:border-r-purple-strokeLight dark:deskxl:[&:nth-child(3)]:border-r-purple-stroke deskxl:[&:nth-child(6)]:border-r-purple-strokeLight dark:deskxl:[&:nth-child(6)]:border-r-purple-stroke deskxl:[&:nth-child(4)]:border-b"
         >
             <div className="relative min-w-[128px] aspect-[128/150] mr-[6px] overflow-hidden tab:min-w-[234px] tab:aspect-[234/186] pc:aspect-[246/196] tab:mr-2 pc:mr-3 mb-2 pc:mb-3 ">
                 <Link href={`/${locale}/member/${id}`}>
@@ -57,11 +57,11 @@ export const MemberCard = ({ data }: MemberCardProps) => {
             </div>
 
             <div className="min-h-[100px] tab:min-h-[59px] pc:min-h-[72px] flex flex-col justify-between content-between">
-                <h4 className="font-caviar text-baseb text-white-200 pr-2">
+                <h4 className="font-caviar text-baseb text-purple-200 dark:text-white-200 pr-2">
                     {data[locale as LocaleType]?.name}
                 </h4>
                 <div className="flex font-geist text-xs justify-between items-center relative">
-                    <p className="text-purple-50 mr-12 min-h-12 flex items-center tab:w-full">
+                    <p className="text-purple-130 dark:text-purple-50 mr-12 min-h-12 flex items-center tab:w-full">
                         {position.toUpperCase()}
                     </p>
                     <SocialLink
