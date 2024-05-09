afb05e1a8fca85edfcf5b25cb2154e00b4cb4395
IraMira333 <irinat333@gmail.com>
2024-05-09 23:28:57 +0300

    Create IconGoogleDrive.tsx

---
+++ src/components/shared/Icons/IconGoogleDrive.tsx
@@ -0,0 +1,20 @@
+interface iconGoogleDrive {
+    className?: string;
+}
+
+export const IconGoogleDrive = ({ className }: iconGoogleDrive) => {
+    return (
+        <svg
+            width="20"
+            height="20"
+            xmlns="http://www.w3.org/2000/svg"
+            className={className}
+            viewBox="0 0 1024 1024"
+        >
+            <path
+                d="M438.016 640 253.408 960h580.608l184.608-320zm554.368-64L696.768 64h-369.6l295.616 512zM290.208 128 0 630.784l184.8 320 290.208-502.816z"
+                fill="currentColor"
+            />
+        </svg>
+    );
+};
