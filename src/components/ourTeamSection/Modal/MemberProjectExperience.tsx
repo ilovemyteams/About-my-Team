8f06b336f7eef86c51ee43b9d2e87c8d1a7c71f0
IraMira333 <irinat333@gmail.com>
2024-05-10 19:25:37 +0300

    Adde underline for projects in Modal

--- src/components/ourTeamSection/Modal/MemberProjectExperience.tsx
+++ src/components/ourTeamSection/Modal/MemberProjectExperience.tsx
@@ -14,12 +14,15 @@ export const MemberProjectExperience = ({
             {projectsExperience.slice(0, 3).map((project, index) => (
                 <li
                     key={index}
-                    className="px-0 truncate w-[230px] tab:w-[442px] dark:pc:hover:text-red pc:hover:text-redLight dark:pc:focus:text-red pc:focus:text-redLight pc:transition pc:ease-out pc:duration-300 dark:active:text-red active:text-redLight"
+                    className="px-0 truncate w-[230px] tab:w-[442px]"
                 >
                     <a
                         target="_blank"
                         href={addProtocol(project)}
-                        className="text-sm ml-4 tab:text-base tab:ml-2"
+                        className="text-sm ml-4 tab:text-base tab:ml-2 border-b dark:border-grey border-greyLight dark:pc:hover:text-red dark:pc:hover:border-red pc:hover:border-redLight
+                        pc:hover:text-redLight dark:active:text-red active:text-redLight dark:active:border-red active:border-redLight 
+                        dark:pc:focus:text-red pc:focus:text-redLight dark:pc:focus:border-red pc:focus:border-redLight font-caviar text-lg outline-none
+                        transition-color ease-out duration-300"
                     >
                         {project}
                     </a>
