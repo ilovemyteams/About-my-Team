34be6db327f3519cea527e01c0864f9a48d100fa
IraMira333 <irinat333@gmail.com>
2024-05-17 11:19:35 +0300

    Fixed URL

--- src/components/ourTeamSection/Modal/MemberProjectExperience.tsx
+++ src/components/ourTeamSection/Modal/MemberProjectExperience.tsx
@@ -1,14 +1,10 @@
+import { addProtocol } from "@/src/utils/addProtocol";
+
 export const MemberProjectExperience = ({
     projectsExperience,
 }: {
     projectsExperience: string[];
 }) => {
-    const addProtocol = (url: string) => {
-        if (!/^https?:\/\//i.test(url)) {
-            return `https://${url}`;
-        }
-        return url;
-    };
     return (
         <ul>
             {projectsExperience.slice(0, 3).map((project, index) => (
