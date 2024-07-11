b9465d1b8fca5c2bf645078a6f16dadaf1833c7b
IraMira333 <irinat333@gmail.com>
2024-07-11 21:23:08 +0300

    Added router.back()

--- src/components/ourTeamSection/Modal/Modal.tsx
+++ src/components/ourTeamSection/Modal/Modal.tsx
@@ -2,7 +2,6 @@
 import React from "react";
 import { useRouter, usePathname } from "next/navigation";
 import { useLocale } from "next-intl";
-import Link from "next/link";
 import { BgImagesDesktop } from "../../shared/WriteUs/modalBgImages/writeUsBgImages/BgImagesDesktop";
 import { BgImagesMobile } from "../../shared/WriteUs/modalBgImages/writeUsBgImages/BgImagesMobile";
 import { BgImagesTablet } from "../../shared/WriteUs/modalBgImages/writeUsBgImages/BgImagesTablet";
@@ -16,33 +15,35 @@ export const Modal = ({ children }: { children: React.ReactNode }) => {
 
     return (
         <div>
-            <Link
-                href={`/${locale}#team`}
-                className="w-full h-full bg-greyLight bg-opacity-70 dark:bg-backdrop dark:bg-opacity-80 fixed top-0 left-0 z-[20] no-doc-scroll"
-                scroll={false}
-            />
             <div
-                className="p-0 min-w-[320px] w-[90vw] max-w-[360px] tab:min-w-[768px] tab:w-[768px] h-auto max-h-[95vh] overflow-y-auto fixed top-1/2 left-1/2 
-            -translate-x-1/2 -translate-y-1/2 z-[21] bg-white-100 dark:bg-purple-400"
+                onClick={() => {
+                    router.back();
+                }}
+                className="w-full h-full bg-greyLight bg-opacity-70 dark:bg-backdrop dark:bg-opacity-80 fixed top-0 left-0 z-[20] no-doc-scroll"
             >
-                <div className="h-auto relative pb-[64px] pt-2 tab:pb-[72px] tab:pt-3">
-                    <BgImagesMobile />
-                    <BgImagesTablet />
-                    <BgImagesDesktop />
-                    <button
-                        onClick={() => {
-                            router.push(`/${locale}#team`);
-                        }}
-                        onTouchStart={() => {
-                            router.push(`/${locale}#team`);
-                        }}
-                        type="button"
-                        className="flex mb-2 ml-auto mr-4 tab:mr-6 tab:mb-3 w-12 h-12 justify-center items-center 
+                <div
+                    className="p-0 min-w-[320px] w-[90vw] max-w-[360px] tab:min-w-[768px] tab:w-[768px] h-auto max-h-[95vh] overflow-y-auto fixed top-1/2 left-1/2 
+            -translate-x-1/2 -translate-y-1/2 z-[21] bg-white-100 dark:bg-purple-400"
+                >
+                    <div className="h-auto relative pb-[64px] pt-2 tab:pb-[72px] tab:pt-3">
+                        <BgImagesMobile />
+                        <BgImagesTablet />
+                        <BgImagesDesktop />
+                        <button
+                            onClick={() => {
+                                router.back();
+                            }}
+                            onTouchStart={() => {
+                                router.back();
+                            }}
+                            type="button"
+                            className="flex mb-2 ml-auto mr-4 tab:mr-6 tab:mb-3 w-12 h-12 justify-center items-center 
                     icon-hover-rounded-purple relative"
-                    >
-                        <IconCloseX />
-                    </button>
-                    {children}
+                        >
+                            <IconCloseX />
+                        </button>
+                        {children}
+                    </div>
                 </div>
             </div>
         </div>
