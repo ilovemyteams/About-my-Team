e03cbc758567209540f8dfaa9d7c639c3cfd642f
IraMira333 <irinat333@gmail.com>
2024-07-12 22:39:09 +0300

    Update Modal.tsx

--- src/components/ourTeamSection/Modal/Modal.tsx
+++ src/components/ourTeamSection/Modal/Modal.tsx
@@ -17,13 +17,10 @@ export const Modal = ({ children }: { children: React.ReactNode }) => {
 
     return (
         <div>
-
             <div
                 onClick={() => router.push(previousURL)}
                 className="w-full h-full bg-greyLight bg-opacity-70 dark:bg-backdrop dark:bg-opacity-80 fixed top-0 left-0 z-[20] no-doc-scroll"
- 
             >
-
                 <div
                     className="p-0 min-w-[320px] w-[90vw] max-w-[360px] tab:min-w-[768px] tab:w-[768px] h-auto max-h-[95vh] overflow-y-auto fixed top-1/2 left-1/2 
             -translate-x-1/2 -translate-y-1/2 z-[21] bg-white-100 dark:bg-purple-400"
@@ -34,9 +31,9 @@ export const Modal = ({ children }: { children: React.ReactNode }) => {
                         <BgImagesDesktop />
                         <button
                             onClick={() => {
-                            router.push(previousURL);
-                        }}
-                        onTouchStart={() => router.push(previousURL)}
+                                router.push(previousURL);
+                            }}
+                            onTouchStart={() => router.push(previousURL)}
                             type="button"
                             className="flex mb-2 ml-auto mr-4 tab:mr-6 tab:mb-3 w-12 h-12 justify-center items-center 
 
