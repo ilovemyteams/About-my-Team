9f153c98b48123dda9f2e91cccbbf0bcf92980b5
IraMira333 <irinat333@gmail.com>
2024-07-29 12:49:03 +0300

    Update policyURL.ts

--- src/constants/policyURL.ts
+++ src/constants/policyURL.ts
@@ -2,4 +2,4 @@ export const POLICY_URL_UA =
     "https://docs.google.com/document/d/1OdqkzzgBu96xJ5nc66WbIXSEKytWRTG2vWRRFy5IUb8/edit";
 
 export const POLICY_URL_EN =
-    "https://docs.google.com/document/d/1Ljr60hQ45cP8QqqBqgUaMxscl9A2nEd818RF80TiGIw/edit";
+    "https://docs.google.com/document/d/1XHKt6wqYITrhsT1yGCLAD7VwEClmeuM_YKy71Sv6l-A/edit";
