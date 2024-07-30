d67916840a7e58abe41e3f04644463c4a87126cc
IraMira333 <irinat333@gmail.com>
2024-07-30 22:26:33 +0300

    Added pl version of POLICY

--- src/utils/selectedLink.tsx
+++ src/utils/selectedLink.tsx
@@ -1,5 +1,13 @@
-import { POLICY_URL_EN, POLICY_URL_UA } from "../constants/policyURL";
+import {
+    POLICY_URL_EN,
+    POLICY_URL_PL,
+    POLICY_URL_UA,
+} from "../constants/policyURL";
 
 export const selectedLink = (locale: string): string => {
-    return locale === "ua" ? POLICY_URL_UA : POLICY_URL_EN;
+    return locale === "ua"
+        ? POLICY_URL_UA
+        : locale === "en"
+          ? POLICY_URL_EN
+          : POLICY_URL_PL;
 };
