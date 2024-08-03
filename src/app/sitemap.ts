d5390bc500bc5547b549a549c8eba98dd33d18e8
IraMira333 <irinat333@gmail.com>
2024-08-03 23:44:48 +0300

    Update sitemap.ts

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -7,13 +7,6 @@ export default function sitemap(): MetadataRoute.Sitemap {
             lastModified: new Date(),
             changeFrequency: "weekly",
             priority: 1,
-            alternates: {
-                languages: {
-                    en: "https://www.ilovemyteam.online/en",
-                    pl: "https://www.ilovemyteam.online/pl",
-                    uk: "https://www.ilovemyteam.online/ua",
-                },
-            },
         },
     ];
 }
