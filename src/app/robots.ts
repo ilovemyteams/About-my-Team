b1e61b547b55c2c6c05fab37f2a6328005f108c8
IraMira333 <irinat333@gmail.com>
2024-08-08 21:22:01 +0300

    Added disallow: /studio

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -5,7 +5,7 @@ export default function robots(): MetadataRoute.Robots {
         rules: {
             userAgent: "*",
             allow: "/",
-            disallow: "/api",
+            disallow: ["/api", "/studio"],
         },
         sitemap: "https://www.ilovemyteam.online/sitemap.xml",
     };
