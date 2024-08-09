d5491458098a37a75b53f0da785fec693d397f85
IraMira333 <irinat333@gmail.com>
2024-08-09 21:44:04 +0300

    Changed siteUrl in next-sitemap.cofig

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next-sitemap').IConfig} */
 
 module.exports = {
-    siteUrl: process.env.SITE_URL,
+    siteUrl: process.env.NEXT_PUBLIC_BASE_URL,
     generateRobotsTxt: true,
     sitemapSize: 5000,
     changefreq: "monthly",
