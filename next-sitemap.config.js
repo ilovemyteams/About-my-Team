698d7e455275780ebfb7405ebf959a59ab34d9fa
IraMira333 <irinat333@gmail.com>
2024-08-09 13:00:52 +0300

    Update next-sitemap.config.js

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -12,4 +12,14 @@ module.exports = {
             { userAgent: "*", disallow: "/api/*" },
         ],
     },
+    alternateRefs: [
+        {
+            href: `${process.env.NEXT_PUBLIC_BASE_URL}/en`,
+            hreflang: "en",
+        },
+        {
+            href: `${process.env.NEXT_PUBLIC_BASE_URL}/pl`,
+            hreflang: "pl",
+        },
+    ],
 };
