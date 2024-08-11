e8723c25a580b85527b4870f7a96221f91dda145
IraMira333 <irinat333@gmail.com>
2024-08-11 22:45:29 +0300

    Changed a colors of Link

--- src/components/shared/GoToSiteLink.tsx
+++ src/components/shared/GoToSiteLink.tsx
@@ -7,17 +7,13 @@ export const GoToSiteLink = ({ siteLink }: { siteLink: string }) => {
             target="_blank"
             href={addProtocol(siteLink)}
             rel="noopener noreferrer"
-            className="flex items-center gap-2 group dark:pc:hover:text-red 
+            className="flex items-center gap-2  text-purple-130 dark:text-purple-50  dark:pc:hover:text-red 
  pc:hover:text-redLight dark:active:text-red active:text-redLight 
  dark:pc:focus:text-red pc:focus:text-redLight font-caviar text-lg outline-none
  transition-color ease-out duration-300"
         >
             <p className=" hidden pc:block underline ">{siteLink}</p>
-            <IconGoToSite
-                className="text-purple-130 dark:text-purple-50  dark:pc:group-hover:text-red 
- pc:group-hover:text-redLight dark:group-active:text-red group-active:text-redLight 
- dark:pc:group-focus:text-red pc:group-focus:text-redLight "
-            />
+            <IconGoToSite />
         </a>
     );
 };
