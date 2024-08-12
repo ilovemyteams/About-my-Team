913aec5541b476bc5d11365570eb3c60d73380a6
IraMira333 <irinat333@gmail.com>
2024-08-12 14:20:51 +0300

    Update PageContentList.tsx

--- src/components/aboutPage/PageContentList.tsx
+++ src/components/aboutPage/PageContentList.tsx
@@ -9,9 +9,9 @@ export const PageContentList = () => {
         getTranslation("contentItem4"),
     ];
     return (
-        <ul className="flex flex-wrap gap-x-16 gap-y-3 justify-between tab:gap-0 tab:justify-between mt-[40px] tab:mt-[56px] pc:mt-[46px]">
+        <ul className="flex flex-wrap gap-x-1 gap-y-1 justify-between tab:gap-0 tab:justify-between mt-[40px] tab:mt-[56px] pc:mt-[46px]">
             {contentList.map((content, idx) => (
-                <li className="min-w-[100px]" key={idx}>
+                <li key={idx}>
                     <span className="text-xs dark:text-purple-50 text-purple-130">
                         {content.toUpperCase()}
                     </span>
