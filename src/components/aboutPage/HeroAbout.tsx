09cbedf62a0637de218a994e38d49bdd180053f6
IraMira333 <irinat333@gmail.com>
2024-08-12 17:30:59 +0300

    Update HeroAbout.tsx

--- src/components/aboutPage/HeroAbout.tsx
+++ src/components/aboutPage/HeroAbout.tsx
@@ -3,12 +3,24 @@ import { Section } from "../shared/Section";
 import { LinkGoBack } from "../shared/LinkGoBack";
 import { localeInURL } from "@/src/utils/localeInURL";
 import { PageContentList } from "./PageContentList";
+import { BreadcrumbItem, Breadcrumbs } from "@nextui-org/react";
 
 export const HeroAbout = () => {
     const getTranslation = useTranslations("AboutPage");
     const locale = useLocale();
     return (
         <Section className="pt-8 tab:pt-[100px] pc:pt-8">
+            <Breadcrumbs
+                classNames={{ base: "bg-purple-100 h-12" }}
+                itemClasses={{
+                    item: "text-purple-100 text-sm h-12",
+                    separator: "text-purple-100",
+                }}
+                variant="solid"
+            >
+                <BreadcrumbItem>Home</BreadcrumbItem>
+                <BreadcrumbItem>Music</BreadcrumbItem>
+            </Breadcrumbs>
             <div className="flex justify-end">
                 <LinkGoBack linkBack={`/${localeInURL(locale)}#mission`} />
             </div>
