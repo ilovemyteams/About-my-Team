f9ceb8906a33080b808ee1f044737683413f3236
IraMira333 <irinat333@gmail.com>
2024-08-12 13:58:55 +0300

    Added QuoteSection to page

--- src/app/[locale]/about/page.tsx
+++ src/app/[locale]/about/page.tsx
@@ -1,9 +1,11 @@
 import { HeroAbout } from "@/src/components/aboutPage/HeroAbout";
+import { QuoteSection } from "@/src/components/aboutPage/QuoteSection";
 
 export default function Home() {
     return (
         <>
             <HeroAbout />
+            <QuoteSection />
         </>
     );
 }
