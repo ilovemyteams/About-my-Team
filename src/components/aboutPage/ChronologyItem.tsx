a8f498c0a4bd1feaa30cf95555cee75cf47d822f
IraMira333 <irinat333@gmail.com>
2024-08-13 13:59:39 +0300

    Create ChronologyItem.tsx

---
+++ src/components/aboutPage/ChronologyItem.tsx
@@ -0,0 +1,24 @@
+import { ChronologyDataTypeItem } from "@/src/mockedData/chronologyData";
+import { LocaleType } from "@/types/LocaleType";
+import { useLocale } from "next-intl";
+
+interface ChronologyItemProp {
+    data: ChronologyDataTypeItem;
+}
+
+export const ChronologyItem = ({ data }: ChronologyItemProp) => {
+    const locale = useLocale();
+    return (
+        <>
+            <div className="flex font-caviar items-center justify-between">
+                <h5 className="text-xlb">
+                    {data[locale as LocaleType]?.title}
+                </h5>
+                <p className="text-[36px] leading-[45px] font-bold text-purple-dark">
+                    {data[locale as LocaleType]?.date}
+                </p>
+            </div>
+            <p className="text-base">{data[locale as LocaleType]?.text}</p>
+        </>
+    );
+};
