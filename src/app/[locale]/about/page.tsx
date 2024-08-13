bdc483c6fed9006f70d937db2d4dde8c107458d8
IraMira333 <irinat333@gmail.com>
2024-08-13 13:59:08 +0300

    Added new components

--- src/app/[locale]/about/page.tsx
+++ src/app/[locale]/about/page.tsx
@@ -1,8 +1,10 @@
 import { AboutTheFounderSection } from "@/src/components/aboutPage/AboutTheFounderSection";
+import { ChronologySection } from "@/src/components/aboutPage/ChronologySection";
 import { HeroAbout } from "@/src/components/aboutPage/HeroAbout";
 import { PresentationSection } from "@/src/components/aboutPage/PresentationSection";
 import { QuoteSection } from "@/src/components/aboutPage/QuoteSection";
 import { TeamSection } from "@/src/components/aboutPage/TeamSection";
+import { JoinTheTeamSection } from "@/src/components/joinTheTeamSection/JoinTheTeamSection";
 import { WriteUsSection } from "@/src/components/writeUsSection/WriteUsSection";
 
 export default function Home() {
@@ -11,7 +13,9 @@ export default function Home() {
             <HeroAbout />
             <QuoteSection />
             <AboutTheFounderSection />
+            <ChronologySection />
             <TeamSection />
+            <JoinTheTeamSection />
             <PresentationSection />
             <WriteUsSection />
         </>
