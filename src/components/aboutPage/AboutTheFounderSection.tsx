fed133e5ee9fd279b712f6aad969699c9defe85c
IraMira333 <irinat333@gmail.com>
2024-08-15 22:31:38 +0300

    Updated styles scrollBox for pc

--- src/components/aboutPage/AboutTheFounderSection.tsx
+++ src/components/aboutPage/AboutTheFounderSection.tsx
@@ -1,9 +1,9 @@
+import { ScrollShadow } from "@nextui-org/react";
 import Image from "next/image";
 import { useTranslations } from "next-intl";
 
 import { IconHeartWeb } from "../shared/Icons/IconHeartWeb";
 import { Section } from "../shared/Section";
-import { ScrollShadow } from "@nextui-org/react";
 
 const SUSANNAS_PHOTO_LINK =
     "https://drive.google.com/uc?export=view&id=1j2sHd4yuBnbgXsl-lZsZypeu-mKlruVI";
@@ -20,18 +20,21 @@ export const AboutTheFounderSection = () => {
                 </span>
             </h2>
             <div className="tab:flex tab:gap-10 pc:gap-[90px] tab:mb-6 pc:mb-10 ">
-                <div className="tab:min-w-[320px]  w-full tab:aspect-[320/364] pc:aspect-[246/196]">
+                <div className="tab:min-w-[322px]  w-[44.7%] tab:aspect-[322/364] pc:aspect-[449/503]">
                     <Image
                         src={SUSANNAS_PHOTO_LINK}
                         alt={getTranslation("aboutOwnerPhotoAlt")}
                         width={449}
                         height={503}
-                        className="mb-6 mx-auto tab:mx-0 tab:mb-0 object-cover tab:min-w-[320px]  w-full 
-                                tab:aspect-[320/364] pc:aspect-[246/196]"
+                        className="mb-6 mx-auto tab:mx-0 tab:mb-0 object-cover tab:min-w-[322px]  w-full 
+                                tab:aspect-[322/364] pc:aspect-[449/503] pc:min-w-[449px]"
                     />
                 </div>
-                <div className="tab:min-h-[360px] tab:max-h-[486px] aspect-[360/364] object-cover border-y dark:border-purple-stroke border-purple-strokeLight">
-                    <ScrollShadow className="tab:py-3 tab:min-h-[360px] tab:max-h-[486px] aspect-[360/364] object-cover  text-sm tab:text-base tab:min-w-[360px] tab:w-[47vw] pc:text-xl  tab:overflow-scroll">
+                <div className="tab:w-[50%] tab:min-h-[360px] tab:aspect-[360/364] pc:min-h-[503px] pc:aspect-[539/503] tab:object-cover tab:border-y dark:border-purple-stroke border-purple-strokeLight">
+                    <ScrollShadow
+                        hideScrollBar
+                        className="tab:py-4 pc:py-5 tab:min-h-[360px]  tab:aspect-[360/364] pc:min-h-[503px] pc:max-w-[668px] pc:aspect-[539/503] tab:object-cover w-full text-sm tab:text-base tab:min-w-[360px] pc:min-w-[539px]  pc:w-[42vw] pc:text-xl  tab:overflow-scroll"
+                    >
                         <p className="mb-6 pc:mb-10">
                             {getTranslation("aboutOwnerP1")}
                         </p>
