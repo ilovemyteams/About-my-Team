7b0838eeb07fc9fba900dcd8fa881dd42be4aff1
IraMira333 <irinat333@gmail.com>
2024-08-17 22:46:09 +0300

    Changed site navigation

--- src/components/footer/MenuFooter.tsx
+++ src/components/footer/MenuFooter.tsx
@@ -1,32 +1,25 @@
 import Link from "next/link";
-import { useTranslations } from "next-intl";
-import { useLocale } from "next-intl";
+import { useLocale, useTranslations } from "next-intl";
 import { localeInURL } from "@/src/utils/localeInURL";
 
 export const MenuFooter = () => {
-    const getTranslation = useTranslations();
+    const locale = useLocale();
+    const getTranslation = useTranslations("Breadcrumbs");
 
     const menuList = [
-        { name: getTranslation("Mission.pageTitle"), id: "mission" },
-        { name: getTranslation("Portfolio.pageTitle"), id: "portfolio" },
-        { name: getTranslation("Feedback.pageTitle"), id: "feedback" },
-        { name: getTranslation("Services.pageTitle"), id: "services" },
-        { name: getTranslation("OurTeam.pageTitle"), id: "team" },
-        { name: getTranslation("Stages.pageTitle"), id: "stages" },
-        { name: getTranslation("Q&A.pageTitle").toUpperCase(), id: "faq" },
+        { name: getTranslation("breadcrumbItemHome"), path: "" },
+        { name: getTranslation("breadcrumbItemAbout"), path: "about" },
     ];
-
-    const locale = useLocale();
     return (
         <ul className="tab:flex flex-start tab:gap-[20px] pc:gap-[28px] font-geist text-base deskxl:text-lg">
-            {menuList.map(({ name, id }, idx) => (
+            {menuList.map(({ name, path }, idx) => (
                 <li
                     key={idx}
                     className=" pc:my-0 pc:py-0.5 pc:p-[2px] leading-6 tracking-normal text-left"
                 >
                     <div className="py-[10px] pl-1 tab:py-0">
                         <Link
-                            href={`/${localeInURL(locale)}#${id}`}
+                            href={`/${locale}/${path}`}
                             className="text-base deskxl:text-lg font-normal font-geist outline-none text-purple-200 dark:text-grey dark:pc:hover:text-red pc:hover:text-redLight 
                             dark:pc:focus:text-red pc:focus:text-redLight dark:active:text-red active:text-redLight transition-color ease-out duration-300"
                         >
---
+++ src/components/heroSection/MainPageNavList.tsx
@@ -0,0 +1,32 @@
+import { localeInURL } from "@/src/utils/localeInURL";
+import { useLocale, useTranslations } from "next-intl";
+import Link from "next/link";
+
+export const MainPageNavList = () => {
+    const getTranslation = useTranslations();
+    const navList = [
+        { name: getTranslation("Mission.pageTitle"), id: "mission" },
+        { name: getTranslation("Portfolio.pageTitle"), id: "portfolio" },
+        { name: getTranslation("Feedback.pageTitle"), id: "feedback" },
+        { name: getTranslation("Services.pageTitle"), id: "services" },
+        { name: getTranslation("OurTeam.pageTitle"), id: "team" },
+        { name: getTranslation("Stages.pageTitle"), id: "stages" },
+        { name: getTranslation("Q&A.pageTitle"), id: "faq" },
+    ];
+    const locale = useLocale();
+    return (
+        <ul className="flex flex-wrap gap-y-4 gap-x-[18%] justify-center mt-[40px] tab:mt-[56px] pc:mt-[46px] tab:gap-4">
+            {navList.map((content, idx) => (
+                <li key={idx}>
+                    <Link
+                        href={`/${locale}/about#${content.id}`}
+                        className="text-xs dark:text-purple-50 text-purple-130 dark:pc:hover:text-red pc:hover:text-redLight
+                dark:pc:focus:text-red pc:focus:text-redLight pc:transition pc:ease-out pc:duration-300 dark:active:text-red active:text-redLight"
+                    >
+                        {content.name.toUpperCase()}
+                    </Link>
+                </li>
+            ))}
+        </ul>
+    );
+};
