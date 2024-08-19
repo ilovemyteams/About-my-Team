33a84d591b71af49ed37f6a731a4488d3863d051
Olena Posternak <101277404+OlenaPosternak@users.noreply.github.com>
2024-08-19 22:32:58 +0300

    Update buttonType.ts

--- sanity/schemas/objects/global/buttonType.ts
+++ sanity/schemas/objects/global/buttonType.ts
@@ -3,59 +3,39 @@ import { defineField } from "sanity";
 export const buttonType = defineField({
     name: "button",
     title: "Button",
-    description: "Please chose type of link, name and color",
     type: "object",
     fields: [
         defineField({
-            name: "linkName",
+            name: "buttonName",
             title: "Button name",
             type: "internationalizedArrayString",
             validation: Rule => Rule.required(),
         }),
         defineField({
-            name: "buttonColor",
-            description: "Please choose button color type",
-            type: "string",
-            options: {
-                list: ["Red", "White"],
-                layout: "radio",
-            },
-            initialValue: "Red",
-        }),
-        defineField({
-            name: "linkType",
-            title: "Link Type",
+            name: "buttonLink",
+            title: "Button type link",
             type: "string",
             options: {
                 list: [
+                    { title: "No link", value: "noLink" },
                     { title: "Internal", value: "internal" },
                     { title: "External", value: "external" },
                 ],
-                layout: "radio",
             },
-            initialValue: "internal",
+            initialValue: "noLink",
+            validation: Rule => Rule.required(),
         }),
         defineField({
             name: "linkInternal",
             title: "Internal Link",
             type: "linkInternal",
-            hidden: ({ parent }) => parent?.linkType !== "internal",
+            hidden: ({ parent }) => parent?.buttonLink !== "internal",
         }),
         defineField({
             name: "linkExternal",
             title: "External Link",
-            type: "url",
-            hidden: ({ parent }) => parent?.linkType !== "external",
+            type: "linkExternal",
+            hidden: ({ parent }) => parent?.buttonLink !== "external",
         }),
     ],
-    validation: Rule =>
-        Rule.custom(fields => {
-            if (fields?.linkType === "internal" && !fields?.linkInternal) {
-                return "Internal link is required.";
-            }
-            if (fields?.linkType === "external" && !fields?.linkExternal) {
-                return "External link is required.";
-            }
-            return true;
-        }),
 });
