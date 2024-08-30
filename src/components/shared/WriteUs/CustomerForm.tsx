827c23eefc7800a386ffd2489de0a05a5c36ca82
IraMira333 <irinat333@gmail.com>
2024-08-30 23:41:01 +0300

    Update CustomerForm.tsx
    Changed scrollbar color to greyLight

--- src/components/shared/WriteUs/CustomerForm.tsx
+++ src/components/shared/WriteUs/CustomerForm.tsx
@@ -36,7 +36,7 @@ const textLabelStyles = "absolute transition-[top] duration-300 ease-out";
 const fieldStyles =
     "block appearance-none w-full h-full py-[7px] bg-transparent outline-none font-caviar text-baseb placeholder-purple-strokeFormLebelLight dark:placeholder-purple-strokeFormLebel border-b-[1px] text-base transition-color duration-300 ease-out";
 const textAreaStyles =
-    "block appearance-none w-full h-[80px] mt-[20px] pr-[5px] bg-transparent outline-none font-caviar text-baseb placeholder-purple-strokeFormLebelLight dark:placeholder-purple-strokeFormLebel text-base transition-color duration-300 ease-out scrollbar scrollbar-w-[3px] scrollbar-thumb-rounded-full scrollbar-track-rounded-full scrollbar-thumb-grey scrollbar-track-transparent";
+    "block appearance-none w-full h-[80px] mt-[20px] pr-[5px] bg-transparent outline-none font-caviar text-baseb placeholder-purple-strokeFormLebelLight dark:placeholder-purple-strokeFormLebel text-base transition-color duration-300 ease-out scrollbar scrollbar-w-[3px] scrollbar-thumb-rounded-full scrollbar-track-rounded-full scrollbar-thumb-greyLight scrollbar-track-transparent";
 const errorStyles =
     "absolute bottom-[-2px] right-0 text-xxs text-error text-right";
 
