4ebaed345434973439786c37b634d2ef9172a444
IraMira333 <irinat333@gmail.com>
2024-09-12 23:58:10 +0300

    Update PreviousURLContext.tsx

--- src/utils/PreviousURLContext.tsx
+++ src/utils/PreviousURLContext.tsx
@@ -3,15 +3,12 @@ import { useLocale } from "next-intl";
 import React, { createContext, ReactNode, useContext, useState } from "react";
 
 import { DEFAULT_SLIDE_ID } from "../constants/defaultSlideId";
-import { Button } from "@/types/sanity.types";
 
 interface PreviousURLContextType {
     previousURL: string;
     setPreviousURL: (url: string) => void;
     slideId: number;
     setSlideId: (id: number) => void;
-    buttonJoinUS: Button | undefined;
-    setButtonJoinUS: (data: Button) => void;
 }
 const PreviousURLContext = createContext<PreviousURLContextType | undefined>(
     undefined
@@ -20,7 +17,6 @@ export const PreviousURLProvider = ({ children }: { children: ReactNode }) => {
     const locale = useLocale();
     const [previousURL, setPreviousURL] = useState<string>(`/${locale}#team`);
     const [slideId, setSlideId] = useState<number>(DEFAULT_SLIDE_ID);
-    const [buttonJoinUS, setButtonJoinUS] = useState<Button>();
 
     return (
         <PreviousURLContext.Provider
@@ -29,8 +25,6 @@ export const PreviousURLProvider = ({ children }: { children: ReactNode }) => {
                 setPreviousURL,
                 slideId,
                 setSlideId,
-                buttonJoinUS,
-                setButtonJoinUS,
             }}
         >
             {children}
