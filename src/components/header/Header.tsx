862f2d8ba89a4df68867c453c30ab01849ce30fe
IraMira333 <irinat333@gmail.com>
2024-09-12 16:15:12 +0300

    Update Header.tsx

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,7 +1,14 @@
+import { Settings } from "@/types/sanity.types";
+
 import { DesktopHeader } from "./DesktopHeader/DesktopHeader";
 import { MobileTabletHeader } from "./MobileTabletHeader/MobileTabletHeader";
 
-export const Header = () => {
+export interface HeaderProps {
+    data?: Settings | null;
+}
+
+export const Header = ({ data }: HeaderProps) => {
+    console.log("Header:", data?._id);
     return (
         <header className="fixed top-0 left-0 z-20">
             <DesktopHeader />
