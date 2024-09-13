93ae21abe29671777f901297dd3bca0ebb7480e1
Alex Petrychuk <93341599+petalser@users.noreply.github.com>
2024-09-13 20:05:47 +0200

    Updated spacing

--- src/components/shared/LikeButton.tsx
+++ src/components/shared/LikeButton.tsx
@@ -14,18 +14,18 @@ const LikeButton = ({ likes }: LikeButtonProps) => {
     };
 
     const style = isActive
-        ? "text-redLight dark:text-red my-2 animate-pulsationBrokenHeart"
-        : "text-greyLight dark:text-grey my-2";
+        ? "text-redLight dark:text-red animate-pulsationBrokenHeart"
+        : "text-greyLight dark:text-grey";
     return (
         <button
             onClick={toggleLike}
-            className="text-purple-100 tab:text-purple-130 dark:text-purple-50 flex gap-2"
+            className="border text-purple-100 tab:text-purple-130 dark:text-purple-50 flex gap-2"
             aria-label="Like"
         >
-            <div className={style}>
+            <div className={`${style} my-auto`}>
                 <IconLike isActive={isActive} />
             </div>
-            <span className="py-2.5">{likes}</span>
+            <span className="my-auto">{likes}</span>
         </button>
     );
 };
