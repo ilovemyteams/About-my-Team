4dd3c98ff2fe8e441e1da8d315793eb5615e2313
Alex Petrychuk <93341599+petalser@users.noreply.github.com>
2024-09-16 17:29:52 +0200

    Created List component and updated Page

--- src/app/[locale]/faq/page.tsx
+++ src/app/[locale]/faq/page.tsx
@@ -1,38 +1,14 @@
 "use client";
 
-import { useTranslations } from "next-intl";
-
-import { Section } from "@/src/components/shared/Section";
-import { normalizeGoogleDriveImgUrls } from "@/src/utils/normalizeGoogleDriveImgUrls";
-
-import { Section } from "@/src/components/shared/Section";
-import { QaCard } from "../../../components/qAsection/QaCard";
+import { QaCardList } from "@/src/components/faqPage/qaSection/QaCardList";
 import { FaqSectionCta } from "@/src/components/faqPage/сtaSection/FaqSectionCta";
-
-const IMAGE = normalizeGoogleDriveImgUrls(
-    "https://drive.google.com/file/d/1ThWQ10FdymqFnc0ELdC_O4892o-eaK9m/view?usp=drive_link"
-);
+import { Section } from "@/src/components/shared/Section";
 
 export default function FAQ() {
-    //current version of this component
-    //is for testing purposes only
-    const getTranslation = useTranslations("Q&A");
-    const ITER: number[] = [1, 2, 3, 4, 5];
-
     return (
         <Section id="faq">
             <FaqSectionCta />
-            {ITER.map(i => (
-                <QaCard
-                    key={i}
-                    data={{ id: i, image: IMAGE, likes: i }}
-                    localizationData={{
-                        questionText: getTranslation(`question${i}`),
-                        answerText: getTranslation(`answer${i}`),
-                        imageAltText: "Illustration",
-                    }}
-                />
-            ))}
+            <QaCardList />
         </Section>
     );
 }
---
+++ src/components/faqPage/qaSection/QaCardList.tsx
@@ -0,0 +1,28 @@
+import { useTranslations } from "next-intl";
+
+import { normalizeGoogleDriveImgUrls } from "@/src/utils/normalizeGoogleDriveImgUrls";
+
+import { QaCard } from "./QaCard";
+
+const IMAGE = normalizeGoogleDriveImgUrls(
+    "https://drive.google.com/file/d/1ThWQ10FdymqFnc0ELdC_O4892o-eaK9m/view?usp=drive_link"
+);
+
+export const QaCardList = () => {
+    //current version of this component
+    //is for testing purposes only
+    const getTranslation = useTranslations("Q&A");
+    const ITER: number[] = [1, 2, 3, 4, 5];
+
+    return ITER.map(i => (
+        <QaCard
+            key={i}
+            data={{ id: i, image: IMAGE, likes: i }}
+            localizationData={{
+                questionText: getTranslation(`question${i}`),
+                answerText: getTranslation(`answer${i}`),
+                imageAltText: "Illustration",
+            }}
+        />
+    ));
+};
