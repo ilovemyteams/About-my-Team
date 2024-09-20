deacfaaed3257488119931ad749e3c64fa87563e
Alex Petrychuk <93341599+petalser@users.noreply.github.com>
2024-09-20 11:15:05 +0200

    Updated spacing

--- src/components/faqPage/qaSection/QaCard.tsx
+++ src/components/faqPage/qaSection/QaCard.tsx
@@ -28,7 +28,7 @@ export const QaCard = ({ data, localizationData }: FaqCardItemProps) => {
     const { questionText, answerText, imageAltText } = localizationData;
 
     return (
-        <div className="tab:mb-2.5 flex flex-col tab:flex-row py-4 tab:gap-3 border-b-1 border-purple-strokeLight dark:border-purple-stroke">
+        <div className="flex flex-col tab:flex-row py-4 tab:gap-3 border-b-1 border-purple-strokeLight dark:border-purple-stroke">
             <Image
                 src={image}
                 alt={imageAltText}
@@ -37,7 +37,7 @@ export const QaCard = ({ data, localizationData }: FaqCardItemProps) => {
                 className="object-cover w-auto tab:w-[300px] h-[200px] tab:my-auto"
             />
 
-            <div className="flex flex-col">
+            <div className="flex flex-col tab:pl-3 pc:px-3">
                 <div className="flex justify-between">
                     <LikeButton likes={likes} />
                     <SharePopover className="" trigerShowShareText={true} />
@@ -55,8 +55,8 @@ export const QaCard = ({ data, localizationData }: FaqCardItemProps) => {
                     />
                     <button
                         onClick={() => alert("Transition to full article")}
-                        className="text-purple-130 dark:text-purple-100 pc:hover:text-redLight dark:pc:hover:text-red
-                    pc:focus:text-redLight dark:pc:focus:text-red focus:outline-none pc:transition pc:ease-out pc:duration-300 relative before:absolute before:bottom-1 before:w-full before:border-b before:border-purple-130 dark:before:border-purple-100 pc:hover:before:border-redLight dark:pc:hover:before:border-red pc:focus:before:border-redLight dark:pc:focus:before:border-red"
+                        className="mr-3 text-purple-130 dark:text-purple-100 pc:hover:text-redLight dark:pc:hover:text-red
+                    pc:focus:text-redLight dark:pc:focus:text-red focus:outline-none pc:transition pc:ease-out pc:duration-300 relative before:absolute before:bottom-1 before:w-full before:border-b before:border-purple-130 dark:before:border-purple-100 pc:hover:before:border-redLight dark:pc:hover:before:border-red pc:focus:before:border-redLight dark:pc:focus:before:border-red pc:before:transition pc:before:ease-out pc:before:duration-300"
                     >
                         {getTranslation("readMore")}
                     </button>
