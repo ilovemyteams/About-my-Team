9d6e41c358be637c246d0cb3d2e4558b52ae558c
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2024-09-25 18:05:30 +0300

    add buttons' max symbols validation

--- sanity/schemas/objects/global/buttonType.ts
+++ sanity/schemas/objects/global/buttonType.ts
@@ -1,5 +1,7 @@
 import { defineField } from "sanity";
 
+const MAX_LENGTH = 18;
+
 export const buttonType = defineField({
     name: "button",
     title: "Button",
@@ -13,7 +15,24 @@ export const buttonType = defineField({
             name: "buttonName",
             title: "Button name",
             type: "internationalizedArrayString",
-            validation: Rule => Rule.required(),
+            validation: rule =>
+                rule.custom<{ value: string; _type: string; _key: string }[]>(
+                    value => {
+                        if (value.some(item => !item.value)) {
+                            return "Button name is required";
+                        }
+
+                        const invalidItems = value.filter(
+                            item => item.value.length > MAX_LENGTH
+                        );
+
+                        if (invalidItems.length) {
+                            return "Button name is too long. A maximum of 18 characters is allowed";
+                        }
+
+                        return true;
+                    }
+                ),
         }),
         defineField({
             name: "buttonLink",
