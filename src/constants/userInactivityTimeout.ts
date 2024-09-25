f73c22d89bef4a22911c81be19f009c4d20cd45e
Anna Prutnik <prutnik.anna@gmail.com>
2024-09-25 11:45:41 +0300

    change timeout

--- src/constants/userInactivityTimeout.ts
+++ src/constants/userInactivityTimeout.ts
@@ -1,3 +1,2 @@
-//Time for user inactivity in milliseconds
-export const FIRST_USER_INACTIVITY_TIMEOUT = 1000 * 5;
-export const SECOND_USER_INACTIVITY_TIMEOUT = 1000 * 5 * 2;
+export const FIRST_USER_INACTIVITY_TIMEOUT = 1000 * 60;
+export const SECOND_USER_INACTIVITY_TIMEOUT = 1000 * 60 * 2;
