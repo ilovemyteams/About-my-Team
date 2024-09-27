ebb3e22daf782e126707aa59b0b83feb83bfa6e0
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2024-09-27 11:26:55 +0300

    updated seo validation

--- sanity/schemas/objects/seoType.ts
+++ sanity/schemas/objects/seoType.ts
@@ -2,8 +2,8 @@ import { defineField } from "sanity";
 
 import { validateMaxLength } from "@/sanity/utils/validateMaxLength";
 
-const TITLE_MAX_LENGTH = 60;
-const DESCRIPTION_MAX_LENGTH = 300;
+const TITLE_MAX_LENGTH = 100;
+const DESCRIPTION_MAX_LENGTH = 200;
 
 export const seoType = defineField({
     name: "seo",
@@ -17,12 +17,14 @@ export const seoType = defineField({
     fields: [
         defineField({
             name: "title",
+            description: "Optimal title length is 50-60 symbols",
             type: "internationalizedArrayString",
             validation: rule =>
                 rule.custom(validateMaxLength(TITLE_MAX_LENGTH)),
         }),
         defineField({
             name: "description",
+            description: "Optimal description length is 160 symbols",
             type: "internationalizedArrayString",
             validation: rule =>
                 rule.custom(validateMaxLength(DESCRIPTION_MAX_LENGTH)),
