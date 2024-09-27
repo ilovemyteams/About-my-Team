9faa97d53d7a6d483ab8d61be79248b13055353b
Anna Prutnik <prutnik.anna@gmail.com>
2024-09-27 11:35:05 +0300

    change background for page

--- src/components/underConstruction/UnderConstruction.tsx
+++ src/components/underConstruction/UnderConstruction.tsx
@@ -6,8 +6,7 @@ export const UnderConstruction = () => {
     const getTranslation = useTranslations("UnderConstruction");
 
     return (
-        <div className="relative bg-underConstructionGradientLight dark:bg-underConstructionGradient ">
-            <div className="absolute top-0 bottom-0 left-0 right-0 bg-[url('/images/under-construction.jpg')] bg-no-repeat bg-cover bg-center -z-10"></div>
+        <div className="relative bg-[url('/images/under-construction.jpg')] bg-no-repeat bg-cover bg-center z-10 before:content-[''] before:absolute before:inset-0 before:bg-underConstructionGradientLight before:dark:bg-underConstructionGradient before:z-[-5]">
             <div className="container pt-[177px] tab:pt-[270px] pc:pt-[247px] pb-[197px] tab:pb-[264px] pc:pb-[297px] text-purple-200 dark:text-grey text-center ">
                 <h1 className="font-caviar text-2xlb tab:text-5xl pc:text-6xl pb-4 tab:pb-3 dark:text-white-200">
                     {getTranslation("title")}
