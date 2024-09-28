1b5408bed94c88c85812a747cf1e3249accfb515
IraMira333 <irinat333@gmail.com>
2024-09-28 23:22:59 +0300

    Added title, description & image for 404

--- src/app/[locale]/[...not-found]/page.tsx
+++ src/app/[locale]/[...not-found]/page.tsx
@@ -1,4 +1,25 @@
+import { getTranslations } from "next-intl/server";
 import { notFound } from "next/navigation";
+
+export async function generateMetadata({
+    params: { locale },
+}: {
+    params: { locale: string };
+}) {
+    const getTranslation = await getTranslations({
+        locale,
+        namespace: "NotFound",
+    });
+
+    return {
+        title: getTranslation("title"),
+        description: getTranslation("info"),
+        openGraph: {
+            description: getTranslation("info"),
+            title: getTranslation("title"),
+        },
+    };
+}
 
 export default function NotFoundCatchAll() {
     notFound();
