b4b20e91fbb6490e54e1bce951362d2a943ec0fd
IraMira333 <irinat333@gmail.com>
2024-09-29 23:24:14 +0300

    Returned the previous version of the code

--- src/components/portfolioSection/Title/MobTitle.tsx
+++ src/components/portfolioSection/Title/MobTitle.tsx
@@ -1,42 +1,24 @@
-// import { useTranslations } from "next-intl";
-import { PortableText } from "@portabletext/react";
+import { useTranslations } from "next-intl";
 
-import { myPortableTextComponents } from "@/sanity/utils/portableTexts";
-import { PortfolioTitleProps } from "@/types/sanityDataPropsTypes";
-
-export const MobTitle = ({
-    value,
-}: {
-    value: PortfolioTitleProps[] | undefined;
-}) => {
-    if (!value) return null;
+export const MobTitle = () => {
+    const getTranslation = useTranslations("Portfolio");
 
     return (
         <div className="tab:hidden">
-            <PortableText value={value} components={myPortableTextComponents} />
+            <p>
+                {getTranslation("titleFirstWord")}
+                <span className="text-purple-100">
+                    {getTranslation("titleSecondWord")}
+                </span>
+                ,{getTranslation("titleThirdWord")}
+                <span className="text-purple-100">
+                    {getTranslation("titleFourthWord")}
+                </span>
+                {getTranslation("titlePartForMob")}
+                <span className="text-purple-100">
+                    {getTranslation("titleSeventhWord")}
+                </span>
+            </p>
         </div>
     );
 };
-
-// export const MobTitle = () => {
-//     const getTranslation = useTranslations("Portfolio");
-
-//     return (
-//         <div className="tab:hidden">
-//             <p>
-//                 {getTranslation("titleFirstWord")}
-//                 <span className="text-purple-100">
-//                     {getTranslation("titleSecondWord")}
-//                 </span>
-//                 ,{getTranslation("titleThirdWord")}
-//                 <span className="text-purple-100">
-//                     {getTranslation("titleFourthWord")}
-//                 </span>
-//                 {getTranslation("titlePartForMob")}
-//                 <span className="text-purple-100">
-//                     {getTranslation("titleSeventhWord")}
-//                 </span>
-//             </p>
-//         </div>
-//     );
-// };
--- src/components/portfolioSection/Title/TabTitle.tsx
+++ src/components/portfolioSection/Title/TabTitle.tsx
@@ -1,46 +1,29 @@
-import { PortableText } from "next-sanity";
+import { useTranslations } from "next-intl";
 
-import { myPortableTextComponents } from "@/sanity/utils/portableTexts";
-import { PortfolioTitleProps } from "@/types/sanityDataPropsTypes";
-
-export const TabTitle = ({
-    value,
-}: {
-    value: PortfolioTitleProps[] | undefined;
-}) => {
-    if (!value) return null;
-
+export const TabTitle = () => {
+    const getTranslation = useTranslations("Portfolio");
     return (
-        <div className="hidden tab:block desk:hidden tab:w-[70%]">
-            <PortableText value={value} components={myPortableTextComponents} />
+        <div className="hidden tab:block desk:hidden">
+            <p>
+                {getTranslation("titleFirstWord")}
+                <span className="text-purple-100">
+                    {getTranslation("titleSecondWord")}
+                </span>
+                ,
+            </p>
+            <p className="tab:-ml-[100px] pc:ml-0">
+                {getTranslation("titleThirdWord")}
+                <span className="text-purple-100">
+                    {getTranslation("titleFourthWord")}
+                </span>
+                {getTranslation("titleFifthWord")}
+            </p>
+            <p className="-ml-[100px] max-w-[700px] pc:max-w-[900px]">
+                {getTranslation("titleSixthWord")}
+                <span className="text-purple-100">
+                    {getTranslation("titleSeventhWord")}
+                </span>
+            </p>
         </div>
     );
 };
-
-// export const TabTitle = () => {
-//     const getTranslation = useTranslations("Portfolio");
-//     return (
-//         <div className="hidden tab:block desk:hidden">
-//             <p>
-//                 {getTranslation("titleFirstWord")}
-//                 <span className="text-purple-100">
-//                     {getTranslation("titleSecondWord")}
-//                 </span>
-//                 ,
-//             </p>
-//             <p className="tab:-ml-[100px] pc:ml-0">
-//                 {getTranslation("titleThirdWord")}
-//                 <span className="text-purple-100">
-//                     {getTranslation("titleFourthWord")}
-//                 </span>
-//                 {getTranslation("titleFifthWord")}
-//             </p>
-//             <p className="-ml-[100px] max-w-[700px] pc:max-w-[900px]">
-//                 {getTranslation("titleSixthWord")}
-//                 <span className="text-purple-100">
-//                     {getTranslation("titleSeventhWord")}
-//                 </span>
-//             </p>
-//         </div>
-//     );
-// };
