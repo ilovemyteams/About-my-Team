1c380efe841c07ed935406fff3016dfa4da45f91
IraMira333 <irinat333@gmail.com>
2024-09-30 13:48:18 +0300

    Updated image path

--- src/app/[locale]/member/[id]/opengraph-image.tsx
+++ src/app/[locale]/member/[id]/opengraph-image.tsx
@@ -16,7 +16,10 @@ export default async function Image({ params }: { params: { id: string } }) {
         return new ImageResponse(
             (
                 <div className="flex items-end justify-center h-full w-full bg-memberMenuGradientDark">
-                    <img src="/images/profile.jpg" alt="Person not found" />
+                    <img
+                        src="https://opengraph.b-cdn.net/production/images/58fd5b62-5cde-4715-a47f-98109358bbbe.jpg?token=a5qWorH768_gbAFM93r0DhTNezSrqC-LlKu7EVmUSfQ&height=630&width=1200&expires=33263692852"
+                        alt="Person not found"
+                    />
                 </div>
             )
         );
@@ -25,12 +28,11 @@ export default async function Image({ params }: { params: { id: string } }) {
     return new ImageResponse(
         (
             <div>
-                <div className="flex items-end justify-center h-full w-full bg-memberMenuGradientDark">
-                    <img
-                        src="/images/profile.png"
-                        alt={member[locale as LocaleType]?.name}
-                    />
-                </div>
+                <img
+                    src="https://opengraph.b-cdn.net/production/images/58fd5b62-5cde-4715-a47f-98109358bbbe.jpg?token=a5qWorH768_gbAFM93r0DhTNezSrqC-LlKu7EVmUSfQ&height=630&width=1200&expires=33263692852"
+                    alt={member[locale as LocaleType]?.name}
+                />
+
                 <p>{member[locale as LocaleType]?.name}</p>
                 <p className=" uppercase">{member.data.position}</p>
             </div>
