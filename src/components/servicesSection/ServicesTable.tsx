dda6c2250dffc32b9dad09f179b9696a1e749aea
IraMira333 <irinat333@gmail.com>
2024-10-01 17:39:24 +0300

    Added services list

--- src/components/servicesSection/ServicesTable.tsx
+++ src/components/servicesSection/ServicesTable.tsx
@@ -1,48 +1,26 @@
-import { useTranslations } from "next-intl";
-
 import { Table } from "../shared/Table/Table";
 import { TableItemsList } from "../shared/Table/TableItemsList";
 import { WriteUs } from "../shared/WriteUs/WriteUs";
 
-export const ServicesTable = () => {
-    const getTranslation = useTranslations();
-
-    const serviceItems = [
-        {
-            title: getTranslation("Services.BATitle"),
-            text: getTranslation("Services.BAText"),
-        },
-        {
-            title: getTranslation("Services.designTitle"),
-            text: getTranslation("Services.designText"),
-        },
-        {
-            title: getTranslation("Services.devTitle"),
-            text: getTranslation("Services.devText"),
-        },
-        {
-            title: getTranslation("Services.QATitle"),
-            text: getTranslation("Services.QAText"),
-        },
-        {
-            title: getTranslation("Services.supportTitle"),
-            text: getTranslation("Services.supportText"),
-        },
-        {
-            title: getTranslation("Services.allServicesTitle"),
-            text: getTranslation("Services.allServicesText"),
-        },
-    ];
-
+export const ServicesTable = ({
+    description,
+    servicesListText,
+    servicesListTitle,
+}: {
+    description: string | null;
+    servicesListTitle: Array<string | null> | null;
+    servicesListText: Array<string | null> | null;
+}) => {
     return (
         <Table>
             <TableItemsList
-                items={serviceItems}
+                textList={servicesListText || []}
+                titleList={servicesListTitle || []}
                 sectionClassName="pc:[&:nth-child(5)]:border-l-[1px]"
             />
             <li className="col-span-2 row-span-1 py-[40px] tab:h-[210px] pc:h-[270px] pc:px-4 pc:py-[65px] flex flex-col items-center">
                 <p className="mb-6 max-w-[361px] deskxl:text-lg">
-                    {getTranslation("Services.contactUs")}
+                    {description || "You forgot a description"}
                 </p>
                 <WriteUs />
             </li>
