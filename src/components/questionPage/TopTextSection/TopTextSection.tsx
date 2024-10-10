96b8cc6d7baa77c21206e386e1444743daaffd43
IraMira333 <irinat333@gmail.com>
2024-10-11 00:12:43 +0300

    Update TopTextSection.tsx

--- src/components/questionPage/TopTextSection/TopTextSection.tsx
+++ src/components/questionPage/TopTextSection/TopTextSection.tsx
@@ -12,7 +12,7 @@ export const TopTextSection = ({
     fullAnswerText,
 }: TopTextSectionProps) => {
     return (
-        <PageSection className="pb-[80px] tab:pb-[100px] pc:pb-[100px] desk:pb-[120px]">
+        <PageSection className="pb-[80px] tab:pb-[100px] desk:pb-[120px]">
             <div className="pb-4 border-y pt-6 tab:pt-8 pc:pt-10 desk:pt-[60px] border-purple-strokeLight dark:border-purple-stroke">
                 <div className="desk:h-[365px] flex flex-col tab:flex-row gap-5 pc:gap-10 desk:gap-[60px] mb-5 tab:mb-10 pc:mb-[60px] text-sm20 tab:text-base23 pc:text-xl28 desk:text-2xl34">
                     <div className="basis-1/2 flex flex-col gap-4 pc:gap-6">
