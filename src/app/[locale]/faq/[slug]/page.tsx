e30ce93c544477824147390e9ab13972e5423602
IraMira333 <irinat333@gmail.com>
2024-10-14 16:19:44 +0300

    Created metadata for some question

--- src/app/[locale]/faq/[slug]/page.tsx
+++ src/app/[locale]/faq/[slug]/page.tsx
@@ -1,12 +1,40 @@
+import { Metadata, ResolvingMetadata } from "next";
 import { notFound } from "next/navigation";
 import React from "react";
 
 import { Question } from "@/src/components/questionPage/QuestionPage";
-import { questionsData } from "@/src/mockedData/questionsData";
+import { QAItemType, questionsData } from "@/src/mockedData/questionsData";
+import { LocaleType } from "@/types/LocaleType";
 
 interface QuestionPageProps {
     params: {
         slug: string;
+        locale: string;
+    };
+}
+
+export async function generateMetadata(
+    { params }: QuestionPageProps,
+    parent: ResolvingMetadata
+): Promise<Metadata> {
+    const displayedQuestion: QAItemType | undefined = questionsData.find(
+        question => question.data.slug === params.slug
+    );
+
+    if (!displayedQuestion) {
+        return {
+            title: "Question Not Found",
+            description: "The requested question does not exist.",
+        };
+    }
+    const previousImages = (await parent).openGraph?.images || [];
+    const localization = displayedQuestion[params.locale as LocaleType];
+    return {
+        title: localization.questionText,
+        description: localization.shortAnswerText[0],
+        openGraph: {
+            images: [...previousImages],
+        },
     };
 }
 
