6c957d4172850f6879a437b8d4689c95dc552e47
IraMira333 <irinat333@gmail.com>
2024-10-15 12:01:19 +0300

    Updated Sanity with dev

--- src/components/shared/WriteUs/WriteUs.tsx
+++ src/components/shared/WriteUs/WriteUs.tsx
@@ -1,9 +1,3 @@
-"use client";
-
-import { useTranslations } from "next-intl";
-
-import { useSettingsContext } from "@/src/utils/SettingsSanityContext";
-
 import { ModalsWithForm } from "../ModalsWithForm/ModalsWithForm";
 import { CustomerForm } from "./CustomerForm";
 import { WriteUsBtn } from "./WriteUsBtn";
@@ -13,16 +7,9 @@ interface WriteUsProps {
 }
 
 export const WriteUs = ({ className }: WriteUsProps) => {
-    const getTranslation = useTranslations("Buttons");
-    const { data } = useSettingsContext();
-    const buttonNameString = data?.buttonOrder?.buttonName
-        ? data.buttonOrder.buttonName.toString()
-        : getTranslation("order");
-
     return (
         <ModalsWithForm
             formComponent={CustomerForm}
-            triggerBtnTitle={buttonNameString}
             triggerComponent={WriteUsBtn}
             className={className}
         />
