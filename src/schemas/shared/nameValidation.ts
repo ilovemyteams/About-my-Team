0b9fcc2d77022395d3c535a29f9c2f2093e942ae
Anna Prutnik <prutnik.anna@gmail.com>
2024-10-20 19:48:00 +0300

    add anispace validation

--- src/schemas/shared/nameValidation.ts
+++ src/schemas/shared/nameValidation.ts
@@ -2,7 +2,7 @@ import { useTranslations } from "next-intl";
 import * as yup from "yup";
 
 const nameRegex =
-    /^[a-zA-ZąćęłńóśźżĄĆĘŁŃÓŚŹŻіІїЇґҐєЄа-яА-Я'"`\s\u02BC\u2019]+$/;
+    /^(?=(.*\S.*\S))[^\s][a-zA-ZąćęłńóśźżĄĆĘŁŃÓŚŹŻіІїЇґҐєЄа-яА-Я'"`\s\u02BC\u2019]+$/;
 
 export const NameValidation = () => {
     const getTranslation = useTranslations("Errors");
