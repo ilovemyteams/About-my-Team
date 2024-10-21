44cd285a018f288daab23a3a9fb8d53fabeed6f4
IraMira333 <irinat333@gmail.com>
2024-10-21 23:47:29 +0300

    Added 5th Answer

--- src/components/questionPage/OrderCard.tsx
+++ src/components/questionPage/OrderCard.tsx
@@ -1,5 +1,8 @@
 import Image from "next/image";
 
+import { parseTextWithLink } from "@/src/utils/parseTextWithLink";
+import { LocaleType } from "@/types/LocaleType";
+
 import { NoImageHeart } from "../shared/NoImageHeart";
 import { PageSection } from "../shared/PageSection";
 import { WriteUs } from "../shared/WriteUs/WriteUs";
@@ -8,12 +11,14 @@ interface OrderCardProps {
     imageLink?: string;
     fullAnswerBottomText?: string[];
     imageAltText: string;
+    locale: LocaleType;
 }
 
 export const OrderCard = ({
     imageLink,
     fullAnswerBottomText,
     imageAltText,
+    locale,
 }: OrderCardProps) => {
     return (
         <PageSection className="flex flex-col gap-6 tab:flex-row tab:gap-8 tab:justify-between pc:gap-[104px] desk:gap-[100px] pb-[80px] tab:pb-[100px] desk:pb-[180px]">
@@ -23,7 +28,7 @@ export const OrderCard = ({
                     width={630}
                     height={362}
                     alt={imageAltText}
-                    className="tab:w-[50%] pc:min-w-[630px] pc:w-[58%] desk:w-[54.6%]"
+                    className="tab:w-[50%] pc:min-w-[630px] pc:w-[58%] desk:w-[54.6%] object-contain mt-0 mb-auto"
                 />
             ) : (
                 <div className="bg-CTAGradientLight dark:bg-CTAGradient tab:w-[50%] pc:min-w-[630px] pc:w-[58%] w-full min-h-[166px] tab:min-h-[206px] pc:min-h-[362px]">
@@ -39,7 +44,7 @@ export const OrderCard = ({
                                 key={index}
                                 className="text-sm20 tab:text-base23 pc:text-xl28 desk:text-2xl34"
                             >
-                                {text}
+                                {parseTextWithLink(text, locale)}
                             </p>
                         ))}
                     </div>
