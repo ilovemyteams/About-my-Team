e71d6760fe0967a0886de60426f862a5d72b1d8a
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2024-10-21 14:13:00 +0300

    update fetchSettingsData function in the providers

--- src/app/[locale]/providers.tsx
+++ src/app/[locale]/providers.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 import { NextUIProvider } from "@nextui-org/react";
 import { useLocale } from "next-intl";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
@@ -7,35 +8,39 @@ import * as React from "react";
 
 import { readToken } from "@/sanity/lib/api";
 import { getClient } from "@/sanity/lib/client";
-import { footerQuery, settingsQuery } from "@/sanity/lib/queries";
+import { settingsQuery } from "@/sanity/lib/queries";
 import { useSettingsContext } from "@/src/utils/SettingsSanityContext";
 
 export const Providers = ({ children, ...props }: ThemeProviderProps) => {
     const { setData } = useSettingsContext();
-    const { setDataFooter } = useSettingsContext();
     const locale = useLocale();
 
     React.useEffect(() => {
-        async function fetchDataSettings() {
-            const client = getClient({ token: readToken });
-            const settingsSanityData = await client.fetch(settingsQuery, {
-                language: locale,
-            });
-            setData(settingsSanityData);
-        }
-        fetchDataSettings();
-    }, [locale, setData]);
+        const client = getClient({ token: readToken });
 
-    React.useEffect(() => {
-        async function fetcFooterhData() {
-            const client = getClient({ token: readToken });
-            const footerSanityData = await client.fetch(footerQuery, {
-                language: locale,
+        const fetchSettingsData = async () => {
+            try {
+                const settingsSanityData = await client.fetch(settingsQuery, {
+                    language: locale,
+                });
+                setData(settingsSanityData);
+            } catch (error) {
+                throw new Error("Failed to fetch settings data");
+            }
+        };
+
+        fetchSettingsData();
+
+        const subscription = client
+            .listen(settingsQuery, { language: locale })
+            .subscribe(async update => {
+                if (update.result) {
+                    await fetchSettingsData();
+                }
             });
-            setDataFooter(footerSanityData);
-        }
-        fetcFooterhData();
-    }, [locale, setDataFooter]);
+
+        return () => subscription.unsubscribe();
+    }, [locale, setData]);
 
     return (
         <NextUIProvider>
