6e2dc7481a97e7834824ef014d2702cf2e9623c6
Anna Prutnik <prutnik.anna@gmail.com>
2024-10-22 21:11:13 +0300

    change styles for big screen

--- src/components/halloween/greetingModal/GreetingModal.tsx
+++ src/components/halloween/greetingModal/GreetingModal.tsx
@@ -65,7 +65,7 @@ export const GreetingModal = ({ onCloseModal }: GreetingModalProps) => {
                 }}
                 aria-label="modal-window"
                 onClick={e => e.stopPropagation()}
-                className="w-full tab:w-unset min-w-[320px] max-w-[360px] tab:min-w-[660px] pc:min-w-[750px] bg-white-100 dark:bg-purple-400 fixed top-7 tab:top-1/2 left-1/2"
+                className="w-full tab:w-unset min-w-[320px] max-w-[360px] tab:min-w-[660px] pc:min-w-[750px] desk:min-w-[850px] bg-white-100 dark:bg-purple-400 fixed top-7 tab:top-1/2 left-1/2"
             >
                 <div className=" overflow-hidden relative w-full h-auto px-[24px] tab:px-[24px] pc:px-[60px] pt-[181px] tab:pt-[219px] pb-[64px] pc:pb-[72px]">
                     <Background />
@@ -80,13 +80,13 @@ export const GreetingModal = ({ onCloseModal }: GreetingModalProps) => {
                         <IconCloseX />
                     </button>
                     <div className="text-center whitespace-pre-wrap">
-                        <p className="mb-[18px] tab:mb-4 pc:mb-3 text-base tab:text-lg pc:text-xl font-caviar font-bold">
+                        <p className="mb-[18px] tab:mb-4 pc:mb-3 text-base tab:text-lg pc:text-xl desk:text-2xl font-caviar font-bold">
                             {parseTextWithLink(
                                 getTranslation("greeting"),
                                 locale
                             )}
                         </p>
-                        <p className=" text-redLight dark:text-red mb-11 tab:mb-9 pc:mb-6 text-sm tab:text-base max-w-[80%] mx-auto">
+                        <p className=" text-redLight dark:text-red mb-11 tab:mb-9 pc:mb-6 desk:text-lg25 text-sm tab:text-base max-w-[80%] mx-auto">
                             {getTranslation("caption")}
                         </p>
                         <Button color="grey" onClick={onCloseModal}>
