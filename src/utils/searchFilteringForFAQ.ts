baa5144d1ae53b50f5a513b2ddce1c2612c03539
Alex Petrychuk <93341599+petalser@users.noreply.github.com>
2024-10-23 13:56:39 +0200

    SearchFiltering refactor

--- src/utils/searchFilteringForFAQ.ts
+++ src/utils/searchFilteringForFAQ.ts
@@ -1,7 +1,10 @@
-import type { LocaleType } from "@/types/LocaleType";
+import { LocaleType } from "@/types/LocaleType";
 
-import type { QAItemType } from "../mockedData/questionsData";
-import type { LongAnswerListTextType } from "../mockedData/questionsData";
+import type {
+    LongAnswerListTextType,
+    LongAnswerListType,
+    QAItemType,
+} from "../mockedData/questionsData";
 
 export const searchFilteringForFAQ = (
     question: QAItemType,
@@ -9,31 +12,52 @@ export const searchFilteringForFAQ = (
     searchValue: string
 ) => {
     const data = question[locale as LocaleType];
-    const linkPattern = /\*link=.*?\*.*?\*\/link\*/gi;
     const lowerCasedSearchTerm = searchValue.toLowerCase();
-    const stepsTexts = data.fullAnswerContent
-        ?.reduce((acc: (string | LongAnswerListTextType)[], item) => {
-            acc.push(item.title);
-            acc.push(...item.text);
-            return acc;
-        }, [])
-        .join("");
+
+    const questionText = cleanText(data.questionText);
+    const shortAnswerText = cleanText(data.shortAnswerText.join(""));
+    const fullAnswerTopText = cleanText(data.fullAnswerTopText?.join("") || "");
+    const stepsTexts = cleanText(joinAnswerTexts(data.fullAnswerContent || []));
+
     if (
-        data.questionText
-            .toLowerCase()
-            .replaceAll(linkPattern, "")
-            .includes(lowerCasedSearchTerm) ||
-        data.shortAnswerText
-            .join("")
-            .toLowerCase()
-            .replaceAll(linkPattern, "")
-            .includes(lowerCasedSearchTerm) ||
-        data.fullAnswerTopText
-            ?.join("")
-            .replaceAll(linkPattern, "")
-            .includes(lowerCasedSearchTerm) ||
-        stepsTexts?.replaceAll(linkPattern, "").includes(lowerCasedSearchTerm)
+        questionText.includes(lowerCasedSearchTerm) ||
+        shortAnswerText.includes(lowerCasedSearchTerm) ||
+        fullAnswerTopText.includes(lowerCasedSearchTerm) ||
+        stepsTexts.includes(lowerCasedSearchTerm)
     ) {
         return question;
     }
 };
+
+function cleanText(text: string): string {
+    const linkPattern = /\*link=.*?\*.*?\*\/link\*/gi;
+    return text.replaceAll(linkPattern, "").toLowerCase();
+}
+
+function flattenText(
+    text: string | string[] | LongAnswerListTextType[]
+): string {
+    if (typeof text === "string") {
+        return text;
+    } else if (Array.isArray(text)) {
+        return text
+            .map(item => {
+                if (typeof item === "string") {
+                    return item;
+                } else if (typeof item === "object" && "text" in item) {
+                    return item.title + flattenText(item.text);
+                }
+                return "";
+            })
+            .join("");
+    }
+    return "";
+}
+
+function joinAnswerTexts(answerContent: LongAnswerListType[]): string {
+    if (answerContent.length === 0) return "";
+    return answerContent.reduce((acc: string, item) => {
+        acc += item.title + flattenText(item.text);
+        return acc;
+    }, "");
+}
