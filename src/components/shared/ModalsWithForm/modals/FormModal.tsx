80ee1025ecb03cc1fc8bf802da87b26de7b1f8f0
Anna Prutnik <prutnik.anna@gmail.com>
2024-10-23 14:52:37 +0300

    add animation during opening modals

--- src/components/shared/ModalsWithForm/modals/FormModal.tsx
+++ src/components/shared/ModalsWithForm/modals/FormModal.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { AnimatePresence, motion } from "framer-motion";
 import {
     ComponentType,
     Dispatch,
@@ -8,6 +9,8 @@ import {
     useState,
 } from "react";
 
+import { SCREEN_NAMES } from "@/src/constants/screenNames";
+import { useScreenSize } from "@/src/hooks/useScreenSize";
 import {
     FormInModalProps,
     TriggerComponentProps,
@@ -41,6 +44,11 @@ export const FormModal = ({
     const onOpenModal = () => setIsModalOpen(true);
     const onCloseModal = () => setIsModalOpen(false);
 
+    const screenSizeName = useScreenSize();
+    const { mobileName } = SCREEN_NAMES;
+
+    const modalTranslate = screenSizeName === mobileName ? "0%" : "-50%";
+
     const notificationHandler = async (submitFn: SubmitFnType) => {
         try {
             await submitFn();
@@ -73,78 +81,72 @@ export const FormModal = ({
                 modalOpenHandler={onOpenModal}
                 isModalOpen={isModalOpen}
             />
-            {isModalOpen && (
-                <div
-                    aria-label="modal-backdrop"
-                    onClick={onCloseModal}
-                    className="fixed z-[20] no-doc-scroll top-0 left-0 w-full h-full bg-greyLight bg-opacity-70 dark:bg-backdrop dark:bg-opacity-80"
-                >
-                    <div
-                        aria-label="modal-window"
-                        onClick={e => e.stopPropagation()}
-                        className="max-h-[90dvh] overflow-y-auto scroll w-full tab:w-unset min-w-[320px] max-w-[360px] tab:min-w-[660px] pc:min-w-[750px] bg-white-100 dark:bg-purple-400 fixed top-7 tab:top-1/2 left-1/2 
-            -translate-x-1/2 tab:-translate-y-1/2 z-[21]"
+            <AnimatePresence>
+                {isModalOpen && (
+                    <motion.div
+                        initial="hidden"
+                        animate="visible"
+                        exit="hidden"
+                        variants={{
+                            hidden: {
+                                opacity: 0,
+                            },
+                            visible: {
+                                opacity: 1,
+                                transition: {
+                                    duration: 0.5,
+                                    delayChildren: 0.3,
+                                },
+                            },
+                        }}
+                        aria-label="modal-backdrop"
+                        onClick={onCloseModal}
+                        className="fixed z-[20] no-doc-scroll top-0 left-0 w-full h-full bg-greyLight bg-opacity-70 dark:bg-backdrop dark:bg-opacity-80"
                     >
-                        <div className="relative w-full h-auto px-[16px] tab:px-[24px] pc:px-[60px] py-[64px] pc:py-[72px]">
-                            <BgImagesMobile />
-                            <BgImagesTablet />
-                            <BgImagesDesktop />
-                            <button
-                                type="button"
-                                onClick={onCloseModal}
-                                disabled={isError}
-                                aria-label="close button"
-                                className="cursor-pointer flex justify-center items-center absolute top-2 right-4 pc:top-3 pc:right-3 p-3 disabled:text-purple-strokeLight
-                         dark:disabled:text-purple-stroke bg-transparent enabled:icon-hover-rounded-purple z-[25] "
-                            >
-                                <IconCloseX />
-                            </button>
-                            <FormComponent
-                                notificationHandler={notificationHandler}
-                            />
-                        </div>
-                    </div>
-                </div>
-            )}
-            {/* <Modal
-                isOpen={isOpen}
-                onOpenChange={onOpenChange}
-                placement={placement}
-                radius="none"
-                shouldBlockScroll={
-                    screenSizeName !== mobileName &&
-                    screenSizeName !== tabletName
-                }
-                hideCloseButton={true}
-                className="top-7 tab:top-0 overflow-y-auto scroll min-w-[320px] max-w-[360px] tab:min-w-[660px] pc:min-w-[750px] bg-white-100 dark:bg-purple-400 no-doc-scroll"
-                classNames={{
-                    backdrop:
-                        "bg-greyLight bg-opacity-70 dark:bg-backdrop dark:bg-opacity-80",
-                    wrapper: "absolute",
-                }}
-            >
-                <ModalContent className="m-0">
-                    <div className="relative w-full h-full px-[16px] tab:px-[24px] pc:px-[60px] py-[64px] pc:py-[72px]">
-                        <BgImagesMobile />
-                        <BgImagesTablet />
-                        <BgImagesDesktop />
-
-                        <button
-                            type="button"
-                            onClick={onClose}
-                            disabled={isError}
-                            aria-label="close button"
-                            className="cursor-pointer flex justify-center items-center absolute top-2 right-4 pc:top-3 pc:right-3 p-3 disabled:text-purple-strokeLight
-                         dark:disabled:text-purple-stroke bg-transparent enabled:icon-hover-rounded-purple"
+                        <motion.div
+                            variants={{
+                                hidden: {
+                                    x: "-50%",
+                                    y: "100vh",
+                                },
+                                visible: {
+                                    x: "-50%",
+                                    y: modalTranslate,
+                                    transition: {
+                                        type: "tween",
+                                    },
+                                },
+                            }}
+                            initial="hidden"
+                            animate="visible"
+                            exit="hidden"
+                            aria-label="modal-window"
+                            onClick={e => e.stopPropagation()}
+                            className="max-h-[90dvh] overflow-y-auto scroll w-full tab:w-unset min-w-[320px] max-w-[360px] tab:min-w-[660px] pc:min-w-[750px] bg-white-100 dark:bg-purple-400 fixed top-7 tab:top-1/2 left-1/2 
+            -translate-x-1/2 tab:-translate-y-1/2 z-[21]"
                         >
-                            <IconCloseX />
-                        </button>
-                        <FormComponent
-                            notificationHandler={notificationHandler}
-                        />
-                    </div>
-                </ModalContent>
-            </Modal> */}
+                            <div className="relative w-full h-auto px-[16px] tab:px-[24px] pc:px-[60px] py-[64px] pc:py-[72px]">
+                                <BgImagesMobile />
+                                <BgImagesTablet />
+                                <BgImagesDesktop />
+                                <button
+                                    type="button"
+                                    onClick={onCloseModal}
+                                    disabled={isError}
+                                    aria-label="close button"
+                                    className="cursor-pointer flex justify-center items-center absolute top-2 right-4 pc:top-3 pc:right-3 p-3 disabled:text-purple-strokeLight
+                         dark:disabled:text-purple-stroke bg-transparent enabled:icon-hover-rounded-purple z-[25] "
+                                >
+                                    <IconCloseX />
+                                </button>
+                                <FormComponent
+                                    notificationHandler={notificationHandler}
+                                />
+                            </div>
+                        </motion.div>
+                    </motion.div>
+                )}
+            </AnimatePresence>
         </div>
     );
 };
