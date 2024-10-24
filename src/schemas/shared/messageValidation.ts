c6c8f52257dc47b1172b1ee7463b96c7c1b554c5
Anna Prutnik <prutnik.anna@gmail.com>
2024-10-24 11:42:50 +0300

    fix validation texts

--- src/schemas/shared/messageValidation.ts
+++ src/schemas/shared/messageValidation.ts
@@ -7,7 +7,7 @@ export const MessageValidation = () => {
     return yup
         .string()
         .trim()
-        .min(2, getTranslation("messageMinMaxSymbols"))
+        .min(1, getTranslation("messageMinMaxSymbols"))
         .max(300, getTranslation("messageMinMaxSymbols"))
         .required(getTranslation("required"));
 };
