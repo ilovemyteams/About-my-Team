16da9fccfe52afcb27a425084d98b11c9010c7ad
IraMira333 <irinat333@gmail.com>
2024-11-11 22:21:06 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ import createNextIntlPlugin from "next-intl/plugin";
 const withNextIntl = createNextIntlPlugin();
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: { urlImports: ["https://themer.sanity.build/"] },
+    // experimental: { urlImports: ["https://themer.sanity.build/"] },
     images: {
         remotePatterns: [
             {
