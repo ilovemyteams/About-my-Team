7c601df99501826875fe103c5b655edd128ecbc9
Anna Prutnik <prutnik.anna@gmail.com>
2024-11-15 15:31:06 +0200

    change opacity and duration

--- src/components/shared/Icons/IconQADecoration.tsx
+++ src/components/shared/Icons/IconQADecoration.tsx
@@ -5,7 +5,7 @@ import { motion } from "framer-motion";
 import { IconProps } from "../../../../types/iconProps.interface";
 
 export const IconQADecoration = ({ className }: IconProps) => {
-    const duration = 1;
+    const duration = 1.4;
 
     const pathArray = [
         "M101.231 78.5L55 116.354L8.76851 78.5L101.231 78.5Z",
@@ -31,7 +31,7 @@ export const IconQADecoration = ({ className }: IconProps) => {
                             stroke="currentColor"
                             key={index}
                             animate={{
-                                opacity: [0.1, 1],
+                                opacity: [0.5, 1],
                             }}
                             transition={{
                                 duration: duration,
