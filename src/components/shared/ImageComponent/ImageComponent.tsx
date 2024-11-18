2f998c128907dfa9622609a15113d81a77f984de
Anna Prutnik <prutnik.anna@gmail.com>
2024-11-18 13:52:37 +0200

    add some descriptions, remove div, add classnames to image

--- src/components/shared/ImageComponent/ImageComponent.tsx
+++ src/components/shared/ImageComponent/ImageComponent.tsx
@@ -5,6 +5,8 @@ import { urlForImage } from "@/sanity/lib/image";
 
 import { NoImageHeart } from "../NoImageHeart";
 
+//blurUrl: lqip image from metadata in sanity image
+//sizes: image size for non-mobile devices
 interface ImageComponentProps {
     src: string | undefined | null;
     alt?: string | undefined | null;
@@ -21,6 +23,7 @@ export const ImageComponent = ({
     sizes = "45vw",
 }: ImageComponentProps) => {
     if (!src) {
+        //default image with brand heart
         return (
             <div
                 className={`bg-CTAGradientLight dark:bg-CTAGradient ${className}`}
@@ -34,17 +37,15 @@ export const ImageComponent = ({
     const url = urlForImage(src).auto("format").fit("max").url();
 
     return (
-        <div className={`relative ${className}`}>
-            <Image
-                src={url}
-                width={dimensions.width}
-                height={dimensions.height}
-                sizes={`(max-width: 768px), 540px, ${sizes}`}
-                alt={alt || "An image without description"}
-                className="object-cover w-full h-full"
-                placeholder="blur"
-                blurDataURL={blurUrl || ""}
-            />
-        </div>
+        <Image
+            src={url}
+            width={dimensions.width}
+            height={dimensions.height}
+            sizes={`(max-width: 768px), 540px, ${sizes}`}
+            alt={alt || "An image without description"}
+            className={`${className} object-cover`}
+            placeholder="blur"
+            blurDataURL={blurUrl || ""}
+        />
     );
 };
