5651a65e1bfb7cba375692748cd425b8588702a2
Alex Petrychuk <93341599+petalser@users.noreply.github.com>
2024-11-25 14:10:55 +0100

    Removed redundand logic

--- src/components/faqPage/сtaSection/FaqSectionCta.tsx
+++ src/components/faqPage/сtaSection/FaqSectionCta.tsx
@@ -7,15 +7,9 @@ import { AskUsModal } from "./modal/AskUsModal";
 
 export const FaqSectionCta = () => {
     const getTranslation = useTranslations("FaqPage");
-    //ToDo: for future pagination
-    const isSectionVisible = true;
-
-    const sectionVisibilityStyle = isSectionVisible
-        ? "flex flex-col tab:flex-row gap-[32px] tab:gap-[90px] items-center tab:justify-between tab:mb-[100px]"
-        : "hidden";
 
     return (
-        <SectionCTA className={sectionVisibilityStyle}>
+        <SectionCTA className="flex flex-col tab:flex-row gap-[32px] tab:gap-[90px] items-center tab:justify-between tab:mb-[100px]">
             <TitleCTAWrapper className="tab:flex-1">
                 {getTranslation("ctaText")}
             </TitleCTAWrapper>
