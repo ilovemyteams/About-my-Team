3e881451832c4cfff3f9b9e58907824f47b458d8
IraMira333 <irinat333@gmail.com>
2024-11-28 00:45:29 +0200

    Update ListWithArrowIconLayout.tsx

--- src/components/servicePage/ListWithArrowIconLayout.tsx
+++ src/components/servicePage/ListWithArrowIconLayout.tsx
@@ -12,7 +12,7 @@ export const ListWithArrowIconLayout = ({
     title?: string;
 }) => {
     return (
-        <PageSection className="pb-8 tab:pb-[100px] pc:pb-[160px] desk:pb-[180px]">
+        <>
             {title && (
                 <h2 className="font-caviar text-2xlb tab:text-4xl pc:text-5xl desk:text-6xl dark:text-white-200 text-purple-200 mb-10 tab:mb-[64px] pc:mb-[80px]">
                     {title}
@@ -24,6 +24,6 @@ export const ListWithArrowIconLayout = ({
                     <IconQADecoration className="tab:sticky tab:top-[88px] pc:top-2" />
                 </div>
             </div>
-        </PageSection>
+        </>
     );
 };
