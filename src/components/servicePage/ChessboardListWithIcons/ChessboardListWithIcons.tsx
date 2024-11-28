b388cb3085099c503157e47062612d4b41cb0766
IraMira333 <irinat333@gmail.com>
2024-11-28 15:32:31 +0200

    Update ChessboardListWithIcons.tsx

--- src/components/servicePage/ChessboardListWithIcons/ChessboardListWithIcons.tsx
+++ src/components/servicePage/ChessboardListWithIcons/ChessboardListWithIcons.tsx
@@ -18,7 +18,6 @@ export const ChessboardListWithIcons = ({
                     className="flex flex-row-reverse gap-[14px] tab:gap-[1.32%] pc:gap-[4.537%] desk:gap-[2.1%] mb-8 last:mb-0 tab:mb-6 tab:odd:flex-row"
                     initial={{ opacity: 0, y: 50 }}
                     whileInView={{ opacity: 1, y: 0 }}
-                    viewport={{ amount: 0.6 }}
                     transition={{ duration: 1 }}
                 >
                     <ChessboardListItem
