4642c0b6bc889aff0d805525f6a2517685648e60
IraMira333 <irinat333@gmail.com>
2024-11-29 13:59:43 +0200

    Added LearnMore Button

--- src/components/servicesSection/ServicesTable.tsx
+++ src/components/servicesSection/ServicesTable.tsx
@@ -1,8 +1,10 @@
-import { useTranslations } from "next-intl";
+"use client";
+import { useRouter } from "next/navigation";
+import { useLocale, useTranslations } from "next-intl";
 
+import { Button } from "../shared/Button";
 import { Table } from "../shared/Table/Table";
 import { TableItemsList } from "../shared/Table/TableItemsList";
-import { WriteUs } from "../shared/WriteUs/WriteUs";
 
 export const ServicesTable = () => {
     const getTranslation = useTranslations();
@@ -33,6 +35,11 @@ export const ServicesTable = () => {
             text: getTranslation("Services.allServicesText"),
         },
     ];
+    const locale = useLocale();
+    const router = useRouter();
+    const handleButtonClick = () => {
+        router.push(`/${locale}/services`);
+    };
 
     return (
         <Table>
@@ -40,11 +47,10 @@ export const ServicesTable = () => {
                 items={serviceItems}
                 sectionClassName="pc:[&:nth-child(5)]:border-l-[1px]"
             />
-            <li className="col-span-2 row-span-1 pt-[40px] tab:h-[210px] pc:h-[270px] pc:px-4 pc:pt-[65px] flex flex-col items-center">
-                <p className="mb-6 max-w-[361px] deskxl:text-lg">
-                    {getTranslation("Services.contactUs")}
-                </p>
-                <WriteUs />
+            <li className="col-span-2 row-span-1 mt-[40px] tab:mt-[56px] pc:px-4 pc:mt-[120px] flex flex-col items-center">
+                <Button onClick={handleButtonClick}>
+                    {getTranslation("Buttons.learnMore")}
+                </Button>
             </li>
         </Table>
     );
