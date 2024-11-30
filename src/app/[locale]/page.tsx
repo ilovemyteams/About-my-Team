8d6fc0ff91de88f3e212e45da489f9d5b9d808a4
IraMira333 <irinat333@gmail.com>
2024-11-30 22:35:13 +0200

    Update page.tsx

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -9,17 +9,17 @@ import {
     loadStages,
 } from "@/sanity/utils/loadQuery";
 import { BackgroundFiguresMain } from "@/src/components/backgroundImages/BackgroundFiguresMain";
-import { FeedbackSection } from "@/src/components/feedbackSection/FeedbackSection";
+// import { FeedbackSection } from "@/src/components/feedbackSection/FeedbackSection";
 import { HeroSection } from "@/src/components/heroSection/HeroSection";
-import { HireUsSection } from "@/src/components/hireUsSection/HireUsSection";
-import { JoinTheTeamSection } from "@/src/components/joinTheTeamSection/JoinTheTeamSection";
-import { MissionSection } from "@/src/components/missionSection/MissionSection";
-import { OurTeamSection } from "@/src/components/ourTeamSection/OurTeamSection";
-import { PortfolioSection } from "@/src/components/portfolioSection/PortfolioSection";
-import { QaSection } from "@/src/components/qAsection/QaSection";
-import { ServicesSection } from "@/src/components/servicesSection/ServicesSection";
-import { StagesSection } from "@/src/components/stagesSection/StagesSection";
-import { WriteUsSection } from "@/src/components/writeUsSection/WriteUsSection";
+// import { HireUsSection } from "@/src/components/hireUsSection/HireUsSection";
+// import { JoinTheTeamSection } from "@/src/components/joinTheTeamSection/JoinTheTeamSection";
+// import { MissionSection } from "@/src/components/missionSection/MissionSection";
+// import { OurTeamSection } from "@/src/components/ourTeamSection/OurTeamSection";
+// import { PortfolioSection } from "@/src/components/portfolioSection/PortfolioSection";
+// import { QaSection } from "@/src/components/qAsection/QaSection";
+// import { ServicesSection } from "@/src/components/servicesSection/ServicesSection";
+// import { StagesSection } from "@/src/components/stagesSection/StagesSection";
+// import { WriteUsSection } from "@/src/components/writeUsSection/WriteUsSection";
 import { PageParamsProps } from "@/types/sanityDataPropsTypes";
 
 export default async function HomePage(props: PageParamsProps) {
@@ -34,12 +34,12 @@ export default async function HomePage(props: PageParamsProps) {
             await loadHomeFaq(props.params.locale),
             await loadHomeReviews(props.params.locale),
         ]);
-
+    console.log(about, cta, portfolioSection, services, stages, faq, reviews);
     return (
         <>
             <BackgroundFiguresMain />
             <HeroSection data={hero} />
-            <MissionSection data={about?.aboutUsHomeSection} />
+            {/* <MissionSection data={about?.aboutUsHomeSection} />
             <JoinTheTeamSection data={cta?.ctaSectionJoinUs} />
             <PortfolioSection data={portfolioSection?.portfolioHome} />
             <FeedbackSection data={reviews} />
@@ -49,7 +49,7 @@ export default async function HomePage(props: PageParamsProps) {
             <JoinTheTeamSection data={cta?.ctaSectionJoinUs} />
             <StagesSection data={stages?.stagesHome} />
             <QaSection data={faq} />
-            <HireUsSection data={cta?.ctaSectionOrder} />
+            <HireUsSection data={cta?.ctaSectionOrder} /> */}
         </>
     );
 }
