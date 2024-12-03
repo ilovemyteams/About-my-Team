48baa37cdee37f2d84eb2c2ee1aee6aa6110b90f
IraMira333 <irinat333@gmail.com>
2024-12-03 22:51:55 +0200

    Update live.ts

--- sanity/lib/live.ts
+++ sanity/lib/live.ts
@@ -1,5 +1,8 @@
 import { defineLive } from "next-sanity";
 
 import { client } from "@/sanity/lib/client";
+const serverToken = process.env.NEXT_PUBLIC_SANITY_API_WRITE_TOKEN;
+const browserToken = process.env.NEXT_PUBLIC_SANITY_API_READ_TOKEN;
 
-export const { sanityFetch, SanityLive } = defineLive({ client });
+
+export const { sanityFetch, SanityLive } = defineLive({ client, serverToken, browserToken });
