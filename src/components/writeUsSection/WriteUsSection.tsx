e6ff9554308ba9ba33554f5ff8e7f5e75e46e175
IraMira333 <irinat333@gmail.com>
2024-12-03 22:41:29 +0200

    Update WriteUsSection.tsx

--- src/components/writeUsSection/WriteUsSection.tsx
+++ src/components/writeUsSection/WriteUsSection.tsx
@@ -6,13 +6,13 @@ import { SectionCTA } from "../shared/SectionCTA";
 import { TitleCTAWrapper } from "../shared/TitleCTAWrapper";
 import { WriteUs } from "../shared/WriteUs/WriteUs";
 
-export const WriteUsSection = ({ data }: HomePageProps) => {
+export const WriteUsSection = ({ data }: {data:{title :string | null} | null | undefined}) => {
     const getTranslation = useTranslations("WriteUs");
     return (
         <SectionCTA className="flex flex-col gap-y-[40px] tab:gap-0 tab:flex-row items-center justify-between">
             <TitleCTAWrapper className="tab:w-[50%] pc:w-[70%]">
-                {data?.ctaSectionWriteUs?.title
-                    ? data.ctaSectionWriteUs.title.toString()
+                {data?.title
+                    ? data.title.toString()
                     : getTranslation("title")}
             </TitleCTAWrapper>
             <WriteUs />
