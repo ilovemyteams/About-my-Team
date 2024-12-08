f67113da14735a2b3c81920ea539e34078bda778
Anna Prutnik <prutnik.anna@gmail.com>
2024-12-08 21:35:48 +0200

    change footer title

--- src/components/footer/TitleFooter/TitleFooter.tsx
+++ src/components/footer/TitleFooter/TitleFooter.tsx
@@ -1,14 +1,24 @@
+import { useLocale, useTranslations } from "next-intl";
+
 import { TitleWrapper } from "../../shared/TitleWrapper";
-import { DesktopTitleFooter } from "./DesktopTitleFooter";
-import { MobileTitleFooter } from "./MobileTitleFooter";
-import { TabletTitleFooter } from "./TabletTitleFooter";
 
 export const TitleFooter = () => {
+    const getTranslation = useTranslations("Footer");
+    const locale = useLocale();
+
+    const pcStyle =
+        locale === "pl"
+            ? "pc:ml-[160px] pc:indent-[-160px]"
+            : "pc:ml-[180px] pc:indent-[-180px]";
+
+    const title = getTranslation.rich("title", {
+        purple: chunk => <span className="text-purple-100">{chunk}</span>,
+    });
     return (
-        <TitleWrapper className="flex flex-col pb-0 mb-9 mt-5  pc:flex-row flex-start tab:pb-0 tab:mb-[98px] pc:mb-[121px] pc:mt-[46px] tab:mt-[40px] desk:mb-[156px]">
-            <MobileTitleFooter />
-            <TabletTitleFooter />
-            <DesktopTitleFooter />
+        <TitleWrapper
+            className={`max-w-[320px] tab:max-w-full tab:whitespace-pre-wrap pb-0 tab:pb-0 mb-9 tab:mb-[98px] pc:mb-[121px] desk:mb-[156px] mt-5 tab:mt-[40px]  pc:mt-[46px] tab:ml-[133px] tab:indent-[-133px] ${pcStyle}`}
+        >
+            {title}
         </TitleWrapper>
     );
 };
