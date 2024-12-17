f2740652547541eb3c8ea1c0c3715892316d254f
IraMira333 <irinat333@gmail.com>
2024-12-17 15:41:15 +0200

    Update ChristmasForm.tsx

--- src/components/christmas/ChristmasForm/ChristmasForm.tsx
+++ src/components/christmas/ChristmasForm/ChristmasForm.tsx
@@ -7,8 +7,8 @@ import { LandingFormValidation } from "@/src/schemas/landingFormValidationSchema
 import { FormInModalProps } from "@/types/FormInModalProps";
 
 import { SubmitButton } from "../../shared/FormElements/SubmitButton";
-import { PolicyLabelEvent } from "../PolicyLabelEvent";
 import { ChristmasCustomField } from "./ChristmasCustomField";
+import { PolicyLabelEvent } from "./PolicyLabelEvent";
 
 export interface ValuesChristmasFormType {
     name: string;
