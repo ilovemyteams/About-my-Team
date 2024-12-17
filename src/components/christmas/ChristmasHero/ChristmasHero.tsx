bc0d75d054099cc08c7066ac10ca0ea25576eb32
Anna Prutnik <prutnik.anna@gmail.com>
2024-12-17 17:21:43 +0200

    style hero decor

--- src/components/christmas/ChristmasHero/ChristmasHero.tsx
+++ src/components/christmas/ChristmasHero/ChristmasHero.tsx
@@ -1,3 +1,4 @@
+import { IconChristmasHeroDecor } from "../../shared/Icons/IconChristmasHeroDecor";
 import LocaleSwitcher from "../../shared/LocaleSwitcher";
 import { PageSection } from "../../shared/PageSection";
 import { ThemeSwitcher } from "../../shared/ThemeSwitcher";
@@ -5,7 +6,11 @@ import { Presents } from "./Presents";
 
 export const ChristmasHero = () => {
     return (
-        <PageSection className="py-8">
+        <PageSection className="py-8 relative">
+            <IconChristmasHeroDecor
+                className="absolute top-0 left-0
+            text-[#B491FF] dark:text-[#D3B0FF] w-[270px] tab:w-[411px] pc:w-[355px] desk:w-[522px] h-auto "
+            />
             <div className="hidden pc:mb-10 pc:flex gap-[204px] justify-end relative z-10">
                 <LocaleSwitcher />
                 <ThemeSwitcher id="HeroSectionThemeSwitcher" />
--- src/components/christmas/ChristmasPage.tsx
+++ src/components/christmas/ChristmasPage.tsx
@@ -2,11 +2,13 @@ import React from "react";
 
 import { ChristmasConditions } from "./ChristmasConditions/ChristmasConditions";
 import { ChristmasCta } from "./ChristmasCta/ChristmasCta";
+import { ChristmasHero } from "./ChristmasHero/ChristmasHero";
 import { ChristmasText } from "./ChristmasText";
 
 export const ChristmasPage = () => {
     return (
         <>
+            <ChristmasHero />
             <ChristmasText />
             <ChristmasConditions />
             <ChristmasCta />
