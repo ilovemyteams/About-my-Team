e45c65e1443e41f33782cc5e58ced486ebafe393
Alex Petrychuk <93341599+petalser@users.noreply.github.com>
2024-12-18 19:32:27 +0100

    Adjusted styling [5]

--- src/components/heroSection/HeroSection.tsx
+++ src/components/heroSection/HeroSection.tsx
@@ -20,7 +20,7 @@ export const HeroSection = () => {
             >
                 <IconLowPolyBall className="absolute top-[41%] right-[92.3%] w-[6.8%] text-purple-200 dark:text-white-200 -z-10" />
                 <IconLowPolyBall className="absolute top-[24%] left-[33.7%] w-[5.4%] text-purple-100 dark:text-purple-130 z-10" />
-                <IconLowPolyBall className="absolute top-[74%] right-[40.5%] tab:top-[77.8%] tab:right-[38.9%] w-[6.9%] tab:w-[10.4%] text-redLight dark:text-red -z-10" />
+                <IconLowPolyBall className="absolute top-[74%] right-[40.5%] tab:top-[77.8%] tab:right-[38.9%] w-[6.9%] tab:w-[10.4%] text-redLight dark:text-red -z-10 tab:z-10" />
                 <IconLowPolyBall className="absolute top-[27.3%] left-[92.7%] w-[7.5%] text-[#ffb700] dark:text-[#ffff40] z-10" />
                 <IconHeroLogo
                     className={` dark:text-white-200 text-purple-200 min-w-[288px] w-full tab:w-[720px] pc:w-[810px] desk:w-[1000px] h-auto`}
