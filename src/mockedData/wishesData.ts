6ce64a868e18d04cf646158123ad2af62016d0b5
IraMira333 <irinat333@gmail.com>
2024-12-18 15:01:32 +0200

    Create wishesData.ts

---
+++ src/mockedData/wishesData.ts
@@ -0,0 +1,69 @@
+export type WishProp = {
+    id: string;
+    ua: string;
+    pl: string;
+    en: string;
+};
+
+export const wishesData: GreetingProp[] = [
+    {
+        id: "oranges-in-a-basket",
+        ua: "Щоб ідеї приносили не тільки лайки, а й реальний прибуток!",
+        pl: "Aby pomysły przynosiły nie tylko polubienia, ale i realne zyski!",
+        en: "That ideas bring not only likes, but also real profit!",
+    },
+    {
+        id: "child-catch-snowflakes",
+        ua: "Нехай кожна маленька ідея стане великим діамантом!",
+        pl: "Niech każdy mały pomysł stanie się wielkim diamentem!",
+        en: "May every little idea become a big diamond!",
+    },
+    {
+        id: "christmas-day-and-a-child",
+        ua: "Бажаємо, щоб кожна сторінка 2025-го була історією, яку хочеться перечитувати!",
+        pl: "Życzymy, aby każda strona roku 2025 była historią, którą będziesz chciał przeczytać ponownie!",
+        en: "We wish every page of 2025 to be a story that you want to re-read!",
+    },
+    {
+        id: "christmas-train",
+        ua: "Нехай мозок працює, як Google: швидко, продуктивно і завжди дає потрібні відповіді!",
+        pl: "Niech Twój mózg działa jak Google: szybko, produktywnie i zawsze daje Ci właściwe odpowiedzi!",
+        en: "Let your brain work like Google: fast, productive and always giving you the right answers!",
+    },
+    {
+        id: "christmas-tree-in-the-village",
+        ua: "Відважно йдіть до вершини, але не забувайте насолоджуватися краєвидами на шляху!",
+        pl: "Idź odważnie na szczyt, ale nie zapomnij cieszyć się widokami po drodze!",
+        en: "Go bravely to the top, but don't forget to enjoy the views along the way!",
+    },
+    {
+        id: "cup-with-marshmallow",
+        ua: "Нехай усі понеділки в новому році будуть святами!",
+        pl: "Niech wszystkie poniedziałki w nowym roku będą dniami wolnymi!",
+        en: "May all Mondays in the new year be holidays!",
+    },
+    {
+        id: "snow-angel",
+        ua: "Танцюйте так, ніби ніхто не бачить, і працюйте так, ніби всі чекають вашого успіху!",
+        pl: "Tańcz tak, jakby nikt nie patrzył i pracuj tak, jakby wszyscy czekali na Twój sukces!",
+        en: "Dance as if no one is watching and work as if everyone is waiting for your success!",
+    },
+    {
+        id: "snowman",
+        ua: "Щоб святковий настрій тримався довше, ніж різдвяні вихідні!",
+        pl: "Niech świąteczny nastrój trwa dłużej niż świąteczny weekend!",
+        en: "May the festive mood last longer than the Christmas weekend!",
+    },
+    {
+        id: "sparkling",
+        ua: "У 2025-му бажаємо більше “DONE” і менше “TODO”!",
+        pl: "W 2025 roku życzymy Ci więcej „ZROBIONE” i mniej „TODO”!",
+        en: "In 2025, we wish you more “DONE” and less “TODO”!",
+    },
+    {
+        id: "sunny-winter-day",
+        ua: "Хай щастя падає, як сніг у казці, і не тане цілий рік!",
+        pl: "Niech szczęście pada jak śnieg w bajce i nie topnieje przez cały rok!",
+        en: "May happiness fall like snow in a fairy tale and not melt all year round!",
+    },
+];
