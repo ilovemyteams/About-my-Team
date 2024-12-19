63d5b40ce7ef55585cdd71072a1e7688c47fb094
IraMira333 <irinat333@gmail.com>
2024-12-19 13:57:59 +0200

    Added logic for randomly selecting wishes for New Year's balls

--- src/components/christmas/ChristmasHero/ChristmasToyButton.tsx
+++ src/components/christmas/ChristmasHero/ChristmasToyButton.tsx
@@ -1,8 +1,12 @@
 "use client";
 
-import { ComponentType } from "react";
+import { ComponentType, useEffect, useState } from "react";
 
 import { IconProps } from "@/types/iconProps.interface";
+import Link from "next/link";
+import { wishRandomizer } from "@/src/utils/wishRandomizer";
+import { wishesData } from "@/src/mockedData/wishesData";
+import { useLocale } from "next-intl";
 
 interface ChristmasToyButtonProps {
     id: string;
@@ -15,17 +19,20 @@ export const ChristmasToyButton = ({
     id,
     icon: Icon,
 }: ChristmasToyButtonProps) => {
-    const onClickBtn = () => {
-        //Logic for clock on tree toys
-    };
+    const [randomWishId, setRandomWishId] = useState<string | null>(null);
+    useEffect(() => {
+        setRandomWishId(wishRandomizer(wishesData));
+    }, []);
+
+    const locale = useLocale();
     return (
-        <button
-            onClick={onClickBtn}
+        <Link
+            href={`/${locale}/events/wish/${randomWishId}`}
             id={id}
             aria-label={id}
             className={`absolute cursor-pointer active:scale-[110%] tab:hover:scale-[130%] ${className}`}
         >
             <Icon className="w-full h-auto" />
-        </button>
+        </Link>
     );
 };
