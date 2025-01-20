99dd587d09763af063a484ac593b54557c019598
Anna Prutnik <prutnik.anna@gmail.com>
2025-01-20 09:50:12 +0200

    change animation for purple text in mobile devices

--- src/components/servicePage/ServiceHero/AnimatedTitle.tsx
+++ src/components/servicePage/ServiceHero/AnimatedTitle.tsx
@@ -1,9 +1,7 @@
 "use client";
 
 import { motion } from "framer-motion";
-import { useLayoutEffect, useRef } from "react";
 
-import { SCREEN_SIZES } from "@/src/constants/screenSizes";
 import { HighlightPurpleItemType } from "@/src/utils/highlightingPurple";
 
 import { AnimatedSubtext } from "./AnimatedSubtext";
@@ -13,22 +11,6 @@ interface AnimatedTitleProps {
 }
 
 export const AnimatedTitle = ({ title }: AnimatedTitleProps) => {
-    const isMobile = useRef(false);
-    useLayoutEffect(() => {
-        const setDevice = () => {
-            isMobile.current = window?.innerWidth < SCREEN_SIZES.tablet;
-        };
-        setDevice();
-    }, []);
-
-    const initialTransformAnimation = isMobile.current
-        ? { x: "50%", marginLeft: 0 }
-        : { x: "0", marginLeft: "30%" };
-
-    const animateTransformationAnimation = isMobile.current
-        ? { x: ["50%", "50%", 0], marginLeft: [0, 0, 0] }
-        : { x: [0, 0, 0], marginLeft: ["30%", "30%", 0] };
-
     return (
         <div className="absolute top-0 left-0 w-full h-full" aria-hidden>
             {title.map((item, i) => {
@@ -36,18 +18,31 @@ export const AnimatedTitle = ({ title }: AnimatedTitleProps) => {
                     return (
                         <motion.span
                             key={i}
-                            className="text-purple-100 inline-block"
+                            className="text-purple-100 inline-block [--marginLeft-from:0] tab:[--marginLeft-from:30%] [--scale-from:0] tab:[--scale-from:1] "
                             initial={{
                                 opacity: 0,
-                                ...initialTransformAnimation,
+                                marginLeft: "var(--marginLeft-from)",
+                                scale: "var(--scale-from)",
                             }}
                             animate={{
-                                opacity: [0.5, 1, 1],
-                                ...animateTransformationAnimation,
+                                opacity: 1,
+                                marginLeft: 0,
+                                scale: 1,
                             }}
                             transition={{
-                                duration: 1,
-                                ease: "easeOut",
+                                marginLeft: {
+                                    delay: 0.5,
+                                    duration: 0.5,
+                                    ease: "easeOut",
+                                },
+                                opacity: {
+                                    duration: 0.5,
+                                    ease: "easeOut",
+                                },
+                                scale: {
+                                    duration: 1,
+                                    ease: "easeOut",
+                                },
                             }}
                         >
                             {item.text}
