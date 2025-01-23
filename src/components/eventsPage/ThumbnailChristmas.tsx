e721c85f6942ee9de25249b3cc838b37a050f33d
Alex Petrychuk <93341599+petalser@users.noreply.github.com>
2025-01-23 23:36:47 +0100

    Component no longer responsive

--- src/components/eventsPage/ThumbnailChristmas.tsx
+++ src/components/eventsPage/ThumbnailChristmas.tsx
@@ -5,7 +5,7 @@ import { IconChristmasTree } from "../shared/Icons/IconChristmasTree";
 import { ThumbnailBg } from "./ThumbnailBG";
 export const ThumbnailChristmas = () => {
     return (
-        <div className="relative  w-auto h-[200px] flex border">
+        <div className="relative w-full max-w-[280px] mx-auto aspect-[280/200] tab:aspect-[300/200] flex">
             <IconChristmasTree className="absolute w-auto h-full bottom-[-.5rem] right-[7%] z-[3]" />
             <IconChristmasBubbles className="absolute w-[30%] h-auto bottom-0 left-0 z-[2]" />
             <IconChristmasLights className="absolute w-[37%] h-auto top-0 right-0 z-[4]" />
