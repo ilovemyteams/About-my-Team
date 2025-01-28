0c90dc4ff8748ef1e0085765e9b3dc315de458fb
Alex Petrychuk <93341599+petalser@users.noreply.github.com>
2025-01-28 22:34:27 +0100

    Updated text visibility and spacing

--- src/components/eventsPage/EventCard.tsx
+++ src/components/eventsPage/EventCard.tsx
@@ -28,7 +28,7 @@ export const EventCard = async ({ content }: EventCardProps) => {
     );
 
     return (
-        <div className="flex flex-col tab:flex-row py-4 gap-3 tab:gap-3 border-b-1 border-purple-strokeLight dark:border-purple-stroke">
+        <div className="flex flex-col tab:flex-row py-4 gap-3 tab:gap-3 border-b-1 border-purple-strokeLight dark:border-purple-stroke mt-3 tab:mt-2.5">
             <div
                 className={`w-full tab:w-[300px] desk:w-[375px] shrink-0 grow-0 relative overflow-hidden`}
             >
@@ -40,9 +40,16 @@ export const EventCard = async ({ content }: EventCardProps) => {
                     <h2 className="pc:focus:text-redLight dark:pc:focus:text-red focus:outline-none  dark:active:text-red active:text-redLight pc:transition pc:ease-out pc:duration-300 font-caviar text-purple-200 dark:text-white-200 text-lg tab:text-xlb desk:text-2xlb line-clamp-2 mb-3">
                         {title}
                     </h2>
-                    <p className="text-sm tab:text-base line-clamp-3 text-greyLight dark:text-grey mb-2.5">
-                        {description}
-                    </p>
+                    {description.map((item, index) => {
+                        return (
+                            <p
+                                key={index}
+                                className="text-sm tab:text-base text-greyLight dark:text-grey mb-2.5"
+                            >
+                                {item}
+                            </p>
+                        );
+                    })}
                 </div>
                 <p>{date}</p>
             </div>
