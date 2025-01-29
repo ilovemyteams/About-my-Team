d70bc7f9c85ecda75b1da4ecbd760a6dc6dfe3e2
Alex Petrychuk <93341599+petalser@users.noreply.github.com>
2025-01-29 11:03:14 +0100

    Updated spacing

--- src/components/eventsPage/PastEvents.tsx
+++ src/components/eventsPage/PastEvents.tsx
@@ -8,7 +8,7 @@ import { EventCard } from "./EventCard";
 export const PastEvents = () => {
     const getTranslation = useTranslations();
     return (
-        <PageSection className="pb-15 tab:pb-10 pc:pb-[40px] desk:pb-12">
+        <PageSection className="pb-[60px] tab:pb-10 desk:pb-12">
             <div className="mb-[21px] tab:mb-12 pc:mb-[56px]">
                 <h1 className="font-caviar text-purple-200 dark:text-grey text-3xl tab:text-4xl pc:text-6xl desk:text-7xl">
                     {getTranslation("PastEvents.pageBigTitle")}
