512bc104435109795515c55d0ee7a08368a53c4f
Alex Petrychuk <93341599+petalser@users.noreply.github.com>
2025-01-30 21:02:57 +0100

    Updated target date

--- src/app/[locale]/events/page.tsx
+++ src/app/[locale]/events/page.tsx
@@ -1,7 +1,7 @@
 import { PastEvents } from "@/src/components/eventsPage/PastEvents";
 import { generatePageMetadata } from "@/src/utils/generateMetaData";
 
-import { Countdown } from "./countdown/Countdown";
+import { Countdown } from "../../../components/eventsPage/countdown/Countdown";
 
 export async function generateMetadata({
     params: { locale },
@@ -19,7 +19,7 @@ export default function Services() {
     return (
         <>
             {/* <Header/> */}
-            <Countdown targetDate="2025-02-14T10:00:00+02:00" />
+            <Countdown targetDate="2025-02-10T10:00:00+02:00" />
             <PastEvents />
         </>
     );
