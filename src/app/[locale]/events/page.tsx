7f05f75eb3c198fd57a30193123ce6a4ae51c400
Alex Petrychuk <93341599+petalser@users.noreply.github.com>
2025-01-30 21:28:44 +0100

    Updated metadata

--- src/app/[locale]/events/page.tsx
+++ src/app/[locale]/events/page.tsx
@@ -10,7 +10,7 @@ export async function generateMetadata({
 }) {
     return generatePageMetadata({
         locale,
-        namespace: "Christmas",
+        namespace: "Events",
         canonical: "/events",
     });
 }
