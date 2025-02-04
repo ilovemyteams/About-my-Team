99fd24935eecc9dc92f8c46e2c226aaa1f6f90b1
Alex Petrychuk <93341599+petalser@users.noreply.github.com>
2025-02-04 15:24:08 +0100

    Stylish Hero

--- src/components/heroSection/HeroSection.tsx
+++ src/components/heroSection/HeroSection.tsx
@@ -15,10 +15,10 @@ export const HeroSection = () => {
                 <LocaleSwitcher />
                 <ThemeSwitcher id="HeroSectionThemeSwitcher" />
             </div>
-            <div className="absolute top-[80px] tab:top-[68px] pc:-top-3 desk:-top-1 left-[32.5%] tab:left-[28%] pc:left-[21.5%] desk:left-[26%] min-w-[111px] w-[35%] tab:min-w-[340px] tab:w-[44%] pc:min-w-[412px] pc:w-[33%] desk:min-w-[470px] desk:w-[32%]  overflow-hidden">
+            <div className="absolute top-[80px] tab:top-[68px] pc:-top-3 desk:-top-1 left-[32.5%] tab:left-[28%] pc:left-[21.5%] desk:left-[26%] w-[111px] tab:w-[340px] pc:w-[412px] desk:w-[470px] overflow-hidden">
                 <HeaderDecoration className="w-[153%] tab:w-[100%]" />
             </div>
-            <div className="absolute top-[100px] tab:top-[101px] pc:top-[60px] desk:top-[87px] left-[7%] tab:left-[13%] pc:left-[12.5%] desk:left-[7.5%] w-[9%] tab:w-[6%] pc:w-[3%] rotate-[-32deg] pc:rotate-[-23deg]">
+            <div className="absolute top-[100px] tab:top-[101px] pc:top-[60px] desk:top-[77px] left-[7%] tab:left-[13%] pc:left-[12.5%] desk:left-[7.5%] w-[29px] tab:w-[46px] pc:w-[36px] desk:w-[75px] rotate-[-32deg] pc:rotate-[-23deg]">
                 <IconEnvelopeCheap />
             </div>
             <div className="absolute top-[97px] tab:top-[102px] pc:top-[120px] right-[12%] tab:right-[8%] pc:right-[16%] desk:right-[13.5%] w-[12%] tab:w-[8%] pc:w-[6%]  rotate-[17deg] tab:rotate-[42deg] pc:rotate-[-4deg] desk:rotate-[-1deg]">
