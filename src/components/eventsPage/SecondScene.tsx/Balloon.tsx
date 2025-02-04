6b2859bdd6a4df1979204cfc18d02ecca96832c7
IraMira333 <irinat333@gmail.com>
2025-02-04 23:15:17 +0200

    Create Balloon.tsx

---
+++ src/components/eventsPage/SecondScene.tsx/Balloon.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import Image from "next/image";
+import { useEffect, useState } from "react";
+
+import { wishesData } from "@/src/mockedData/wishesData";
+import { Link } from "@/src/navigation";
+import { wishRandomizer } from "@/src/utils/wishRandomizer";
+
+interface BalloonButtonProps {
+    id: string;
+    className: string;
+    heartPNG: string;
+}
+
+export const Balloon = ({ className, id, heartPNG }: BalloonButtonProps) => {
+    const [randomWishId, setRandomWishId] = useState<string | null>(null);
+    useEffect(() => {
+        setRandomWishId(wishRandomizer(wishesData));
+    }, []);
+
+    return (
+        <Link
+            href={`/events/wish/${randomWishId}`}
+            id={id}
+            aria-label={id}
+            className={`absolute cursor-pointer active:scale-[110%] tab:hover:scale-[130%] ${className} transition ease-out duration-300`}
+        >
+            <Image
+                src={heartPNG}
+                alt="heart-shaped balloon"
+                width={306}
+                height={299}
+                className={`w-full h-auto`}
+            />
+        </Link>
+    );
+};
