f629f2d7c685788d20b30a812471deeb7284c88b
Anna Prutnik <prutnik.anna@gmail.com>
2025-02-06 16:26:27 +0200

    create array for animation

--- src/components/eventsPage/valentines/ValentinesActivity.tsx
+++ src/components/eventsPage/valentines/ValentinesActivity.tsx
@@ -22,14 +22,42 @@ export const ValentinesActivity = () => {
         const x = heartWidth / 3 + heartLeftPosition;
         const y = heartHeight / 5 + heartTopPosition;
 
-        // const containerHeight = containerRef.current?.offsetHeight || 0;
-        // const containerWidth = containerRef.current?.offsetWidth || 0;
+        const containerHeight = containerRef.current?.offsetHeight || 0;
+        const containerWidth = containerRef.current?.offsetWidth || 0;
 
-        // const pathToBottom = containerHeight - y;
+        const pathToBottom = containerHeight - y;
 
-        // const stepY = Math.floor(pathToBottom / 50);
-        // const totalSteps = 60;
+        const stepY = Math.floor(pathToBottom / 60);
+        const totalSteps = 60;
 
+        const yStepsArray = Array.from({ length: totalSteps }, (_, index) => {
+            let position = 0;
+            if (index < 6) {
+                position = y - stepY * (index + 1);
+            } else if (index < 10) {
+                position = y - stepY * 6 + stepY * (index + 1);
+            } else {
+                position = y + stepY * 11;
+            }
+            return `${position}px`;
+        });
+        const xStepsArray = Array.from({ length: totalSteps }, (_, index) => {
+            let position = 0;
+            if (index < 6) {
+                position = x - containerWidth * 0.02 * (index + 1);
+            } else if (index < 10) {
+                position =
+                    x -
+                    containerWidth * 0.12 +
+                    containerWidth * 0.02 * (index + 1);
+            } else {
+                position =
+                    x - containerWidth * 0.12 + containerWidth * 0.02 * 11;
+            }
+            return `${position}px`;
+        });
+
+        console.log(yStepsArray);
         if (
             heartRef.current &&
             leftPartOfHeartRef.current &&
@@ -53,6 +81,7 @@ export const ValentinesActivity = () => {
             await animate(
                 certificateRef.current,
                 {
+                    zIndex: [-1, -1, -1, -1],
                     opacity: [0, 0, 0, 1],
                     y: [0, `${y}px`, `${y}px`, `${y}px`],
                     x: [0, `${x}px`, `${x}px`, `${x}px`],
@@ -61,14 +90,15 @@ export const ValentinesActivity = () => {
                     duration: 0.5,
                 }
             );
+
             await animate(
                 certificateRef.current,
                 {
-                    y: [`${y}px`, `${y - 40}px`, `${y - 80}px`],
-                    x: [`${x}px`, `${x - 60}px`, `${x - 40}px`],
+                    y: yStepsArray,
+                    x: xStepsArray,
                 },
                 {
-                    duration: 3,
+                    duration: 15,
                 }
             );
         }
@@ -86,7 +116,7 @@ export const ValentinesActivity = () => {
             <ClickCertificateSection />
 
             <div
-                className={`absolute w-[100px] tab:w-[180px] pc:w-[210px] desk:w-[320px] opacity-0 top-0 left-0`}
+                className={`absolute w-[100px] tab:w-[180px] pc:w-[210px] desk:w-[320px] opacity-0 top-0 left-0 `}
                 ref={certificateRef}
             >
                 <Certificate />
