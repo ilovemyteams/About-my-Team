81ab102d2a5a3607cb287492a40fb0f211bcc8ba
IraMira333 <irinat333@gmail.com>
2025-02-08 15:40:34 +0200

    Update Switchers.tsx

--- src/components/eventsPage/Switchers.tsx
+++ src/components/eventsPage/Switchers.tsx
@@ -6,7 +6,7 @@ export const Switchers = () => {
         <div className="container max-w-[540px] tab:max-w-full desk:max-w-[1456px] scroll-mt-[90px] pc:scroll-mt-[30px] pt-8 tab:pt-12 pc:pt-8 pb-[111px] tab:pb-[100px] pc:pb-12">
             <div className="hidden pc:mb-10 pc:flex gap-[204px] justify-end relative z-10">
                 <LocaleSwitcher />
-                <ThemeSwitcher id="HeroSectionThemeSwitcher" />
+                <ThemeSwitcher id="DesktopHeaderMenuThemeSwitcher" />
             </div>
         </div>
     );
