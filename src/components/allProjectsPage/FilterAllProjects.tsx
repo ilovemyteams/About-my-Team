3593fb0af3be806aa2480101250c0a3ffff0a0a7
IraMira333 <irinat333@gmail.com>
2025-02-27 17:28:57 +0200

    Create FilterAllProjects.tsx

---
+++ src/components/allProjectsPage/FilterAllProjects.tsx
@@ -0,0 +1,5 @@
+export const FilterAllProjects = () => {
+    return (
+        <div className="w-[288px] h-12 border dark:border-purple-stroke border-purple-strokeLight mb-6 tab:mb-11 pc:mb-12"></div>
+    );
+};
