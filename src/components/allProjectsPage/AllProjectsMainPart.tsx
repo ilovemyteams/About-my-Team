8614dc86cdafddc03bbc9cf23f641f47559f8724
IraMira333 <irinat333@gmail.com>
2025-03-05 21:40:05 +0200

    Changed the ViewCaseButton position from pc screens

--- src/components/allProjectsPage/AllProjectsMainPart.tsx
+++ src/components/allProjectsPage/AllProjectsMainPart.tsx
@@ -8,12 +8,9 @@ export const AllProjectsMainPart = () => {
     return (
         <PageSection className="mb-[80px] tab:mb-[100px] desk:mb-[120px] items-center pc:pb-[60px]">
             <FilterAllProjects />
-            <ul className="tab:grid flex-[0_0_100%] w-full tab:grid-cols-2 tab:gap-10 pc:gap-[60px] desk:gap-10">
+            <ul className="flex flex-col gap-6 tab:grid flex-[0_0_100%] w-full tab:grid-cols-2 tab:gap-10 pc:gap-[60px] desk:gap-10">
                 {allProjectsData.map(item => (
-                    <li
-                        key={item.data.id}
-                        className="mb-6 tab:mb-10 pc:mb-[60px] desk:mb-10"
-                    >
+                    <li key={item.data.id}>
                         <ProjectCard data={item} />
                     </li>
                 ))}
