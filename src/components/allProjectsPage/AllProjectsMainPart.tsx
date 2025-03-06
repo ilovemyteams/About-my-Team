eb006347b216e4a6b3853984738094b622a986fd
Alex Petrychuk <93341599+petalser@users.noreply.github.com>
2025-03-06 11:58:14 +0100

    Implemented pagination on portfolio page

--- src/components/allProjectsPage/AllProjectsMainPart.tsx
+++ src/components/allProjectsPage/AllProjectsMainPart.tsx
@@ -1,21 +1,43 @@
+import { usePaginationData } from "@/src/hooks/usePaginationData";
 import { allProjectsData } from "@/src/mockedData/allProjectsData";
 
 import { PageSection } from "../shared/PageSection";
+import { Pagination } from "../shared/Pagination";
 import { FilterAllProjects } from "./FilterAllProjects";
 import { ProjectCard } from "./ProjectCard";
 import { ProjectStub } from "./ProjectStub";
 
-export const AllProjectsMainPart = () => {
+interface AllProjectsMainPartProps {
+    searchParams: { page?: string };
+}
+
+const ITEMS_PER_PAGE = 6;
+
+export const AllProjectsMainPart = ({
+    searchParams,
+}: AllProjectsMainPartProps) => {
+    const pageNumber = searchParams?.page ? parseInt(searchParams.page) : 1;
+    const {
+        dataSlice: portfolios,
+        totalPages,
+        isPaginationNeeded,
+    } = usePaginationData(allProjectsData, ITEMS_PER_PAGE, pageNumber);
+
+    console.log(JSON.stringify(searchParams), ">>num");
+
     return (
         <PageSection className="mb-[80px] tab:mb-[100px] desk:mb-[120px] items-center pc:pb-[60px]">
             <FilterAllProjects />
-            <ul className="flex flex-col gap-6 tab:grid flex-[0_0_100%] w-full tab:grid-cols-2 tab:gap-10 pc:gap-[60px] desk:gap-10">
-                {allProjectsData.map(item => (
-                    <li key={item.data.id}>
+            <ul className="tab:grid flex-[0_0_100%] w-full tab:grid-cols-2 tab:gap-10 pc:gap-[60px] desk:gap-10">
+                {portfolios.map(item => (
+                    <li
+                        key={item.data.id}
+                        className="mb-6 tab:mb-10 pc:mb-[60px] desk:mb-10"
+                    >
                         <ProjectCard data={item} />
                     </li>
                 ))}
-                {allProjectsData.length < 6 && (
+                {portfolios.length < 6 && (
                     <li
                         key={`${allProjectsData.length < 10 ? "0" : ""}${allProjectsData.length + 1}`}
                         className="mb-6 tab:mb-10 pc:mb-[60px] desk:mb-10"
@@ -24,6 +46,11 @@ export const AllProjectsMainPart = () => {
                     </li>
                 )}
             </ul>
+
+            {isPaginationNeeded && (
+                // It is not real NextUI pagination component
+                <Pagination total={totalPages} currentPage={pageNumber} />
+            )}
         </PageSection>
     );
 };
--- src/mockedData/allProjectsData.ts
+++ src/mockedData/allProjectsData.ts
@@ -122,4 +122,109 @@ export const allProjectsData: ProjectDataItemType[] = [
             subtitle: "Strona internetowa dla zespołu IT",
         },
     },
+    //break
+    {
+        data: {
+            id: "051",
+            slug: "viktoriia-zabara",
+            siteLink: "https://viktoriia-zabara.netlify.app/",
+            image: "https://drive.google.com/uc?export=view&id=18RWpznE25pfHZpFXStzp4MujPpm0cwbI",
+            category: "landing",
+        },
+
+        ua: {
+            title: "ІТ рекрутер",
+            subtitle: "Лендінг для IT-рекрутера",
+        },
+        en: {
+            title: "IT Recruiter",
+            subtitle: "Landing Page for an IT Recruiter",
+        },
+        pl: { title: "Rekruter IT", subtitle: "Landing page dla rekrutera IT" },
+    },
+    {
+        data: {
+            id: "041",
+            slug: "protection-in-ua",
+            siteLink: "https://protection.in.ua/",
+            image: "https://drive.google.com/uc?export=view&id=11FGZgLaN9t04MQCCBGyVIveLNszNZaY6",
+            category: "website",
+        },
+
+        ua: {
+            title: "Міграційний адвокат",
+            subtitle: "Вебсайт для міграційного адвоката",
+        },
+        en: {
+            title: "Migration Lawyer",
+            subtitle: "Website for a Migration Lawyer",
+        },
+        pl: {
+            title: "Prawnik ds. migracji",
+            subtitle: "Strona internetowa dla prawnika ds. migracji",
+        },
+    },
+    {
+        data: {
+            id: "031",
+            slug: "theatermag-com-ua",
+            siteLink: "https://theatermag.com.ua/",
+            image: "https://drive.google.com/uc?export=view&id=1VQJcG8uJi6bSKhlDfY9bd3608dxhGJTQ",
+            category: "website",
+        },
+
+        ua: {
+            title: "Український театр",
+            subtitle: "Вебсайт для ГО “Український театр”",
+        },
+        en: {
+            title: "Ukrainian Theater",
+            subtitle: `Website for the NGO "Ukrainian Theater"`,
+        },
+        pl: {
+            title: "Ukraiński Teatr",
+            subtitle: `Strona internetowa dla NGO "Ukraiński Teatr"`,
+        },
+    },
+    {
+        data: {
+            id: "021",
+            slug: "batatfarm-com",
+            siteLink: "https://batatfarm.com/",
+            image: "https://drive.google.com/uc?export=view&id=1tgltGLTRcbdHia5fjV4K6sFVJlOyi14l",
+            category: "shop",
+        },
+
+        ua: {
+            title: "Смачно! на селі",
+            subtitle: "Інтернет магазин натуральних продуктів",
+        },
+        en: {
+            title: "Smachno! na seli",
+            subtitle: "Online Store for Natural Products",
+        },
+        pl: {
+            title: "Smachno! na seli",
+            subtitle: "Sklep internetowy z produktami naturalnymi",
+        },
+    },
+    {
+        data: {
+            id: "011",
+            slug: "ilovemyteam-online",
+            siteLink: "https://www.ilovemyteam.online/",
+            image: "https://drive.google.com/uc?export=view&id=1euhTPDYseSUaGESWCxCcdZ0IAlp-mGVX",
+            category: "website",
+        },
+
+        ua: {
+            title: "i love my team",
+            subtitle: "Вебсайт для IT команди",
+        },
+        en: { title: "i love my team", subtitle: "Website for an IT Team" },
+        pl: {
+            title: "i love my team",
+            subtitle: "Strona internetowa dla zespołu IT",
+        },
+    },
 ];
