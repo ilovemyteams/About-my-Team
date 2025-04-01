f971146383afe01f706216c7c47a7a834902ce15
IraMira333 <irinat333@gmail.com>
2025-04-01 22:11:21 +0300

    Create GreetingPortal.tsx

---
+++ src/components/easter/greetingModal/GreetingPortal.tsx
@@ -0,0 +1,57 @@
+"use client";
+
+import { AnimatePresence } from "framer-motion";
+import { useLocale } from "next-intl";
+import { useEffect, useState } from "react";
+
+import { GreetingModal } from "./GreetingModal";
+
+const SESSION_LANG_KEY = "langOpened";
+
+export const GreetingPortal = () => {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const locale = useLocale();
+
+    const onCloseModal = () => setIsModalOpen(false);
+
+    useEffect(() => {
+        const langOpened = sessionStorage.getItem(SESSION_LANG_KEY);
+
+        if (langOpened) {
+            const sessionLang = langOpened.split(",");
+            const isInclude = sessionLang.includes(locale);
+
+            if (!isInclude) {
+                sessionLang.push(locale);
+                setIsModalOpen(true);
+                sessionStorage.setItem(SESSION_LANG_KEY, sessionLang.join(","));
+            }
+        }
+
+        if (!langOpened) {
+            setIsModalOpen(true);
+            sessionStorage.setItem(SESSION_LANG_KEY, locale);
+        }
+    }, [locale]);
+
+    useEffect(() => {
+        const handleEsc = (event: KeyboardEvent) => {
+            if (event.key === "Escape") {
+                onCloseModal();
+            }
+        };
+
+        document.addEventListener("keydown", handleEsc);
+
+        return () => {
+            document.removeEventListener("keydown", handleEsc);
+        };
+    }, []);
+
+    return (
+        <AnimatePresence>
+            {isModalOpen && <GreetingModal onCloseModal={onCloseModal} />}
+        </AnimatePresence>
+    );
+};
