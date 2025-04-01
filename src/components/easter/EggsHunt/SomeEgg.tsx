d54c5493352d9f8a81c5e5329bcf5d05e0ff17e4
IraMira333 <irinat333@gmail.com>
2025-04-01 16:41:35 +0300

    Create SomeEgg.tsx

---
+++ src/components/easter/EggsHunt/SomeEgg.tsx
@@ -0,0 +1,27 @@
+import { IconEggGreen } from "../icons/IconEggGreen";
+import { IconEggMulticolored } from "../icons/IconEggMulticolored";
+import { IconEggMulticolored2 } from "../icons/IconEggMulticolored2";
+import { IconEggMulticolored3 } from "../icons/IconEggMulticolored3";
+import { IconEggOrange } from "../icons/IconEggOrange";
+
+interface SomeEggProps {
+    x: number;
+    y: number;
+    kindOfEgg: number;
+}
+const eggIcons = [
+    IconEggMulticolored,
+    IconEggMulticolored2,
+    IconEggMulticolored3,
+    IconEggGreen,
+    IconEggOrange,
+];
+
+export const SomeEgg = ({ x, y, kindOfEgg }: SomeEggProps) => {
+    const EggIcon = eggIcons[kindOfEgg - 1];
+    return (
+        <div className="absolute z-10" style={{ top: `${y}%`, left: `${x}%` }}>
+            <EggIcon className="w-12 h-auto absolute" />
+        </div>
+    );
+};
