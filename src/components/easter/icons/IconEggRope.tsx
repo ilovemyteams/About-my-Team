69578df8e846c00d32c0c0a160081922e9e129e8
IraMira333 <irinat333@gmail.com>
2025-04-03 20:31:04 +0300

    Update IconEggRope.tsx

--- src/components/easter/icons/IconEggRope.tsx
+++ src/components/easter/icons/IconEggRope.tsx
@@ -11,15 +11,15 @@ export const IconEggRope = ({ className }: IconProps) => {
             className={className}
             aria-label="A decor egg icon"
         >
-            <path d="M27 93V1" stroke="#C7C0F3" stroke-linecap="round" />
+            <path d="M27 93V1" stroke="currentColor" stroke-linecap="round" />
             <path
                 d="M8.82248 82C15.7295 82 27 93.4614 27 93.4614C27 93.4614 5.22772 97.6711 1.91502 92.1639C-0.509705 88.1328 1.91546 82 8.82248 82Z"
-                stroke="#C7C0F3"
+                stroke="currentColor"
                 stroke-width="0.5"
             />
             <path
                 d="M45.8767 81C38.704 81 27 93.343 27 93.343C27 93.343 49.6097 97.8766 53.0498 91.9457C55.5678 87.6046 53.0493 81 45.8767 81Z"
-                stroke="#C7C0F3"
+                stroke="currentColor"
                 stroke-width="0.5"
             />
         </svg>
