c18840161345a6f16e527398a406126454ac772b
IraMira333 <irinat333@gmail.com>
2025-04-04 12:41:31 +0300

    Update Basket.tsx

--- src/components/easter/Basket.tsx
+++ src/components/easter/Basket.tsx
@@ -7,14 +7,14 @@ export const Basket = () => {
     return (
         <Link
             href="/events"
-            className=" relative w-8 tab:w-10 desk:w-[50px] h-[50px] tab:h-[60px] desk:h-[76px] flex justify-center"
+            className=" relative w-8 tab:w-[36px] desk:w-[50px] h-[50px] tab:h-[50px] desk:h-[76px] flex justify-center"
         >
             <Image
                 src="/images/easter/upper-part-basket.svg"
                 alt="basket handle"
                 width={44}
                 height={40}
-                className=" absolute w-full top-0 z-[-1]"
+                className=" absolute w-full top-0 z-[-1] tab:w-8 desk:w-full"
             />
             <motion.div
                 animate={{
@@ -26,7 +26,7 @@ export const Basket = () => {
                     repeat: Infinity,
                     ease: "easeInOut",
                 }}
-                className="w-5 tab:w-6 desk:w-8 h-auto absolute bottom-1 tab:bottom-[10px] desk:bottom-4"
+                className="w-5 tab:w-6 desk:w-8 h-auto absolute bottom-1 tab:bottom-[8px] desk:bottom-4"
                 style={{
                     transformOrigin: "bottom center",
                 }}
