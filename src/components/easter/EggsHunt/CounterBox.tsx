3682017ea435af63083915b0b52a636963f15b56
IraMira333 <irinat333@gmail.com>
2025-04-07 23:43:06 +0300

    Fixed size of X

--- src/components/easter/EggsHunt/CounterBox.tsx
+++ src/components/easter/EggsHunt/CounterBox.tsx
@@ -8,7 +8,7 @@ export const CounterBox = () => {
     return (
         <div
             className="px-[11px] py-[7px] w-[72px] h-[42px] items-center text-base tab:text-2xl desk:text-3xl tab:leading-6 desk:leading-7 leading-4 justify-center
-            tab:w-[107px] tab:h-[62px] desk:w-[129px] desk:h-[76px]
+            tab:w-[107px] tab:h-[62px] desk:w-[129px] desk:h-[76px] desk:font-light
             dark:bg-CTAGradient bg-CTAGradientLight text-purple-200 dark:text-grey border border-t-0 tab:border-t-1 border-purple-strokeLight dark:border-purple-stroke
         flex"
         >
@@ -20,7 +20,9 @@ export const CounterBox = () => {
                     height={193}
                     className="w-[14px] h-auto tab:w-[22px] desk:w-[26px]"
                 />
-                <p className="">x</p>
+                <p className="text-xxs tab:text-base mb-[3px] desk:text-lg">
+                    x
+                </p>
                 <p className="">{totalFoundEggs}</p>
             </div>
         </div>
