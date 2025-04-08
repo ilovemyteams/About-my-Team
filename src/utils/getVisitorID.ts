83e18edc7d1305c3594785e4427a774c7e2adbd9
IraMira333 <irinat333@gmail.com>
2025-04-08 14:02:31 +0300

    Delete getVisitorID.ts

--- src/utils/getVisitorID.ts
+++
@@ -1,7 +0,0 @@
-import FingerprintJS from "@fingerprintjs/fingerprintjs";
-
-export const getFingerprintId = async () => {
-    const fp = await FingerprintJS.load();
-    const result = await fp.get();
-    return result.visitorId;
-};
