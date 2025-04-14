d5c23d89ef1ed9a0fe7ea4fc6c4f0916e96eddaa
IraMira333 <irinat333@gmail.com>
2025-04-14 13:52:24 +0300

    Deleted useless texts & images

--- src/utils/normalizeGoogleDriveImgUrls.ts
+++
@@ -1,7 +0,0 @@
-//TODO: delete this file once images come from Sanity
-export const normalizeGoogleDriveImgUrls = (url: string): string => {
-    const RAW_URL1 = url.split("/d/");
-    const RAW_URL2 = RAW_URL1[1].split("/view");
-    const IMAGE_ID = RAW_URL2[0];
-    return `https://drive.google.com/thumbnail?id=${IMAGE_ID}`;
-};
