1e9b7d09976a8aab9c6c8275cf55671fecbef5c7
IraMira333 <irinat333@gmail.com>
2025-04-18 22:36:47 +0300

    Update FeedbackProject.tsx

--- src/components/someProjectPage/FeedbackSection/FeedbackProject.tsx
+++ src/components/someProjectPage/FeedbackSection/FeedbackProject.tsx
@@ -5,6 +5,7 @@ import { FeedbackDataItemType } from "@/src/mockedData/feedbackData";
 import { FeedbackSlider } from "../../shared/Feedbacks/FeedbackSlider";
 import { FeedbackSliderFromTab } from "../../shared/Feedbacks/FeedbackSliderFromTab";
 import { Section } from "../../shared/Section";
+import { useTranslations } from "next-intl";
 
 export const FeedbackProject = ({
     feedback,
@@ -12,8 +13,17 @@ export const FeedbackProject = ({
     feedback: FeedbackDataItemType[];
 }) => {
     const OPTIONS: EmblaOptionsType = { loop: true, align: "start" };
+    const getTranslation = useTranslations("SomeProjectPage");
+
     return (
         <Section id="feedback" className="relative">
+            <h2 className="font-caviar dark:text-grey text-purple-200 text-2xl tab:text-4xl pc:text-5xl desk:text-6xl font-bold mb-6 tab:mb-10 pc:mb-[64px]">
+                {getTranslation.rich("feedback", {
+                    purple: chunk => (
+                        <span className="text-purple-100">{chunk}</span>
+                    ),
+                })}
+            </h2>
             <div className="tab:hidden">
                 <FeedbackSlider feedbacks={feedback} options={OPTIONS} />
             </div>
