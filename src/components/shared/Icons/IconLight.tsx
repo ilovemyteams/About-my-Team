d0641e8c5a811acdd22c0eb3b5dbcf9e5db18bc1
Anna Prutnik <prutnik.anna@gmail.com>
2025-04-23 19:08:22 +0300

    add animation for light icon

--- src/components/shared/Icons/IconLight.tsx
+++ src/components/shared/Icons/IconLight.tsx
@@ -1,9 +1,23 @@
+"use client";
+
+import { motion } from "framer-motion";
+
 import { IconProps } from "@/types/iconProps.interface";
+
+const pathes = [
+    "M89 231L64 256",
+    "M61 146H31",
+    "M94 77L69 52",
+    "M180 43V13",
+    "M266 77L291 52",
+    "M299.5 146H329.5",
+    "M272 231L297 256",
+];
 
 export const IconLight = ({ className }: IconProps) => {
     return (
         <svg
-            viewBox="0 0 180 160"
+            viewBox="0 0 360 360"
             fill="none"
             xmlns="http://www.w3.org/2000/svg"
             className={className}
@@ -11,39 +25,27 @@ export const IconLight = ({ className }: IconProps) => {
             preserveAspectRatio="xMidYMid meet"
         >
             <path
-                d="M72.8577 149.498H107.152M90.0006 48.8555C98.7032 48.8501 107.152 51.788 113.974 57.1916C120.796 62.5952 125.59 70.147 127.577 78.6198C129.564 87.0925 128.627 95.9883 124.919 103.861C121.211 111.735 114.95 118.122 107.152 121.987V135.998H72.8577V121.987C65.0608 118.123 58.7996 111.736 55.0914 103.863C51.3833 95.9911 50.4461 87.0962 52.4321 78.6239C54.4181 70.1517 59.2106 62.5999 66.0309 57.1956C72.8513 51.7913 81.2987 48.8521 90.0006 48.8555Z"
+                d="M145.715 287.001H214.304M180.001 85.7149C197.406 85.7042 214.304 91.5799 227.948 102.387C241.591 113.194 251.179 128.298 255.153 145.243C259.128 162.189 257.255 179.98 249.838 195.727C242.422 211.473 229.899 224.249 214.304 231.978V260.001H145.715V231.978C130.122 224.249 117.599 211.475 110.183 195.731C102.767 179.986 100.892 162.196 104.864 145.252C108.836 128.307 118.421 113.204 132.062 102.395C145.703 91.5865 162.597 85.7081 180.001 85.7149Z"
                 stroke="currentColor"
-                strokeWidth="0.5"
                 strokeLinecap="round"
                 strokeLinejoin="round"
             />
-            <path
-                d="M44.5 121.5L38.5 127.5"
-                stroke="currentColor"
-                strokeWidth="0.5"
-            />
-            <path
-                d="M47 44.5L41 38.5"
-                stroke="currentColor"
-                strokeWidth="0.5"
-            />
-            <path
-                d="M142 127.5L136 121.5"
-                stroke="currentColor"
-                strokeWidth="0.5"
-            />
-            <path
-                d="M139 38.5L133 44.5"
-                stroke="currentColor"
-                strokeWidth="0.5"
-            />
-            <path
-                d="M149.75 79H158.75"
-                stroke="currentColor"
-                strokeWidth="0.5"
-            />
-            <path d="M21.5 79H30.5" stroke="currentColor" strokeWidth="0.5" />
-            <path d="M90 27.5V18.5" stroke="currentColor" strokeWidth="0.5" />
+            {pathes.map((path, index) => (
+                <motion.path
+                    key={index}
+                    d={path}
+                    stroke="currentColor"
+                    initial={{ pathLength: 0.5 }}
+                    animate={{ pathLength: [0.5, 0.8, 1, 1, 1, 1, 0.8, 0.5] }}
+                    transition={{
+                        duration: 0.5,
+                        delay: 0.5 * index,
+                        ease: "easeInOut",
+                        repeat: Infinity,
+                        repeatDelay: 4,
+                    }}
+                />
+            ))}
         </svg>
     );
 };
