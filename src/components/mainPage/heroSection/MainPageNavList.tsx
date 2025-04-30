89accea2d38d6455c15d35b0be6888cf847837d1
IraMira333 <irinat333@gmail.com>
2025-04-30 16:04:41 +0300

    Restyled hero

--- src/components/mainPage/heroSection/MainPageNavList.tsx
+++ src/components/mainPage/heroSection/MainPageNavList.tsx
@@ -6,22 +6,21 @@ import { localeInURL } from "@/src/utils/localeInURL";
 export const MainPageNavList = () => {
     const getTranslation = useTranslations();
     const navList = [
-        { name: getTranslation("Mission.pageTitle"), id: "aboutus" },
         { name: getTranslation("Portfolio.pageTitle"), id: "portfolio" },
-        { name: getTranslation("Feedback.pageTitle"), id: "feedback" },
         { name: getTranslation("Services.pageTitle"), id: "services" },
-        { name: getTranslation("OurTeam.pageTitle"), id: "team" },
+        { name: getTranslation("Feedback.pageTitle"), id: "feedback" },
         { name: getTranslation("Stages.pageTitle"), id: "stages" },
+        { name: getTranslation("OurTeam.pageTitle"), id: "team" },
         { name: getTranslation("Q&A.pageTitle"), id: "faq" },
     ];
     const locale = useLocale();
     return (
-        <ul className="flex flex-wrap gap-y-4 gap-x-[18%] justify-center tab:justify-between mt-[40px] tab:mt-[56px] pc:mt-[46px] tab:gap-4">
+        <ul className="flex flex-wrap gap-y-4 gap-x-4 justify-center mt-[52px] tab:justify-around tab:mt-0 pc:mt-[97px] tab:gap-8">
             {navList.map((content, idx) => (
                 <li key={idx}>
                     <Link
                         href={`/${localeInURL(locale)}#${content.id}`}
-                        className="text-xs pc:text-sm desk:text-base dark:text-purple-50 text-purple-130 dark:pc:hover:text-red pc:hover:text-redLight
+                        className="text-sm desk:text-base dark:text-purple-50 text-purple-130 dark:pc:hover:text-red pc:hover:text-redLight
                 dark:pc:focus:text-red pc:focus:text-redLight pc:transition pc:ease-out pc:duration-300 dark:active:text-red active:text-redLight"
                     >
                         {content.name.toUpperCase()}
