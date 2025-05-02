2afeed24f109e6905212313ff87cb0ea4b0f522f
Anna Prutnik <prutnik.anna@gmail.com>
2025-05-02 15:35:29 +0300

    fix personal brand icon, fix task section for tablet, fix english text for Zabara

--- src/components/someProjectPage/TasksSection/TasksSection.tsx
+++ src/components/someProjectPage/TasksSection/TasksSection.tsx
@@ -1,3 +1,4 @@
+import { HighlightTitleFromMessages } from "@/src/components/shared/HighlightTitleFromMessages";
 import { TasksSectionDataType } from "@/src/mockedData/portfolioData";
 
 import { PageSection } from "../../shared/PageSection";
@@ -11,6 +12,11 @@ export const TasksSection = ({
 }) => {
     return (
         <PageSection className="pb-[80px] tab:pb-[100px] desk:pb-[120px]">
+            <HighlightTitleFromMessages
+                title="SomeProjectPage"
+                text="taskTitle"
+                className="mb-6 tab:mb-10 pc:mb-[64px] hidden tab:block"
+            />
             <TasksSectionMobile data={content} />
             <TasksSectionNotMobile data={content} />
         </PageSection>
