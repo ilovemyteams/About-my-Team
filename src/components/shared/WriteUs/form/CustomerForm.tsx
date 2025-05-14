59977bbf2279ba281f8bf5b0eb3dc5f24bc1b09e
Anna Prutnik <prutnik.anna@gmail.com>
2025-05-14 11:07:34 +0300

    add title and text for customer form

--- src/components/shared/WriteUs/form/CustomerForm.tsx
+++ src/components/shared/WriteUs/form/CustomerForm.tsx
@@ -27,12 +27,8 @@ export interface ValuesWriteUsFormType {
 export const CustomerForm = ({ notificationHandler }: FormInModalProps) => {
     const getTranslation = useTranslations("CustomerForm");
     const searchParams = useSearchParams();
-    const source = searchParams.get("source");
 
     const path = usePathname();
-
-    const titleProp =
-        source === "/" ? "customerFormHomeTitle" : "customerFormDefaultTitle";
 
     const validationSchema = WriteUsValidation();
 
@@ -101,9 +97,15 @@ export const CustomerForm = ({ notificationHandler }: FormInModalProps) => {
             }) => {
                 return (
                     <Form className="flex flex-col items-center pt-[12px] border-t-[1px] border-purple-strokeLight dark:border-purple-stroke">
-                        <h1 className="font-caviar text-purple-200 dark:text-white-200 text-3xl tab:text-4xl pc:text-5xl self-start mb-[10px]">
-                            {getTranslation(titleProp)}
-                        </h1>
+                        <div className="self-start mb-4 pc:mb-12">
+                            <h1 className="text-purple-200 dark:text-white-200 font-caviar text-3xl tab:text-4xl pc:text-5xl mb-2">
+                                {getTranslation("customerFormTitle")}
+                            </h1>
+                            <p className="text-base">
+                                {getTranslation("customerFormText")}
+                            </p>
+                        </div>
+
                         <CustomField
                             name="name"
                             value={values.name}
