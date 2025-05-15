800ce247c868e3dd0522db7dcbb738c301542ecc
IraMira333 <irinat333@gmail.com>
2025-05-15 21:56:36 +0300

    Create renderOnMain.ts

---
+++ src/constants/renderOnMain.ts
@@ -0,0 +1,45 @@
+export const MAIN_MEMBERS = [
+    "susanna-salata-product-manager",
+    "vlad-shumkov-fullstack-developer",
+    "iryna-stoliarova-ui-ux-designer",
+    "anastasiia-nazarenko-graphic-designer",
+    "iryna-trynkal-fullstack-developer",
+    "kateryna-rubanik-qa-engineer",
+    "anna-prutnik-frontend-developer",
+    "tetiana-seletska-project-manager-scrum-master",
+    "mila-maksymenko-ui-ux-designer",
+    "viktoriia-lapina-ui-ux-designer",
+    "oksana-arpul-qa-engineer",
+];
+
+export const PORTFOLIO_FOR_MAIN = [
+    "alex-chudov",
+    "theatermag-com-ua",
+    "batatfarm-com",
+    "protection-in-ua",
+];
+
+export const DEMO_VIDEOS = [
+    "253ZzoPX1Wc",
+    "cfc30V33h6g",
+    "0g8QIs963dw",
+    "k-c_mDTxpr4",
+    "cBBFrzD8Vsc",
+    "fAZKP3pSNZY",
+    "YpF1TCXBkLs",
+    "gFo6T9kWuZw",
+    "QPyFqwmRjHc",
+    "OWK_mCxyWBI",
+    "3UyOImmhkFE",
+    "7qN4dAMtK7s",
+    "Sfo4XqYN0VA",
+    "1oDKpKw8dPo",
+    "e2Ax8mLlad0",
+    "yuOVqmU9Wsk",
+    "K2B_Gmz73do",
+    "NPVb4npdj-k",
+    "gq2RxByn1Y4",
+    "IJXL7u_p42Y",
+    "KoKDAKDjG8E",
+    "TNa3zk3WMb4",
+];
