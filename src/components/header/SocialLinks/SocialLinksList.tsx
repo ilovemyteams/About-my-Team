75bb3bc149b2e5e35265717af81911d166568ed2
IraMira333 <irinat333@gmail.com>
2025-06-07 23:48:43 +0300

    Update SocialLinksList.tsx

--- src/components/header/SocialLinks/SocialLinksList.tsx
+++ src/components/header/SocialLinks/SocialLinksList.tsx
@@ -1,49 +1,36 @@
-import { useSettingsContext } from "@/src/utils/SettingsSanityContext";
-import { SupportedPlatforms } from "@/types/sanityDataPropsTypes";
-
 import { IconBehance } from "../../shared/Icons/IconBehance";
-import { IconFacebook } from "../../shared/Icons/IconFacebook";
 import { IconGithub } from "../../shared/Icons/IconGithub";
-import { IconGoogleDrive } from "../../shared/Icons/IconGoogleDrive";
 import { IconLinkedin } from "../../shared/Icons/IconLinkedin";
-import { IconTelegram } from "../../shared/Icons/IconTelegram";
-import { IconWhatsapp } from "../../shared/Icons/IconWhatsapp";
 import { IconYoutube } from "../../shared/Icons/IconYoutube";
 import { SocialLink } from "../../shared/SocialLink";
 
-export const SocialLinksList = () => {
-    const { data } = useSettingsContext();
-    const socialLinksIcon = {
-        linkedin: <IconLinkedin />,
-        behance: <IconBehance />,
-        github: <IconGithub />,
-        youtube: <IconYoutube />,
-        facebook: <IconFacebook />,
-        googleDrive: <IconGoogleDrive />,
-        telegram: <IconTelegram />,
-        whatsapp: <IconWhatsapp />,
-    };
+const socialLinksData = [
+    {
+        url: "https://www.linkedin.com/in/susanna-salata/",
+        image: <IconLinkedin />,
+    },
+    {
+        url: "https://www.behance.net/aboutmyteam",
+        image: <IconBehance />,
+    },
+    {
+        url: "https://www.youtube.com/channel/UCx95sgknPRI8rVujgpRYO0g",
+        image: <IconYoutube />,
+    },
+    {
+        url: "https://github.com/Ilovemyteams",
+        image: <IconGithub />,
+    },
+];
 
-    const socialLinksData = data?.header?.socialLinks;
+export const SocialLinksList = () => {
     return (
         <ul className="flex pc:flex-col gap-2">
-            {socialLinksData &&
-                socialLinksData.map(({ platform, url, newWindow }, idx) => {
-                    const icon =
-                        platform &&
-                        socialLinksIcon[platform as SupportedPlatforms];
-
-                    if (!icon) return null;
-                    return (
-                        <li key={idx}>
-                            <SocialLink
-                                url={url || ""}
-                                image={icon}
-                                newWindow={newWindow || true}
-                            />
-                        </li>
-                    );
-                })}
+            {socialLinksData.map(({ url, image }, idx) => (
+                <li key={idx}>
+                    <SocialLink url={url} image={image} />
+                </li>
+            ))}
         </ul>
     );
 };
