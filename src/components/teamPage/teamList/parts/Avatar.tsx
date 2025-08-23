b50bdc95d4fa6be688285887e8246ad5eaf70af8
Anna Prutnik <prutnik.anna@gmail.com>
2025-08-23 18:43:51 +0300

    add default photo icon

--- src/components/teamPage/teamList/parts/Avatar.tsx
+++ src/components/teamPage/teamList/parts/Avatar.tsx
@@ -1,7 +1,11 @@
+"use client";
+
 import Image from "next/image";
 import { useTranslations } from "next-intl";
-import React from "react";
+import { useState } from "react";
 import { twMerge } from "tailwind-merge";
+
+import { IconProfile } from "@/src/components/shared/Icons/IconProfile";
 
 interface AvatarProps {
     imageURL?: string;
@@ -10,12 +14,25 @@ interface AvatarProps {
 }
 
 export const Avatar = ({ imageURL, name, status }: AvatarProps) => {
+    const [error, setError] = useState(false);
     const t = useTranslations("MemberPage");
     return (
         <div className="relative w-fit">
-            <div className="size-[80px]  tab:size-[92px] rounded-full bg-purple-strokeLight dark:bg-purple-stroke overflow-hidden">
+            <div className="size-[80px] isolate tab:size-[92px] rounded-full bg-purple-strokeLight dark:bg-purple-stroke overflow-hidden">
+                <div className="flex items-end justify-center bg-memberMenuGradientLight dark:bg-memberMenuGradientDark absolute inset-0 w-full z-[-1] rounded-full overflow-hidden">
+                    <IconProfile className="w-[80%] tab:w-[59%] h-auto text-purple-strokeLight dark:text-purple-stroke" />
+                </div>
                 {imageURL && (
-                    <Image src={imageURL} alt={name} width={100} height={100} />
+                    <Image
+                        src={imageURL}
+                        alt={name}
+                        width={100}
+                        height={100}
+                        onError={() => setError(true)}
+                        className={
+                            error ? "hidden" : "object-cover w-full h-full"
+                        }
+                    />
                 )}
             </div>
             <div
