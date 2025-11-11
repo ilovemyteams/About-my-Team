c8cdbe205c22138d76481821c6e62661b10ed5e9
AnnaPrutnik <prutnik.anna@gmail.com>
2025-11-11 11:42:12 +0200

    fix theme type and add script for sanity in package json

--- src/app/(site)/providers.tsx
+++ src/app/(site)/providers.tsx
@@ -1,7 +1,8 @@
 "use client";
 import { HeroUIProvider } from "@heroui/react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
-import { type ThemeProviderProps } from "next-themes";
+// import { type ThemeProviderProps } from "next-themes";
+import { ThemeProviderProps } from "next-themes/dist/types";
 import * as React from "react";
 
 export const Providers = ({ children, ...props }: ThemeProviderProps) => {
