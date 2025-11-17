6ef6f1bc1d3412c546f5c1833d1caa72a2e9c5cf
Dmitriy Snitko <dmitriy.snitko@gmail.com>
2025-11-17 21:49:14 +0200

    bugfix

--- src/components/memberPage/parts/Projects.tsx
+++ src/components/memberPage/parts/Projects.tsx
@@ -1,50 +1,41 @@
 import Link from "next/link";
 import { getTranslations } from "next-intl/server";
 
-import { portfolioData } from "@/src/mockedData/portfolioData";
+import { addProtocol, removeProtocol } from "@/src/utils/httpsProtocols";
 
 interface ProjectsProps {
-    projectId: string[];
+    projectsExperience: string[];
 }
 
-export const Projects = async ({ projectId }: ProjectsProps) => {
+export const Projects = async ({ projectsExperience }: ProjectsProps) => {
+    if (!projectsExperience?.length) return null;
+
     const t = await getTranslations("MemberPage");
 
-    const memberProjects = portfolioData.filter(project =>
-        projectId.includes(project.data.slug)
-    );
-
     return (
-        <div>
+        <section>
             <h2 className="font-caviar text-baseb tab:text-lg pc:text-xl desk:text-2xl capitalize">
                 {t("projects")}
             </h2>
             <ul className="grid grid-cols-1 gap-2 desk:gap-3 p-4">
-                {memberProjects.map(({ data }) => {
-                    const cleanUrl = data.siteLink?.replace(
-                        /^(https?:\/\/)?(www\.)?/,
-                        ""
-                    );
-                    const displayUrl = cleanUrl ? `www.${cleanUrl}` : "";
-
+                {projectsExperience.map(project => {
                     return (
                         <li
-                            key={data.id}
+                            key={project}
                             className="text-sm pc:text-base desk:text-[18px] font-normal"
                         >
                             <Link
-                                href={data.siteLink || "#"}
+                                href={addProtocol(project)}
                                 target="_blank"
                                 rel="noopener noreferrer"
                                 className="block max-w-min underline hover:text-redLight hover:dark:text-red truncate"
-                                title={displayUrl}
                             >
-                                {displayUrl}
+                                {removeProtocol(project)}
                             </Link>
                         </li>
                     );
                 })}
             </ul>
-        </div>
+        </section>
     );
 };
