2c7cda99377627e3b82ef62b7f0c4a60f60a92e2
Dmitriy Snitko <dmitriy.snitko@gmail.com>
2025-11-27 19:47:00 +0200

    Fixed the title color for Projects on the member page

--- src/components/memberPage/parts/shared/Title.tsx
+++ src/components/memberPage/parts/shared/Title.tsx
@@ -6,7 +6,7 @@ interface TitleProps {
 
 export const Title = ({ children }: TitleProps) => {
     return (
-        <h2 className="font-caviar text-baseb tab:text-lg pc:text-xl desk:text-2xl capitalize">
+        <h2 className="font-caviar text-baseb tab:text-lg pc:text-xl desk:text-2xl capitalize text-purple-200 dark:text-grey">
             {children}
         </h2>
     );
