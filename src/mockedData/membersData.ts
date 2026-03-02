bfa246031146a9123af4ea7b4702a651af3d11a0
Dmitriy Snitko <dmitriy.snitko@gmail.com>
2026-03-02 19:52:40 +0200

    .

--- src/mockedData/membersData.ts
+++ src/mockedData/membersData.ts
@@ -3128,7 +3128,7 @@ export const membersData: MemberDataItemType[] = [
             position: "FrontEnd Developer",
             categoryName: "developer",
             projectsExperience: ["ilovemyteam.online"],
-            pricePerHour: "7",
+            pricePerHour: "5",
             socialLinks: {
                 linkedin: "https://www.linkedin.com/in/snitko-dmitriy/",
                 github: "https://github.com/dmitriy-snitko",
